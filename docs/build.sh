12c453f07fea7c0f37fe1e2a75f1466106a849c4
Jono Gray <jonathan.gray@flowminder.org>
2018-11-01 15:59:09 +0000

    Support running notebooks in documentation build
    Former-commit-id: ceaf0c165ce9d7a8f6d7585c5b08e4d48c07c2d1

--- docs/build.sh
+++ docs/build.sh
@@ -4,25 +4,53 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-
 set -e
 
-# if [ "$CI" != "true" ]; then
-#     echo "Setting up docker containers"
-#     echo "Bringing down any existing ones."
-#     docker-compose -f docs-build-containers.yml down
-#     echo "Bringing up new ones."
-#     docker-compose -f docs-build-containers.yml up -d
-#     echo "Waiting for flowdb to be ready"
-#     docker exec flowkit_docs_flowdb bash -c 'i=0; until [ $i -ge 24 ] || (pg_isready -h 127.0.0.1 -p 5432); do let i=i+1; echo Waiting 10s; sleep 10; done'
-#     echo "Starting build."
-# fi
 
-BRANCH=${CIRCLE_BRANCH:="master"} mkdocs build
+KillJobs() {
+    for job in $(jobs -p); do
+    		echo "Killing $job"
+            kill -s SIGTERM $job > /dev/null 2>&1 || (sleep 10 && kill -9 $job > /dev/null 2>&1 &)
 
-# if [ "$CI" != "true" ]; then
-#     echo "Bringing down containers."
-#     docker-compose -f docs-build-containers.yml down
-# fi
+    done
+}
 
-echo "Docs built. HTML in flowkit-docs"
+TrapQuit() {
+    if [ "$CI" != "true" ]; then
+	    echo "Bringing down containers."
+	    docker-compose -f docs-build-containers.yml down
+	fi
+
+	echo "Shutting down FlowMachine and FlowAPI"
+    KillJobs
+}
+
+trap TrapQuit EXIT
+
+if [ "$CI" != "true" ]; then
+	export PIPENV_DOTENV_LOCATION=$(pwd)/.env
+    echo "Setting up docker containers"
+    echo "Bringing down any existing ones."
+    docker-compose -f docs-build-containers.yml down
+    echo "Bringing up new ones."
+    docker-compose -f docs-build-containers.yml up -d
+    echo "Waiting for flowdb to be ready"
+    docker exec flowkit_docs_flowdb bash -c 'i=0; until [ $i -ge 24 ] || (pg_isready -h 127.0.0.1 -p 5432); do let i=i+1; echo Waiting 10s; sleep 10; done'
+else
+	export PIPENV_DONT_LOAD_ENV=1
+fi
+
+pushd ../flowmachine
+pipenv install -d
+pipenv run flowmachine &
+echo "Started FlowMachine."
+popd
+pushd ../flowapi
+pipenv install -d
+pipenv run quart run --port 9090 &
+echo "Started FlowAPI."
+popd
+echo "Starting build."
+
+pipenv install
+BRANCH=${CIRCLE_BRANCH:="master"} pipenv run mkdocs "${@:-build}"
