3c9be66f2feabc41749fd43f487e3dd26a85c517
James Harrison <james.harrison@flowminder.org>
2018-11-22 12:25:51 +0000

    Add "OK" button to dismiss error dialog

--- flowauth/frontend/src/ErrorDialog.js
+++ flowauth/frontend/src/ErrorDialog.js
@@ -4,9 +4,11 @@
 
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from "@material-ui/core/Button";
 
 class ErrorDialog extends React.Component {
     state = {
@@ -26,11 +28,15 @@ class ErrorDialog extends React.Component {
                 onClose={this.handleClose}
                 aria-labelledby="error-dialog-title"
                 aria-describedby="error-dialog-description"
+                id="error-dialog"
             >
                 <DialogTitle id="error-dialog-title">{"Error"}</DialogTitle>
                 <DialogContent>
                     <DialogContentText id="error-dialog-description">{message}</DialogContentText>
                 </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">OK</Button>
+                </DialogActions>
             </Dialog>
         );
     }
