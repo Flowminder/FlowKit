5918c0f347365434098739c5d05f145e08df93a3
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2018-12-27 20:39:11 +0000

    Update 04_schema_other.sql

--- flowdb/sql/04_schema_other.sql
+++ flowdb/sql/04_schema_other.sql
@@ -75,6 +75,7 @@ CREATE TABLE etl.etl (
         id SERIAL NOT NULL,
         file_name VARCHAR,
         cdr_type cdrtype,
+	date DATE,
         status status,
         time_stamp TIMESTAMP WITHOUT TIME ZONE,
         PRIMARY KEY (id)
