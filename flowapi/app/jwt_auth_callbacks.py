4d19fdf79040f758cdc5644c6820ef8ec40c5924
Jono Gray <jonathan.gray@flowminder.org>
2019-01-10 16:27:40 +0000

    Move callback fns out for easier testing

--- flowapi/app/jwt_auth_callbacks.py
+++ flowapi/app/jwt_auth_callbacks.py
@@ -37,109 +37,114 @@ def register_logging_callbacks(jwt: JWTManager):
 
     """
 
-    @jwt.expired_token_loader
-    async def expired_token_callback() -> Response:
-        """
-        Log that an access attempt was made with an expired token.
-
-        Returns
-        -------
-        Response
-        """
-
-        current_app.access_logger.error(
-            "EXPIRED_TOKEN",
-            route=request.path,
-            request_id=request.request_id,
-            user=get_jwt_identity(),
-            src_ip=request.headers.get("Remote-Addr"),
-            json_payload=await request.json,
-        )
-
-        return default_expired_token_callback()
-
-    @jwt.claims_verification_failed_loader
-    async def claims_verification_failed_callback() -> Response:
-        """
-        Log that an access attempt was made with claims that failed verification.
-
-        Returns
-        -------
-        Response
-        """
-        current_app.access_logger.error(
-            "CLAIMS_VERIFICATION_FAILED",
-            route=request.path,
-            request_id=request.request_id,
-            user=str(get_jwt_identity()),
-            src_ip=request.headers.get("Remote-Addr"),
-            json_payload=await request.json,
-        )
-        return default_verify_claims_failed_callback()
-
-    @jwt.invalid_token_loader
-    async def invalid_token_callback(error_string) -> Response:
-        """
-        Log that an access attempt was made with a token that was invalid.
-
-        Parameters
-        ----------
-        error_string : str
-            Reason the token is invalid
-
-        Returns
-        -------
-        Response
-
-        """
-        current_app.access_logger.error(
-            "INVALID_TOKEN",
-            error_string=error_string,
-            route=request.path,
-            request_id=request.request_id,
-            user=str(get_jwt_identity()),
-            src_ip=request.headers.get("Remote-Addr"),
-            json_payload=await request.json,
-        )
-        return default_invalid_token_callback(error_string)
-
-    @jwt.revoked_token_loader
-    async def revoked_token_callback() -> Response:
-        """
-        Log that an access attempt was made with a revoked token.
-
-        Returns
-        -------
-        Response
-        """
-        current_app.access_logger.error(
-            "REVOKED_TOKEN",
-            route=request.path,
-            request_id=request.request_id,
-            user=str(get_jwt_identity()),
-            src_ip=request.headers.get("Remote-Addr"),
-            json_payload=await request.json,
-        )
-        return default_revoked_token_callback()
-
-    @jwt.unauthorized_loader
-    async def unauthorized_callback(error_string) -> Response:
-        """
-        Log that an access attempt was made without a token.
-
-        Returns
-        -------
-        Response
-        """
-        current_app.access_logger.error(
-            "UNAUTHORISED",
-            error_string=error_string,
-            route=request.path,
-            request_id=request.request_id,
-            user=str(get_jwt_identity()),
-            src_ip=request.headers.get("Remote-Addr"),
-            json_payload=await request.json,
-        )
-        return default_unauthorized_callback(error_string)
-
+    jwt.expired_token_loader(expired_token_callback)
+    jwt.claims_verification_failed_loader(claims_verification_failed_callback)
+    jwt.invalid_token_loader(invalid_token_callback)
+    jwt.revoked_token_loader(revoked_token_callback)
+    jwt.unauthorized_loader(unauthorized_callback)
     return jwt
+
+
+async def expired_token_callback() -> Response:
+    """
+    Log that an access attempt was made with an expired token.
+
+    Returns
+    -------
+    Response
+    """
+
+    current_app.access_logger.error(
+        "EXPIRED_TOKEN",
+        route=request.path,
+        request_id=request.request_id,
+        user=get_jwt_identity(),
+        src_ip=request.headers.get("Remote-Addr"),
+        json_payload=await request.json,
+    )
+
+    return default_expired_token_callback()
+
+
+async def claims_verification_failed_callback() -> Response:
+    """
+    Log that an access attempt was made with claims that failed verification.
+
+    Returns
+    -------
+    Response
+    """
+    current_app.access_logger.error(
+        "CLAIMS_VERIFICATION_FAILED",
+        route=request.path,
+        request_id=request.request_id,
+        user=str(get_jwt_identity()),
+        src_ip=request.headers.get("Remote-Addr"),
+        json_payload=await request.json,
+    )
+    return default_verify_claims_failed_callback()
+
+
+async def invalid_token_callback(error_string) -> Response:
+    """
+    Log that an access attempt was made with a token that was invalid.
+
+    Parameters
+    ----------
+    error_string : str
+        Reason the token is invalid
+
+    Returns
+    -------
+    Response
+
+    """
+    current_app.access_logger.error(
+        "INVALID_TOKEN",
+        error_string=error_string,
+        route=request.path,
+        request_id=request.request_id,
+        user=str(get_jwt_identity()),
+        src_ip=request.headers.get("Remote-Addr"),
+        json_payload=await request.json,
+    )
+    return default_invalid_token_callback(error_string)
+
+
+async def revoked_token_callback() -> Response:
+    """
+    Log that an access attempt was made with a revoked token.
+
+    Returns
+    -------
+    Response
+    """
+    current_app.access_logger.error(
+        "REVOKED_TOKEN",
+        route=request.path,
+        request_id=request.request_id,
+        user=str(get_jwt_identity()),
+        src_ip=request.headers.get("Remote-Addr"),
+        json_payload=await request.json,
+    )
+    return default_revoked_token_callback()
+
+
+async def unauthorized_callback(error_string) -> Response:
+    """
+    Log that an access attempt was made without a token.
+
+    Returns
+    -------
+    Response
+    """
+    current_app.access_logger.error(
+        "UNAUTHORISED",
+        error_string=error_string,
+        route=request.path,
+        request_id=request.request_id,
+        user=str(get_jwt_identity()),
+        src_ip=request.headers.get("Remote-Addr"),
+        json_payload=await request.json,
+    )
+    return default_unauthorized_callback(error_string)
--- flowapi/tests/unit/test_access_control.py
+++ flowapi/tests/unit/test_access_control.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import json
 import os
+
 import pytest
 from asynctest import return_once
 from .utils import query_kinds
