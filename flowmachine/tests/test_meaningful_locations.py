af95f714628ee068c1d50594f90fafa4d8010fcb
Jono Gray <jonathan.gray@flowminder.org>
2019-01-22 16:38:16 +0000

    Test for bad level handling

--- flowmachine/tests/test_meaningful_locations.py
+++ flowmachine/tests/test_meaningful_locations.py
@@ -75,6 +75,27 @@ def test_column_names_meaningful_locations_aggregate(
     assert get_column_names_from_run(mfl_agg) == mfl_agg.column_names
 
 
+def test_meaningful_locations_aggregate_disallowed_level_raises():
+    """ Test that a bad level raises a ValueError"""
+
+    with pytest.raises(ValueError):
+        mfl_agg = MeaningfulLocations(
+            clusters=HartiganCluster(
+                calldays=CallDays(
+                    subscriber_locations=subscriber_locations(
+                        start="2016-01-01", stop="2016-01-02", level="versioned-site"
+                    )
+                ),
+                radius=1,
+            ),
+            scores=EventScore(
+                start="2016-01-01", stop="2016-01-02", level="versioned-site"
+            ),
+            labels=labels,
+            label="evening",
+        ).aggregate(level="NOT_A_LEVEL")
+
+
 def test_column_names_meaningful_locations_od(
     exemplar_level_param, get_column_names_from_run
 ):
@@ -155,3 +176,95 @@ def test_meaningful_locations_results(
     ).count()
     # Check that query has correctly counted the number of clusters per subscriber
     assert all(count_clusters.n_clusters == count_clusters.cluster)
+
+
+def test_meaningful_locations_aggregation_results(exemplar_level_param, get_dataframe):
+    """
+    Test that MeaningfulLocations returns expected results and counts clusters per subscriber correctly.
+    """
+    if exemplar_level_param["level"] not in MeaningfulLocationsAggregate.allowed_levels:
+        pytest.xfail(
+            f'The level "{exemplar_level_param["level"]}" is not supported as an aggregation unit for MeaningfulLocations.'
+        )
+    mfl = MeaningfulLocations(
+        clusters=HartiganCluster(
+            calldays=CallDays(
+                subscriber_locations=subscriber_locations(
+                    start="2016-01-01", stop="2016-01-02", level="versioned-site"
+                )
+            ),
+            radius=1,
+        ),
+        scores=EventScore(
+            start="2016-01-01", stop="2016-01-02", level="versioned-site"
+        ),
+        labels=labels,
+        label="evening",
+    )
+    mfl_agg = mfl.aggregate(**exemplar_level_param)
+    mfl_df = get_dataframe(mfl)
+    mfl_agg_df = get_dataframe(mfl_agg)
+    # Aggregate should not include any counts below 15
+    assert all(mfl_agg_df.total > 15)
+    # Sum of aggregate should be less than the number of unique subscribers
+    assert mfl_agg_df.total.sum() < mfl_df.subscriber.nunique()
+
+
+def test_meaningful_locations_aggregation_results(exemplar_level_param, get_dataframe):
+    """
+    Test that aggregating MeaningfulLocations returns expected results redacts values below 15.
+    """
+    if exemplar_level_param["level"] not in MeaningfulLocationsAggregate.allowed_levels:
+        pytest.xfail(
+            f'The level "{exemplar_level_param["level"]}" is not supported as an aggregation unit for MeaningfulLocations.'
+        )
+    mfl = MeaningfulLocations(
+        clusters=HartiganCluster(
+            calldays=CallDays(
+                subscriber_locations=subscriber_locations(
+                    start="2016-01-01", stop="2016-01-02", level="versioned-site"
+                )
+            ),
+            radius=1,
+        ),
+        scores=EventScore(
+            start="2016-01-01", stop="2016-01-02", level="versioned-site"
+        ),
+        labels=labels,
+        label="evening",
+    )
+    mfl_agg = mfl.aggregate(**exemplar_level_param)
+    mfl_df = get_dataframe(mfl)
+    mfl_agg_df = get_dataframe(mfl_agg)
+    # Aggregate should not include any counts below 15
+    assert all(mfl_agg_df.total > 15)
+    # Sum of aggregate should be less than the number of unique subscribers
+    assert mfl_agg_df.total.sum() < mfl_df.subscriber.nunique()
+
+
+def test_meaningful_locations_od_raises_for_bad_level(
+    exemplar_level_param, get_dataframe
+):
+    """
+    Test that od on meaningful locations raises a ValueError for a bad level.
+    """
+    mfl = MeaningfulLocations(
+        clusters=HartiganCluster(
+            calldays=CallDays(
+                subscriber_locations=subscriber_locations(
+                    start="2016-01-01", stop="2016-01-02", level="versioned-site"
+                )
+            ),
+            radius=1,
+        ),
+        scores=EventScore(
+            start="2016-01-01", stop="2016-01-02", level="versioned-site"
+        ),
+        labels=labels,
+        label="evening",
+    )
+
+    with pytest.raises(ValueError):
+        mfl_od = MeaningfulLocationsOD(
+            meaningful_locations_a=mfl, meaningful_locations_b=mfl, level="NOT_A_LEVEL"
+        )
