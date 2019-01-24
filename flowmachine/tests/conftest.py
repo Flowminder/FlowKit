2fcab302704d6d41654b88907587a673d82f82b5
Jono Gray <jonathan.gray@flowminder.org>
2019-01-24 14:40:36 +0000

    Mention using the mark to enable date checking

--- flowmachine/tests/conftest.py
+++ flowmachine/tests/conftest.py
@@ -68,6 +68,8 @@ def skip_datecheck(request, monkeypatch):
     """Temporarily patches EventTableSubset so that it thinks any date is
     available, _without_ needing to touch the database. This shaves a little
     time off every `daily_location` creation.
+
+    Use the `check_available_dates` mark on your test to opt-in to date checking.
     """
     run_date_checks = request.node.get_closest_marker("check_available_dates", False)
     if not run_date_checks:
