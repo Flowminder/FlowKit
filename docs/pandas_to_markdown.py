72756975befe60e03f3b8e8874c8497076aa2bff
James Harrison <james.harrison@flowminder.org>
2019-01-31 16:45:26 +0000

    Move token generation from example notebook into preprocessor script

--- docs/pandas_to_markdown.py
+++ docs/pandas_to_markdown.py
@@ -23,3 +23,39 @@ get_ipython().display_formatter.formatters["text/markdown"].for_type(dict, forma
 get_ipython().display_formatter.formatters["text/markdown"].for_type(
     str, lambda x: f">`{x}`"
 )
+
+# Create an API access token
+import os
+import sys
+from dotenv import find_dotenv
+
+path_to_utils_module = os.path.join(
+    os.path.dirname(find_dotenv()), "..", "integration_tests", "tests"
+)
+sys.path.insert(0, path_to_utils_module)
+
+from datetime import timedelta
+from utils import make_token
+
+claims = {
+    "daily_location": {
+        "permissions": {"run": True, "poll": True, "get_result": True},
+        "spatial_aggregation": ["admin3", "admin2"],
+    },
+    "modal_location": {
+        "permissions": {"run": True, "poll": True, "get_result": True},
+        "spatial_aggregation": ["admin3", "admin2"],
+    },
+    "flows": {
+        "permissions": {"run": True, "poll": True, "get_result": True},
+        "spatial_aggregation": ["admin3", "admin2", "admin1"],
+    },
+    "location_event_counts": {
+        "permissions": {"run": True, "poll": True, "get_result": True},
+        "spatial_aggregation": ["admin3", "admin2", "admin1"],
+    },
+}
+
+TOKEN = make_token(
+    username="testuser", secret_key="secret", lifetime=timedelta(days=1), claims=claims
+)
