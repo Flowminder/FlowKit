6088fac9dd42f2672851a1315545c0813fea5bbe
gzagatti <gzagatti@gmail.com>
2019-02-04 14:55:00 -0500

    Updates unit test for NocturnalEvents.

--- flowmachine/tests/test_nocturnal_events.py
+++ flowmachine/tests/test_nocturnal_events.py
@@ -5,24 +5,30 @@
 
 import pytest
 
-from flowmachine.features import NocturnalCalls
+from flowmachine.features import NocturnalEvents
 
 
 def test_has_right_columns():
     """
-    NocturnalCalls() dataframe returns the right columns.
+    NocturnalEvents() dataframe returns the right columns.
     """
-    noc = NocturnalCalls("2016-01-01", "2016-01-02")
+    noc = NocturnalEvents("2016-01-01", "2016-01-02")
     expected_columns = ["subscriber", "percentage_nocturnal"]
     assert noc.column_names == expected_columns
 
 
 def test_values(get_dataframe):
     """
-    NocturnalCalls() returns correct values for a few hand picked values.
+    NocturnalEvents() returns correct values for a few hand picked values.
     """
-    noc = NocturnalCalls("2016-01-01", "2016-01-02")
+    noc = NocturnalEvents("2016-01-01", "2016-01-02")
     df = get_dataframe(noc)
     df.set_index("subscriber", inplace=True)
     assert df.percentage_nocturnal["BKMy1nYEZpnoEA7G"] == pytest.approx(57.142857)
     assert df.percentage_nocturnal["dM4aLP8N97eYABwR"] == pytest.approx(33.333333)
+
+    noc = NocturnalEvents("2016-01-01", "2016-01-05", direction="in")
+    df = get_dataframe(noc)
+    df.set_index("subscriber", inplace=True)
+    assert df.percentage_nocturnal["BKMy1nYEZpnoEA7G"] == pytest.approx(50.0)
+    assert df.percentage_nocturnal["dM4aLP8N97eYABwR"] == pytest.approx(25.0)
