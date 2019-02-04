d7a76d6d2e1683c5779e8791918278f550de84f5
gzagatti <gzagatti@gmail.com>
2019-02-04 14:12:15 -0500

    Increase test coverage.

--- flowmachine/tests/test_contact_balance.py
+++ flowmachine/tests/test_contact_balance.py
@@ -22,6 +22,23 @@ def test_some_results(get_dataframe):
         set_df.loc["3XKdxqvyNxO2vLD1"]["msisdn_counterpart"].values[0]
         == "DELmRj9Vvl346G50"
     )
+    df = get_dataframe(ContactBalance("2016-01-01", "2016-01-07", direction="in"))
+    set_df = df.set_index("subscriber")
+    assert set_df.loc["bvEWVnZdwJ8Lgkm2"]["proportion"] == 1.000000
+    assert set_df.loc["8lo9EgjnyjgKO7vL"]["events"] == 19
+    assert (
+        set_df.loc["3XKdxqvyNxO2vLD1"]["msisdn_counterpart"]
+        == "7lNP0mDOAK3xKWv4"
+    )
+
+    df = get_dataframe(ContactBalance("2016-01-01", "2016-01-07", direction="out"))
+    set_df = df.set_index("subscriber")
+    assert set_df.loc["V1QBpMj0vEwr2PGW"]["proportion"] == 1.000000
+    assert set_df.loc["7XebRKr35JMJnq8A"]["events"] == 12
+    assert (
+        set_df.loc["3XKdxqvyNxO2vLD1"]["msisdn_counterpart"]
+        == "DELmRj9Vvl346G50"
+    )
 
 
 def test_no_result_is_greater_than_one(get_dataframe):
