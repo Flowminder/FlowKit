a3ec51acb39301aa68909d04fe26b116ae1f2109
Maximilian Albert <maximilian.albert@flowminder.org>
2019-02-05 12:44:16 +0000

    Add approval tests for daily location query with non-standard parameters

--- flowmachine/functional_tests/test_daily_location.py
+++ flowmachine/functional_tests/test_daily_location.py
@@ -22,3 +22,39 @@ def test_daily_location_1_df(get_dataframe, diff_reporter):
     dl = daily_location("2016-01-01", "2016-01-02")
     df = get_dataframe(dl)
     verify(df.to_csv(), diff_reporter)
+
+
+def test_daily_location_2_sql(diff_reporter):
+    """
+    Daily location query with non-default parameters returns the expected SQL string.
+    """
+    dl = daily_location(
+        "2016-01-04",
+        level="admin2",
+        hours=(3, 9),
+        method="most-common",
+        subscriber_identifier="imei",
+        column_name="admin2pcod",
+        ignore_nulls=False,
+        subscriber_subset=["2GJxeNazvlgZbqj6", "7qKmzkeMbmk5nOa0", "8dpPLR15XwR7jQyN", "1NqnrAB9bRd597x2"],
+    )
+    sql = dl.get_query()
+    verify(sql, diff_reporter)
+
+
+def test_daily_location_2_df(get_dataframe, diff_reporter):
+    """
+    Daily location query with non-default parameters returns the expected data.
+    """
+    dl = daily_location(
+        "2016-01-04",
+        level="admin2",
+        hours=(3, 9),
+        method="most-common",
+        #subscriber_identifier="imei",
+        #column_name="admin2pcod",
+        ignore_nulls=False,
+        subscriber_subset=["2GJxeNazvlgZbqj6", "7qKmzkeMbmk5nOa0", "8dpPLR15XwR7jQyN", "1NqnrAB9bRd597x2"],
+    )
+    df = get_dataframe(dl)
+    verify(df.to_csv(), diff_reporter)
