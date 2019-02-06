3f0d094d7466f4d02300e58b0068cdd4d833d7a6
gzagatti <gzagatti@gmail.com>
2019-02-06 16:29:34 -0500

    Implements unit testing for ProportionEventType.

--- flowmachine/tests/test_subscriber_event_type_proportion.py
+++ flowmachine/tests/test_subscriber_event_type_proportion.py
@@ -6,36 +6,23 @@ from flowmachine.features.subscriber.event_type_proportion import *
 
 import pytest
 
-
-@pytest.mark.parametrize(
-    "numerator, msisdn, want",
-    [
-        ("events.calls", "AgB6KR3Levd9Z1vJ", 0.351_852),
-        ("events.sms", "7ra3xZakjEqB1Al5", 0.362_069),
-        ("events.mds", "QrAlXqDbXDkNJe3E", 0.236_363_63),
-        ("events.topups", "bKZLwjrMQG7z468y", 0.183_098_5),
-        (["events.calls", "events.sms"], "AgB6KR3Levd9Z1vJ", 0.648_148_1),
-    ],
-)
-def test_proportion_event_type(get_dataframe, numerator, msisdn, want):
+@pytest.mark.parametrize("event_type, msisdn, want", [
+    ("calls", "AgB6KR3Levd9Z1vJ", 0.351852),
+    ("sms", "7ra3xZakjEqB1Al5", 0.362069),
+    ("mds", "QrAlXqDbXDkNJe3E", 0.23636363),
+    ("topups", "bKZLwjrMQG7z468y", 0.1830985),
+])
+def test_proportion_event_type(get_dataframe, event_type, msisdn, want):
     """
     Test some hand picked periods and tables
     """
-    query = ProportionEventType(
-        "2016-01-01",
-        "2016-01-08",
-        numerator,
-        tables=[
-            "events.calls",
-            "events.sms",
-            "events.mds",
-            "events.topups",
-            "events.forwards",
-        ],
-    )
+    query = ProportionEventType("2016-01-01", "2016-01-08", event_type,
+        tables=["events.calls", "events.sms", "events.mds", "events.topups", "events.forwards"])
     df = get_dataframe(query).set_index("subscriber")
-    assert df.value[msisdn] == pytest.approx(want)
+    assert df.loc[msisdn].proportion == pytest.approx(want)
 
-    query = ProportionEventType("2016-01-02", "2016-01-04", numerator, tables=numerator)
+    query = ProportionEventType("2016-01-02", "2016-01-04", event_type,
+        tables=[f"events.{event_type}"])
     df = get_dataframe(query).set_index("subscriber")
-    assert df.value.unique() == [1]
+    assert df.proportion.unique() == [1]
+
