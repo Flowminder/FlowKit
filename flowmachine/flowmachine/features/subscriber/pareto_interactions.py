6ab9e4f0017453a7eca56a600a9927b49524c5e1
gzagatti <gzagatti@gmail.com>
2019-02-06 17:45:17 -0500

    Refactors code and include direction in arguments.

--- flowmachine/flowmachine/features/subscriber/pareto_interactions.py
+++ flowmachine/flowmachine/features/subscriber/pareto_interactions.py
@@ -33,9 +33,11 @@ class ParetoInteractions(SubscriberFeature):
     hours : tuple of float, default 'all'
         Restrict the analysis to only a certain set
         of hours within each day.
+    direction : {'in', 'out', 'both'}, default 'both'
+        Whether to consider calls made, received, or both. Defaults to 'out'.
     exclude_self_calls : bool, default True
         Set to false to *include* calls a subscriber made to themself
-    tables : str, default 'all'
+    table : str, default 'all'
     subscriber_identifier : {'msisdn', 'imei'}, default 'msisdn'
         Either msisdn, or imei, the column that identifies the subscriber.
     subscriber_subset : str, list, flowmachine.core.Query, flowmachine.core.Table, default None
@@ -50,13 +52,14 @@ class ParetoInteractions(SubscriberFeature):
     --------
     >>> p = ParetoInteractions("2016-01-01", "2016-01-02")
     >>> p.get_dataframe()
+
              subscriber  pareto
     0  038OVABN11Ak4W5P     1.0
     1  09NrjaNNvDanD8pk     1.0
     2  0ayZGYEQrqYlKw6g     1.0
     3  0DB8zw67E9mZAPK2     1.0
     4  0Gl95NRLjW2aw8pW     1.0
-
+                    ...     ...
     """
 
     def __init__(
@@ -64,79 +67,90 @@ class ParetoInteractions(SubscriberFeature):
         start,
         stop,
         proportion=0.8,
+        *,
+        direction="both",
         tables="all",
         subscriber_identifier="msisdn",
         hours="all",
         exclude_self_calls=False,
-        **kwargs
+        subscriber_subset=None,
     ):
 
-        self.tables = tables
         self.start = start
         self.stop = stop
+        self.hours = hours
+        self.direction = direction
+        self.tables = tables
         self.subscriber_identifier = subscriber_identifier
-        self.contact_balance = ContactBalance(
-            start,
-            stop,
-            tables=tables,
-            subscriber_identifier=subscriber_identifier,
-            hours=hours,
-            exclude_self_calls=exclude_self_calls,
-            **kwargs
-        )
-        self.subscriber_degree = SubscriberDegree(
-            start,
-            stop,
-            tables=tables,
-            subscriber_identifier=subscriber_identifier,
-            hours=hours,
-            **kwargs
-        )
-        self._cols = ["subscriber", "pareto"]
+        self.exclude_self_calls = exclude_self_calls
+
         if 1 > proportion > 0:
             self.proportion = proportion
         else:
             raise ValueError("{} is not a valid proportion.".format(proportion))
+
+
+        self.contact_balance = ContactBalance(
+            self.start,
+            self.stop,
+            hours=self.hours,
+            tables=self.tables,
+            subscriber_identifier=self.subscriber_identifier,
+            direction=self.direction,
+            exclude_self_calls=exclude_self_calls,
+            subscriber_subset = subscriber_subset,
+        )
+
+        self.subscriber_degree = SubscriberDegree(
+            self.start,
+            self.stop,
+            hours=self.hours,
+            tables=self.tables,
+            subscriber_identifier=self.subscriber_identifier,
+            direction=self.direction,
+            exclude_self_calls=self.exclude_self_calls,
+            subscriber_subset=subscriber_subset,
+        )
+
+        self._cols = ["subscriber", "pareto"]
+
         super().__init__()
 
     def _make_query(self):
-        target_qur = """SELECT b.subscriber, ceil(sum(events*{pct})) as target 
-                  FROM ({balance}) b
-                  GROUP BY b.subscriber
-        """.format(
-            pct=self.proportion, balance=self.contact_balance.get_query()
-        )
-        cumulative_events = """
-                          SELECT row_number() OVER (
-                          PARTITION BY c.subscriber ORDER BY events DESC, c.msisdn_counterpart DESC) AS n_contacts,
-                           c.subscriber, c.msisdn_counterpart, c.events,
-                            sum(c.events) OVER (
-                          PARTITION BY c.subscriber ORDER BY events DESC, c.msisdn_counterpart DESC) AS cum_events
-                          FROM ({balance}) c
-                          ORDER BY cum_events DESC
-                          """.format(
-            balance=self.contact_balance.get_query(), uid=self.subscriber_identifier
-        )
-        subscriber_count = """
-            SELECT DISTINCT ON(cu.subscriber) n_contacts, cu.subscriber as subscriber, (tgt.target - cu.cum_events) as dif FROM
-             ({cum}) cu
-             LEFT JOIN
-             ({target}) tgt
-             ON cu.subscriber = tgt.subscriber
-             WHERE (tgt.target - cu.cum_events) <= 0
-             ORDER BY cu.subscriber, n_contacts
-        """.format(
-            cum=cumulative_events, target=target_qur
-        )
-        qur = """
-            SELECT uc.subscriber as subscriber, n_contacts/degree::FLOAT as pareto FROM
-            ({uc}) uc
-            LEFT JOIN
-            ({ud}) ud
-            ON uc.subscriber = ud.subscriber
-            ORDER BY uc.subscriber
-        """.format(
-            uc=subscriber_count, ud=self.subscriber_degree.get_query()
-        )
 
-        return qur
+        target_qur = f"""
+        SELECT b.subscriber, ceil(sum(events*{self.proportion})) as target
+        FROM ({self.contact_balance.get_query()}) b
+        GROUP BY b.subscriber
+        """
+
+        cumulative_events = f"""
+        SELECT row_number() OVER (
+        PARTITION BY c.subscriber ORDER BY events DESC, c.msisdn_counterpart DESC) AS n_contacts,
+         c.subscriber, c.msisdn_counterpart, c.events,
+          sum(c.events) OVER (
+        PARTITION BY c.subscriber ORDER BY events DESC, c.msisdn_counterpart DESC) AS cum_events
+        FROM ({self.contact_balance.get_query()}) c
+        ORDER BY cum_events DESC
+        """
+
+        subscriber_count = f"""
+        SELECT DISTINCT ON(cu.subscriber) n_contacts, cu.subscriber as subscriber, (tgt.target - cu.cum_events) as dif FROM
+        ({cumulative_events}) cu
+        LEFT JOIN
+        ({target_qur}) tgt
+        ON cu.subscriber = tgt.subscriber
+        WHERE (tgt.target - cu.cum_events) <= 0
+        ORDER BY cu.subscriber, n_contacts
+        """
+
+        sql = f"""
+        SELECT uc.subscriber as subscriber, n_contacts/degree::FLOAT as pareto FROM
+        ({subscriber_count}) uc
+        LEFT JOIN
+        ({self.subscriber_degree.get_query()}) ud
+        ON uc.subscriber = ud.subscriber
+        ORDER BY uc.subscriber
+        """
+
+        return sql
