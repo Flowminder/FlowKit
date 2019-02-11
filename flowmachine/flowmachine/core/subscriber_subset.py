99dbaf480daeb8401a045d34c6a7a138e0f14fce
Maximilian Albert <maximilian.albert@flowminder.org>
2019-02-11 23:16:02 +0000

    Add docstrings

--- flowmachine/flowmachine/core/subscriber_subset.py
+++ flowmachine/flowmachine/core/subscriber_subset.py
@@ -40,15 +40,29 @@ class SubscriberSubsetBase(Query):
 
 class AllSubscribers(SubscriberSubsetBase):
     """
-    Represents the 'subset' of all subscribers - i.e., no subsetting at all.
+    Represents the subset of all subscribers (i.e., no subsetting at all).
     """
 
     is_proper_subset = False
 
     def _make_query(self):
+        # Return a dummy string representing this subset. This is only needed
+        # because SubscriberSubsetBase currently inherits from Query, but will
+        # eventually be removed.
         return "<AllSubscribers>"
 
-    def apply_subset(self, sql, *, subscriber_identifier):
+    def apply_subset(self, sql, *, subscriber_identifier=None):
+        """
+        Return the input query unchanged, since no subsetting is applied.
+
+        Parameters
+        ----------
+        sql : sqlalchemy.sql.ClauseElement
+            The SQL query to which the subset should be applied.
+
+        subscriber_identifier : str
+            This argument is ignored for subsets of type 'AllSubscribers'.
+        """
         return sql
 
 
@@ -60,14 +74,34 @@ class SubsetFromFlowmachineQuery(SubscriberSubsetBase):
     is_proper_subset = True
 
     def __init__(self, flowmachine_query):
+        assert isinstance(flowmachine_query, Query)
+
         self.subset_query = text(flowmachine_query.get_query()).columns(
             column("subscriber")
         )
 
     def _make_query(self):
+        # Return a dummy string representing this subset. This is only needed
+        # because SubscriberSubsetBase currently inherits from Query, but will
+        # eventually be removed.
         return "<SubsetFromFlowmachineQuery>"
 
     def apply_subset(self, sql, *, subscriber_identifier=None):
+        """
+        Return a modified version of the input SQL query which has the subset applied.
+
+        Parameters
+        ----------
+        sql : sqlalchemy.sql.ClauseElement
+            The SQL query to which the subset should be applied.
+
+        subscriber_identifier : str
+            This argument is ignored for subsets of type 'SubsetFromFlowmachineQuery'.
+
+        Returns
+        ----------
+        sqlalchemy.sql.ClauseElement
+        """
         assert isinstance(sql, ClauseElement)
 
         tbl = sql.alias("tbl")
@@ -93,12 +127,31 @@ class ExplicitSubset(SubscriberSubsetBase):
     is_proper_subset = True
 
     def __init__(self, subscribers):
+        assert isinstance(subscribers, (list, tuple))
         self.subscribers = subscribers
 
     def _make_query(self):
+        # Return a dummy string representing this subset. This is only needed
+        # because SubscriberSubsetBase currently inherits from Query, but will
+        # eventually be removed.
         return "<ExplicitSubset>"
 
     def apply_subset(self, sql, *, subscriber_identifier):
+        """
+        Return a modified version of the input SQL query which has the subset applied.
+
+        Parameters
+        ----------
+        sql : sqlalchemy.sql.ClauseElement
+            The SQL query to which the subset should be applied.
+
+        subscriber_identifier : str
+            The column in the parent table which contains the subscriber information.
+
+        Returns
+        ----------
+        sqlalchemy.sql.ClauseElement
+        """
         assert isinstance(sql, ClauseElement)
         assert len(sql.froms) == 1
         parent_table = sql.froms[0]
@@ -106,6 +159,19 @@ class ExplicitSubset(SubscriberSubsetBase):
 
 
 def make_subscriber_subset(subset):
+    """
+    Return an instance of an appropriate subclass of SubscriberSubsetBase representing the given input.
+
+    Parameters
+    ----------
+    subset : "all" or None or list or tuple or flowmachine.Query or SubscriberSubsetBase
+        This can be one of the following:
+          - "all" or None: represents the subset of "all subscribers (i.e., no subsetting at all)
+          - list or tuple: represents a subset of an explicit list of subscribers
+          - flowmachine.Query: represents a subset given by the result of a flowmachine query
+            (where the resulting table must have a "subscriber" column)
+        If `subset` is already an instance of SubscriberSubsetBase then it is returned unchanged.
+    """
     if isinstance(subset, SubscriberSubsetBase):
         return subset
     elif subset == "all" or subset is None:
