0a0b9a0d36577f7f0d93b27a9bd6d4ad079d2af8
Maximilian Albert <maximilian.albert@flowminder.org>
2019-02-12 13:23:17 +0000

    Remove unused import

--- flowmachine/tests/functional_tests/test_daily_location.py
+++ flowmachine/tests/functional_tests/test_daily_location.py
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import pytest
-
 from flowmachine.core.utils import pretty_sql
 
 from approvaltests.approvals import verify
