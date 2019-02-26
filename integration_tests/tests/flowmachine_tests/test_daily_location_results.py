4e92224d45f89aae7b1837b77e47d4743008c4e4
Jono Gray <jonathan.gray@flowminder.org>
2019-02-26 10:09:20 +0000

    Fix another couple of customquery usages

--- integration_tests/tests/flowmachine_tests/test_daily_location_results.py
+++ integration_tests/tests/flowmachine_tests/test_daily_location_results.py
@@ -14,7 +14,8 @@ def test_daily_location_1_sql(diff_reporter):
     Daily location query with non-default parameters returns the expected data.
     """
     subset_query = CustomQuery(
-        "SELECT DISTINCT msisdn AS subscriber FROM events.calls WHERE msisdn in ('GNLM7eW5J5wmlwRa', 'e6BxY8mAP38GyAQz', '1vGR8kp342yxEpwY')"
+        "SELECT DISTINCT msisdn AS subscriber FROM events.calls WHERE msisdn in ('GNLM7eW5J5wmlwRa', 'e6BxY8mAP38GyAQz', '1vGR8kp342yxEpwY')",
+        column_names=["subscriber"],
     )
     dl = daily_location(
         "2016-01-05",
@@ -34,7 +35,8 @@ def test_daily_location_1_df(get_dataframe, diff_reporter):
     # Note that subscriber `1vGR8kp342yxEpwY` should be missing from the result
     # because they have no event on 2016-01-05 after 11pm or before 5am.
     subset_query = CustomQuery(
-        "SELECT DISTINCT msisdn AS subscriber FROM events.calls WHERE msisdn in ('GNLM7eW5J5wmlwRa', 'e6BxY8mAP38GyAQz', '1vGR8kp342yxEpwY')"
+        "SELECT DISTINCT msisdn AS subscriber FROM events.calls WHERE msisdn in ('GNLM7eW5J5wmlwRa', 'e6BxY8mAP38GyAQz', '1vGR8kp342yxEpwY')",
+        column_names=["subscriber"],
     )
     dl = daily_location(
         "2016-01-05",
