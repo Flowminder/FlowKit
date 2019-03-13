557c418b1667a99359d11e9b065320819f63c4e6
Maximilian Albert <maximilian.albert@flowminder.org>
2019-03-13 11:22:17 +0000

    Add test for unknown action

--- integration_tests/tests/flowmachine_server_tests/test_server_NEW.py
+++ integration_tests/tests/flowmachine_server_tests/test_server_NEW.py
@@ -6,3 +6,13 @@ def test_ping_flowmachine_server(send_zmq_message_and_receive_reply):
     reply = send_zmq_message_and_receive_reply(msg)
     expected_reply = {"status": "accepted", "msg": "pong", "data": {}}
     assert expected_reply == reply
+
+
+def test_unknown_action_returns_error(send_zmq_message_and_receive_reply):
+    """
+    Unknown action returns an error response.
+    """
+    msg = {"action": "foobar", "request_id": "DUMMY_ID"}
+    reply = send_zmq_message_and_receive_reply(msg)
+    expected_reply = {"status": "error", "msg": "Unknown action: 'foobar'", "data": {}}
+    assert expected_reply == reply
