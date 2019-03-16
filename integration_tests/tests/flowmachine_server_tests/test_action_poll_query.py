8be894b0e8728732de4e3c62fe6d70aed40c2fed
Maximilian Albert <maximilian.albert@flowminder.org>
2019-03-16 01:35:36 +0000

    Remove query_kind arg from poll_query (only query_id needed)

--- integration_tests/tests/flowmachine_server_tests/test_action_poll_query.py
+++ integration_tests/tests/flowmachine_server_tests/test_action_poll_query.py
@@ -9,6 +9,7 @@ async def test_poll_existing_query(zmq_url):
     """
     Polling a query with non-existent query id returns expected error.
     """
+    expected_query_id = "dummy_query_d5d01a68ba6305f24a721b802341335b"
     msg = {
         "action": "run_query",
         "params": {"query_kind": "dummy_query", "dummy_param": "foobar"},
@@ -18,23 +19,20 @@ async def test_poll_existing_query(zmq_url):
     expected_reply = {
         "status": "accepted",
         "msg": "",
-        "data": {"query_id": "7a2553620227ccd7e4b3ef6a1e0aa9aa"},
+        "data": {"query_id": expected_query_id},
     }
     assert expected_reply == reply
 
     msg = {
         "action": "poll_query",
-        "params": {
-            "query_id": "7a2553620227ccd7e4b3ef6a1e0aa9aa",
-            "query_kind": "dummy_query",
-        },
+        "params": {"query_id": expected_query_id},
         "request_id": "DUMMY_ID",
     }
     reply = send_message_and_get_reply(zmq_url, msg)
     expected_reply = {
         "status": "done",
         "msg": "",
-        "data": {"query_id": "7a2553620227ccd7e4b3ef6a1e0aa9aa", "status": "completed"},
+        "data": {"query_id": expected_query_id, "query_state": "completed"},
     }
     assert expected_reply == reply
 
@@ -44,7 +42,11 @@ async def test_poll_query_with_nonexistent_query_id_fails(zmq_url):
     """
     Polling a query with non-existent query id returns expected error.
     """
-    msg = {"action": "poll", "query_id": "FOOBAR", "request_id": "DUMMY_ID"}
+    msg = {
+        "action": "poll_query",
+        "params": {"query_id": "FOOBAR"},
+        "request_id": "DUMMY_ID",
+    }
 
     reply = send_message_and_get_reply(zmq_url, msg)
     assert {
