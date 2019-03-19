d91d1e8d03c68feab8e297a0ce15466b22771fe7
Maximilian Albert <maximilian.albert@flowminder.org>
2019-03-19 12:40:06 +0000

    Fix schema for meaningful locations aggregate

--- flowmachine/flowmachine/core/server/query_schemas/meaningful_locations.py
+++ flowmachine/flowmachine/core/server/query_schemas/meaningful_locations.py
@@ -1,6 +1,15 @@
 from marshmallow import Schema, fields, post_load
 from marshmallow.validate import OneOf, Range
+from typing import Union, Dict, List
 
+from flowmachine.features import (
+    MeaningfulLocationsAggregate,
+    MeaningfulLocations,
+    HartiganCluster,
+    CallDays,
+    EventScore,
+    subscriber_locations,
+)
 from .base_exposed_query import BaseExposedQuery
 
 
@@ -37,14 +46,85 @@ class MeaningfulLocationsSchema(Schema):
 
 
 class MeaningfulLocationsAggregateSchema(Schema):
-    meaningful_locations = fields.Nested(MeaningfulLocationsSchema)
+    start_date = fields.Date(required=True)
+    stop_date = fields.Date(required=True)
+    label = fields.String(required=True)
+    labels = fields.Dict(
+        keys=fields.String(), values=fields.Dict()
+    )  # TODO: use custom field here for stricter validation!
+    tower_hour_of_day_scores = fields.List(
+        fields.Float(validate=Range(min=-1.0, max=1.0))
+    )
+    tower_day_of_week_scores = fields.Dict(
+        keys=fields.String(
+            validate=OneOf(
+                [
+                    "monday",
+                    "tuesday",
+                    "wednesday",
+                    "thursday",
+                    "friday",
+                    "saturday",
+                    "sunday",
+                ]
+            )
+        ),
+        values=fields.Float(validate=Range(min=-1.0, max=1.0)),
+    )
     aggregation_unit = fields.String(
         validate=OneOf(["admin0", "admin1", "admin2", "admin3"])
+    )
+    tower_cluster_radius = fields.Float(default=1.0)
+    tower_cluster_call_threshold: fields.Integer(default=0)
+    subscriber_subset: fields.String(
+        required=False, allow_none=True, validate=OneOf([None])
     )
 
     @post_load
     def make_query_object(self, params):
-        return Foobar(**params)
+        return _make_meaningful_locations(**params)
+
+
+def _make_meaningful_locations(
+    start_date: str,
+    stop_date: str,
+    aggregation_unit: str,
+    label: str,
+    labels: Dict[str, Dict[str, dict]],
+    tower_day_of_week_scores: Dict[str, float],
+    tower_hour_of_day_scores: List[float],
+    tower_cluster_radius: float = 1.0,
+    tower_cluster_call_threshold: int = 0,
+    subscriber_subset: Union[dict, None] = None,
+):
+    q_subscriber_locations = subscriber_locations(
+        start=start_date,
+        stop=stop_date,
+        level="versioned-site",
+        subscriber_subset=subscriber_subset,
+    )
+    q_call_days = CallDays(subscriber_locations=q_subscriber_locations)
+    q_hartigan_cluster = HartiganCluster(
+        calldays=q_call_days,
+        radius=tower_cluster_radius,
+        call_threshold=tower_cluster_call_threshold,
+        buffer=0,  # we're not exposing 'buffer', apparently
+    )
+    q_event_score = EventScore(
+        start=start_date,
+        stop=stop_date,
+        score_hour=tower_hour_of_day_scores,
+        score_dow=tower_day_of_week_scores,
+        level="versioned-site",
+        subscriber_subset=subscriber_subset,
+    )
+    q_meaningful_locations = MeaningfulLocations(
+        clusters=q_hartigan_cluster, labels=labels, scores=q_event_score, label=label
+    )
+
+    return MeaningfulLocationsAggregate(
+        meaningful_locations=q_meaningful_locations, level=aggregation_unit
+    )
 
 
 class MeaningfulLocationsAggregateExposed(BaseExposedQuery):
