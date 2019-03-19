1c8d65339579529699697943dbbc034f838ff90d
James Harrison <james.harrison@flowminder.org>
2019-03-19 10:27:02 +0000

    Add tests for geo_augment and distance_matrix_query methods

--- flowmachine/tests/test_spatial_unit.py
+++ flowmachine/tests/test_spatial_unit.py
@@ -2,8 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from flowmachine.core import CustomQuery
 from flowmachine.core.spatial_unit import (
-    SpatialUnit,
+    BaseSpatialUnit,
     LatLonSpatialUnit,
     VersionedCellSpatialUnit,
     VersionedSiteSpatialUnit,
@@ -15,7 +16,7 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "spatial_unit, args",
+    "spatial_unit, kwargs",
     [
         (LatLonSpatialUnit, {}),
         (VersionedCellSpatialUnit, {}),
@@ -37,16 +38,16 @@ import pytest
         (GridSpatialUnit, {"size": 5}),
     ],
 )
-def test_spatial_unit_column_names(spatial_unit, args):
+def test_spatial_unit_column_names(spatial_unit, kwargs):
     """
     Test that the SpatialUnit classes have accurate column_names properties.
     """
-    instance = spatial_unit(**args)
-    assert instance.head(0).columns.tolist() == instance.column_names
+    su = spatial_unit(**kwargs)
+    assert su.head(0).columns.tolist() == su.column_names
 
 
 @pytest.mark.parametrize(
-    "spatial_unit, args, loc_cols",
+    "spatial_unit, kwargs, loc_cols",
     [
         (LatLonSpatialUnit, {}, ["lat", "lon"]),
         (VersionedCellSpatialUnit, {}, ["location_id", "version", "lon", "lat"]),
@@ -75,12 +76,12 @@ def test_spatial_unit_column_names(spatial_unit, args):
         (GridSpatialUnit, {"size": 5}, ["grid_id"]),
     ],
 )
-def test_spatial_unit_location_columns(spatial_unit, args, loc_cols):
+def test_spatial_unit_location_columns(spatial_unit, kwargs, loc_cols):
     """
     Test that the SpatialUnit classes have the correct location_columns properties.
     """
-    instance = spatial_unit(**args)
-    assert loc_cols == instance.location_columns
+    su = spatial_unit(**kwargs)
+    assert loc_cols == su.location_columns
 
 
 def test_polygon_spatial_unit_column_list():
@@ -104,8 +105,13 @@ def test_missing_location_columns_raises_error():
     Test that a ValueError is raised if the location_column_names passed to
     SpatialUnit are not a subset of column_names.
     """
+
+    class TestSpatialUnit(BaseSpatialUnit):
+        def geo_augment(self, query):
+            pass
+
     with pytest.raises(ValueError, match="['NOT_A_COLUMN']"):
-        su = SpatialUnit(
+        su = TestSpatialUnit(
             selected_column_names=[
                 "id AS location_id",
                 "date_of_first_service",
@@ -113,3 +119,44 @@ def test_missing_location_columns_raises_error():
             ],
             location_column_names=["location_id", "NOT_A_COLUMN"],
         )
+
+
+@pytest.mark.parametrize(
+    "spatial_unit, kwargs",
+    [
+        (LatLonSpatialUnit, {}),
+        (VersionedCellSpatialUnit, {}),
+        (VersionedSiteSpatialUnit, {}),
+        (
+            PolygonSpatialUnit,
+            {"polygon_column_names": "admin3pcod", "polygon_table": "geography.admin3"},
+        ),
+        (AdminSpatialUnit, {"level": 2}),
+        (AdminSpatialUnit, {"level": 2, "column_name": "admin2name"}),
+        (GridSpatialUnit, {"size": 5}),
+    ],
+)
+def test_geo_augment_columns(spatial_unit, kwargs):
+    """
+    Test that the columns returned by the geo_augment method are correct.
+    """
+    su = spatial_unit(**kwargs)
+    sql, cols = su.geo_augment(su)
+    cq = CustomQuery(sql, cols)
+    assert cq.head(0).columns.tolist() == cols
+
+
+@pytest.mark.parametrize(
+    "spatial_unit", [VersionedCellSpatialUnit, VersionedSiteSpatialUnit]
+)
+@pytest.mark.parametrize("return_geometry", [True, False])
+def test_distance_matrix_columns(spatial_unit, return_geometry):
+    """
+    Test that the columns returned by the distance_matrix_columns method match
+    the columns of the distance_matrix_query.
+    """
+    su = spatial_unit()
+    sql = su.distance_matrix_query(return_geometry=return_geometry)
+    cols = su.distance_matrix_columns(return_geometry=return_geometry)
+    cq = CustomQuery(sql, cols)
+    assert cq.head(0).columns.tolist() == cols
