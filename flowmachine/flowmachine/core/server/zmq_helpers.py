53d1fa626cb095a3ac38e908029d27da0a909de8
Maximilian Albert <maximilian.albert@flowminder.org>
2019-03-25 12:20:18 +0000

    Only allow success/error as valid zmq reply status

--- flowmachine/flowmachine/core/server/zmq_helpers.py
+++ flowmachine/flowmachine/core/server/zmq_helpers.py
@@ -22,8 +22,7 @@ class ZMQReplyStatus(str, Enum):
     Valid status values for a zmq reply.
     """
 
-    ACCEPTED = "accepted"
-    DONE = "done"
+    SUCCESS = "success"
     ERROR = "error"
 
 
