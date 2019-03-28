388ddb75c18f5b01ace98d1c4275377290fb8266
Jono Gray <jonathan.gray@flowminder.org>
2019-03-28 12:18:48 +0000

    Add redoc, and a redoc friendly api spec

--- flowapi/flowapi/api_spec.py
+++ flowapi/flowapi/api_spec.py
@@ -2,19 +2,60 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from quart import Blueprint, current_app, request, stream_with_context, jsonify
+from quart import (
+    Blueprint,
+    current_app,
+    request,
+    stream_with_context,
+    jsonify,
+    render_template,
+)
+from zmq.asyncio import Socket
 
 from ._version import get_versions
 
 blueprint = Blueprint("spec", __name__)
 
 
-@blueprint.route("/")
-async def get_api_spec():
-    msg = {"request_id": request.request_id, "action": "get_query_schemas"}
-    request.socket.send_json(msg)
+def remove_discriminators(spec: dict) -> dict:
+    """
+    Remove any discriminator keys from an openapi spec dict.
+
+    Parameters
+    ----------
+    spec : dict
+        Dict version of an openapi spec
+
+    Returns
+    -------
+    dict
+        The same spec, but with all discriminator keys removed.
+    """
+    newdict = {}
+    for k, v in spec.items():
+        if k != "discriminator":
+            newdict[k] = remove_discriminators(v) if isinstance(v, dict) else v
+    return newdict
+
+
+async def get_spec(socket: Socket, request_id: str) -> dict:
+    """
+    Construct open api spec by interrogating FlowMachine.
+
+    Parameters
+    ----------
+    socket : Socket
+    request_id : str
+        Unique id of the request
+
+    Returns
+    -------
+
+    """
+    msg = {"request_id": request_id, "action": "get_query_schemas"}
+    socket.send_json(msg)
     #  Get the reply.
-    reply = await request.socket.recv_json()
+    reply = await socket.recv_json()
     schema = {
         "openapi": "3.0.1",
         "info": {
@@ -35,9 +76,9 @@ async def get_api_spec():
                     "requestBody": {
                         "content": {
                             "application/json": {
-                                "schema": {
-                                    "$ref": "#/components/schemas/FlowmachineQuerySchema"
-                                }
+                                "schema": reply["payload"]["query_schemas"][
+                                    "FlowmachineQuerySchema"
+                                ]
                             }
                         },
                         "required": True,
@@ -66,4 +107,21 @@ async def get_api_spec():
         },
         "components": {"schemas": reply["payload"]["query_schemas"]},
     }
-    return jsonify(schema)
+    return schema
+
+
+@blueprint.route("/openapi.json")
+async def get_api_spec():
+    return jsonify(await get_spec(request.socket, request.request_id))
+
+
+@blueprint.route("/openapi-redoc.json")
+async def get_redoc_api_spec():
+    return jsonify(
+        remove_discriminators(await get_spec(request.socket, request.request_id))
+    )
+
+
+@blueprint.route("/redoc")
+async def redoc_api_spec():
+    return await render_template("spec.html", api_version=get_versions()["version"])
