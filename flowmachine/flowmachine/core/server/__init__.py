64803ddfd69bc1d5c524a4e1ece74419cb8f1445
Jono Gray <jonathan.gray@flowminder.org>
2019-03-29 00:31:54 +0000

    Fixup

--- flowmachine/flowmachine/core/server/__init__.py
+++ flowmachine/flowmachine/core/server/__init__.py
@@ -3,14 +3,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import logging
+import sys
+
 import structlog
 
 __all__ = ["query_run_log"]
 
 # Logger for all queries run or accessed
-query_run_log = logging.getLogger("flowmachine-server")
-query_run_log.setLevel(logging.INFO)
-ch = logging.StreamHandler()
+query_run_log = logging.getLogger("flowmachine").getChild("query_run_log")
+ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.INFO)
 query_run_log.addHandler(ch)
 query_run_log = structlog.wrap_logger(query_run_log)
