c7e50963d508611854e3502fb472d1c201dddc75
Maximilian Albert <maximilian.albert@flowminder.org>
2019-03-29 16:13:42 +0000

    Add test for event_types=None

--- integration_tests/tests/flowmachine_tests/test_available_dates.py
+++ integration_tests/tests/flowmachine_tests/test_available_dates.py
@@ -71,6 +71,21 @@ from flowmachine.core.available_dates import AvailableDates
     ],
 )
 def test_available_dates(event_types, expected_result, get_dataframe):
+    """
+    Test that the result contains the expected available dates for the given event types.
+    """
     q = AvailableDates(event_types=event_types)
     result = json.loads(get_dataframe(q).to_json(orient="records"))
     assert expected_result == result
+
+
+def test_available_dates_without_explicit_event_types(get_dataframe):
+    """
+    Test that without an explicit list of event types all supported ones are used.
+    """
+    q = AvailableDates(event_types=None)
+    result = json.loads(get_dataframe(q).to_json(orient="records"))
+    result_event_types = sorted([item["event_type"] for item in result])
+    # TODO: should probably import the list of supported event types
+    # from flowmachine rather than hard-coding it here?
+    assert ["calls", "mds", "sms", "topups"] == result_event_types
