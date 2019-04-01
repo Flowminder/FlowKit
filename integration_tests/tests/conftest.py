c801991f2a7a75689c893fc42c3622513b45e26d
Jono Gray <jonathan.gray@flowminder.org>
2019-04-01 12:17:24 +0100

    Fix some glitches from another commit that snuck in

--- integration_tests/tests/conftest.py
+++ integration_tests/tests/conftest.py
@@ -22,17 +22,15 @@ import quart.flask_patch
 
 
 @pytest.fixture(scope="session")
-def logging_config(tmpdir_factory):
+def logging_config():
     """
     Fixture which configures logging for flowmachine and flowapi.
-    Creates a temporary directory for log files to be written to, and sets the log level to debug.
+    Sets the log level to debug.
     """
-    tmpdir = tmpdir_factory.mktemp("logs")
     from _pytest.monkeypatch import MonkeyPatch
 
     mpatch = MonkeyPatch()
     mpatch.setenv("LOG_LEVEL", "debug")
-    print(f"Logs will be written to {tmpdir}")
     yield
     mpatch.undo()
 
