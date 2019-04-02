81ed094540027813bffc83ce30fa5805fc909cf0
Jono Gray <jonathan.gray@flowminder.org>
2019-04-02 14:00:08 +0100

    Add query_kind to dfs metric as well

--- flowmachine/setup.py
+++ flowmachine/setup.py
@@ -72,7 +72,7 @@ setup(
         "SQLAlchemy",
         "cachetools",
         "apispec-oneofschema",
-        "marshmallow==3.0.0rc4",  # TODO: unpin
+        "marshmallow>=3.0.0rc5",  # TODO: unpin
         "marshmallow-oneofschema==2.0.0b2",  # TODO: unpin
         "numpy",
         "networkx",
