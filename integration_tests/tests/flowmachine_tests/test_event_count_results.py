ed7e4c05c8aa8abb6f39ad8b396fd56f764981b3
Maximilian Albert <maximilian.albert@flowminder.org>
2019-04-04 15:39:18 +0100

    Add a few more reference tests

--- integration_tests/tests/flowmachine_tests/test_event_count_results.py
+++ integration_tests/tests/flowmachine_tests/test_event_count_results.py
@@ -11,17 +11,53 @@ from flowmachine.features import EventCount
 
 def test_event_count_1_sql(diff_reporter):
     """
-    Event count query with non-default parameters returns the expected data.
+    Event count query with explicit start and end date returns the expected sql string.
     """
-    ec = EventCount(start="2016-01-02", stop="2016-01-04", tables=None)
+    ec = EventCount(start="2016-01-02", stop="2016-01-04")
     sql = pretty_sql(ec.get_query())
     verify(sql, diff_reporter)
 
 
 def test_event_count_1_df(get_dataframe, diff_reporter):
     """
-    Event count query with non-default parameters returns the expected data.
+    Event count query with explicit start and end date returns the expected data.
     """
-    ec = EventCount(start="2016-01-02", stop="2016-01-04", tables=None)
+    ec = EventCount(start="2016-01-02", stop="2016-01-04")
+    df = get_dataframe(ec)
+    verify(df.to_csv(), diff_reporter)
+
+
+def test_event_count_2_sql(diff_reporter):
+    """
+    Event count query with implicit end date returns the expected sql string.
+    """
+    ec = EventCount(start="2016-01-05", stop=None)
+    sql = pretty_sql(ec.get_query())
+    verify(sql, diff_reporter)
+
+
+def test_event_count_2_df(get_dataframe, diff_reporter):
+    """
+    Event count query with implicit end date returns the expected data.
+    """
+    ec = EventCount(start="2016-01-05", stop=None)
+    df = get_dataframe(ec)
+    verify(df.to_csv(), diff_reporter)
+
+
+def test_event_count_3_sql(diff_reporter):
+    """
+    Event count query with implicit start date returns the expected sql string.
+    """
+    ec = EventCount(start=None, stop="2016-01-02")
+    sql = pretty_sql(ec.get_query())
+    verify(sql, diff_reporter)
+
+
+def test_event_count_3_df(get_dataframe, diff_reporter):
+    """
+    Event count query with implicit start date returns the expected data.
+    """
+    ec = EventCount(start=None, stop="2016-01-02")
     df = get_dataframe(ec)
     verify(df.to_csv(), diff_reporter)
