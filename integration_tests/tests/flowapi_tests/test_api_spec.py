a9bd597ce6eac80a48eefc6bd298ca5bf31e3583
Jono Gray <jonathan.gray@flowminder.org>
2019-04-15 12:01:44 +0100

    Sort spec

--- integration_tests/tests/flowapi_tests/test_api_spec.py
+++ integration_tests/tests/flowapi_tests/test_api_spec.py
@@ -1,5 +1,5 @@
 import json
-
+from flowmachine.utils import sort_recursively
 import requests
 from approvaltests import verify
 
@@ -9,5 +9,5 @@ def test_generated_openapi_spec(flowapi_url, diff_reporter):
     Verify the OpenAPI spec for FlowAPI.
     """
     spec = requests.get(f"{flowapi_url}/api/0/spec/openapi-redoc.json").json()
-    spec_as_json_string = json.dumps(spec, indent=2)
+    spec_as_json_string = json.dumps(sort_recursively(spec), indent=2, sort_keys=True)
     verify(spec_as_json_string, diff_reporter)
