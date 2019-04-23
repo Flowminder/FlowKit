357730c7d2501208dd74be1572a1563ca894fa3e
Maximilian Albert <maximilian.albert@flowminder.org>
2019-04-23 14:53:03 +0100

    Add more integration tests for restricted hours

--- integration_tests/tests/flowmachine_tests/test_event_count_results.py
+++ integration_tests/tests/flowmachine_tests/test_event_count_results.py
@@ -61,3 +61,39 @@ def test_event_count_3_df(get_dataframe, diff_reporter):
     ec = EventCount(start=None, stop="2016-01-02")
     df = get_dataframe(ec)
     verify(df.to_csv(), diff_reporter)
+
+
+def test_event_count_4_sql(diff_reporter):
+    """
+    Event count query with restricted hours returns the expected sql string.
+    """
+    ec = EventCount(start="2016-01-02", stop="2016-01-04", hours=(12, 18))
+    sql = pretty_sql(ec.get_query())
+    verify(sql, diff_reporter)
+
+
+def test_event_count_4_df(get_dataframe, diff_reporter):
+    """
+    Event count query with restricted hours returns the expected data.
+    """
+    ec = EventCount(start="2016-01-02", stop="2016-01-04", hours=(12, 18))
+    df = get_dataframe(ec)
+    verify(df.to_csv(), diff_reporter)
+
+
+def test_event_count_5_sql(diff_reporter):
+    """
+    Event count query with hours spanning midnight returns the expected sql string.
+    """
+    ec = EventCount(start="2016-01-03", stop="2016-01-06", hours=(22, 4))
+    sql = pretty_sql(ec.get_query())
+    verify(sql, diff_reporter)
+
+
+def test_event_count_5_df(get_dataframe, diff_reporter):
+    """
+    Event count query with hours spanning modnight returns the expected data.
+    """
+    ec = EventCount(start="2016-01-03", stop="2016-01-06", hours=(22, 4))
+    df = get_dataframe(ec)
+    verify(df.to_csv(), diff_reporter)
