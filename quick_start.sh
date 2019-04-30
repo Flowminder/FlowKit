09b91aeadc64fa27985e7062731f120f7be69868
Jono Gray <jonathan.gray@flowminder.org>
2019-04-30 18:02:54 +0100

    Check flowmachine using netcat

--- quick_start.sh
+++ quick_start.sh
@@ -19,6 +19,8 @@ else
     echo "Waiting for containers to be ready.."
     docker exec ${DOCKER_FLOWDB_HOST} bash -c 'i=0; until [ $i -ge 24 ] || (pg_isready -h 127.0.0.1 -p 5432); do let i=i+1; echo Waiting 10s; sleep 10; done'
     curl -s http://localhost:$FLOWAPI_PORT/api/0/spec/openapi-redoc.json > /dev/null || (>&2 echo "FlowAPI failed to start :( Please open an issue at https://github.com/Flowminder/FlowKit/issues/new?template=bug_report.md&labels=FlowAPI,bug including:" && docker logs flowapi && exit 1)
+    curl -s http://localhost:$FLOWAUTH_PORT > /dev/null || (>&2 echo "FlowAuth failed to start :( Please open an issue at https://github.com/Flowminder/FlowKit/issues/new?template=bug_report.md&labels=FlowAuth,bug including:" && docker logs flowauth && exit 1)
+    nc -z localhost $FLOWMACHINE_PORT > /dev/null || (>&2 echo "FlowMachine failed to start :( Please open an issue at https://github.com/Flowminder/FlowKit/issues/new?template=bug_report.md&labels=FlowMachine,bug including:" && docker logs flowmachine && exit 1)
     echo "Containers ready!"
     echo "Access FlowDB using 'PGHOST=$FLOWDB_HOST PGPORT=$FLOWDB_PORT PGDATABASE=flowdb PGUSER=$FLOWMACHINE_FLOWDB_USER PGPASSWORD=$FLOWMACHINE_FLOWDB_PASSWORD psql'"
     echo "Access FlowAPI using FlowClient at http://localhost:$FLOWAPI_PORT"
