de17d70775811944fc85a611eab32e2d2c123d4a
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-09 17:41:52 +0200

    add doc

--- flowetl/tests/integration/test_dags_present.py
+++ flowetl/tests/integration/test_dags_present.py
@@ -2,5 +2,8 @@ from airflow.models import DagBag
 
 
 def test_dags_present(airflow_dagbag):
+    """
+    Test that the correct dags are parsed
+    """
     dags = airflow_dagbag.dags
     assert set(airflow_dagbag.dag_ids) == set(["etl", "etl_sensor"])
