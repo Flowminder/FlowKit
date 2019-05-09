29b11e2ea8bf0712e3f394db893b621fbe1558ce
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-09 15:57:01 +0200

    update fixtures

--- flowetl/tests/integration/conftest.py
+++ flowetl/tests/integration/conftest.py
@@ -1,24 +1,34 @@
 """
 Conftest for flowetl integration tests
 """
+import os
 
 import pytest
 import docker
-import os
 
 
 @pytest.fixture(scope="session")
 def docker_client():
+    """
+    docker client object
+    """
     return docker.from_env()
 
 
 @pytest.fixture(scope="session")
 def flowetl_tag():
+    """
+    Get flowetl tag to use
+    """
     return os.environ.get("FLOWETL_TAG", "latest")
 
 
 @pytest.fixture(scope="module")
 def flowetl_solo_longrunning(docker_client, flowetl_tag):
+    """
+    Fixture that starts a running flowetl container and
+    yeilds the container object.
+    """
     container = docker_client.containers.run(
         f"flowminder/flowetl:{flowetl_tag}", detach=True
     )
@@ -29,6 +39,11 @@ def flowetl_solo_longrunning(docker_client, flowetl_tag):
 
 @pytest.fixture(scope="function")
 def flowetl_run_command(docker_client, flowetl_tag):
+    """
+    Fixture that returns a function for running arbitrary
+    command on flowetl container.
+    """
+
     def run_commmand(command, **kwargs):
         out = docker_client.containers.run(
             f"flowminder/flowetl:{flowetl_tag}", command, **kwargs
