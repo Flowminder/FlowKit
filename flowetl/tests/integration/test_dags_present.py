3b49e108346a4e39e4e068fd8b2bc5f2240fdc76
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-09 18:57:05 +0200

    add test to check tasks

--- flowetl/tests/integration/test_dags_present.py
+++ flowetl/tests/integration/test_dags_present.py
@@ -1,4 +1,4 @@
-from airflow.models import DagBag
+import pytest
 
 
 def test_dags_present(airflow_dagbag):
@@ -7,3 +7,30 @@ def test_dags_present(airflow_dagbag):
     """
     dags = airflow_dagbag.dags
     assert set(airflow_dagbag.dag_ids) == set(["etl", "etl_sensor"])
+
+
+@pytest.mark.parametrize(
+    "dag_name,expected_task_list",
+    [
+        (
+            "etl",
+            [
+                "init",
+                "extract",
+                "transform",
+                "success_branch",
+                "load",
+                "archive",
+                "quarantine",
+                "clean",
+                "fail",
+            ],
+        ),
+        ("etl_sensor", ["sense"]),
+    ],
+)
+def test_correct_tasks(airflow_dagbag, dag_name, expected_task_list):
+
+    dag = airflow_dagbag.dags[dag_name]
+    assert False
+    assert set(dag.task_ids) == set(expected_task_list)
