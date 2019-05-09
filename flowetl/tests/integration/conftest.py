9dcb8a7c757f136f0fba7795ab2956eb8d35227d
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-09 22:46:55 +0200

    local dag testing setup

--- flowetl/tests/integration/conftest.py
+++ flowetl/tests/integration/conftest.py
@@ -5,7 +5,7 @@ import os
 import shutil
 
 from time import sleep
-
+from subprocess import DEVNULL, Popen
 import pytest
 import docker
 
@@ -58,9 +58,44 @@ def flowetl_run_command(docker_client, flowetl_tag):
     return run_commmand
 
 
-@pytest.fixture(scope="module")
-def airflow_dagbag():
-    from airflow.models import DagBag
+# @pytest.fixture(scope="module")
+# def airflow_dagbag():
+#     from airflow.models import DagBag
 
-    yield DagBag("./dags", include_examples=False)
-    shutil.rmtree(os.environ["AIRFLOW_HOME"])
+#     yield DagBag("./dags", include_examples=False)
+#     shutil.rmtree(os.environ["AIRFLOW_HOME"])
+
+
+@pytest.fixture(scope="function")
+def airflow_local_setup():
+    extra_env = {
+        "AIRFLOW__CORE__DAGS_FOLDER": "./dags",
+        "AIRFLOW__CORE__LOAD_EXAMPLES": "false",
+    }
+    env = {**os.environ, **extra_env}
+    initdb = Popen(
+        ["airflow", "initdb"], shell=False, stdout=DEVNULL, stderr=DEVNULL, env=env
+    )
+    initdb.wait()
+
+    with open("scheduler.log", "w") as f:
+        scheduler = Popen(
+            ["airflow", "scheduler"], shell=False, stdout=f, stderr=f, env=env
+        )
+
+    sleep(2)
+
+    with open("webserver.log", "w") as f:
+        webserver = Popen(
+            ["airflow", "webserver"], shell=False, stdout=f, stderr=f, env=env
+        )
+
+    sleep(2)
+    # yeilding a lambda that allows for subprocess calls with the correct env
+    yield lambda cmd, **kwargs: Popen(cmd.split(), env=env, **kwargs)
+    scheduler.terminate()
+    webserver.terminate()
+
+    shutil.rmtree(env["AIRFLOW_HOME"])
+    os.unlink("./scheduler.log")
+    os.unlink("./webserver.log")
---
+++ flowetl/tests/integration/test_dag_run.py
@@ -0,0 +1,37 @@
+import os
+from time import sleep
+
+from subprocess import DEVNULL, STDOUT, Popen
+
+from airflow.models import DagRun
+
+
+def test_foo(airflow_local_setup):
+
+    p = airflow_local_setup("airflow unpause etl_sensor", shell=False)
+    p.wait()
+    p = airflow_local_setup("airflow unpause etl", shell=False)
+    p.wait()
+    p = airflow_local_setup("airflow trigger_dag etl_sensor", shell=False)
+    p.wait()
+
+    while not DagRun.find("etl", state="success"):
+        sleep(1)
+
+    assert True
+
+
+def test_bar(airflow_local_setup):
+
+    p = airflow_local_setup("airflow unpause etl_sensor", shell=False)
+    p.wait()
+    p = airflow_local_setup("airflow unpause etl", shell=False)
+    p.wait()
+    p = airflow_local_setup("airflow trigger_dag etl_sensor", shell=False)
+    p.wait()
+
+    while not DagRun.find("etl", state="success"):
+        sleep(1)
+
+    assert True
+
