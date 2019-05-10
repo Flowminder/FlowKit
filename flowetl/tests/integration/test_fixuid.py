02dcf4f476307de32604e2fa3e8f4e33e46c8ac8
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-10 12:22:29 +0200

    update the fixuid test

--- flowetl/tests/integration/test_fixuid.py
+++ flowetl/tests/integration/test_fixuid.py
@@ -1,21 +1,25 @@
 import pytest
 
 
-def test_uid(flowetl_run_command):
+def test_uid(flowetl_container):
     """
     test that we can run the flowetl container with a specific user
     """
 
     user = "1002:1003"
-    out = flowetl_run_command("bash -c 'id -u'", user=user)
+    out = flowetl_container.exec_run("bash -c 'id -u'", user=user).output.decode(
+        "utf-8"
+    )
     assert out.strip() == "1002"
 
 
-def test_gid(flowetl_run_command):
+def test_gid(flowetl_container):
     """
     test that we can run the flowetl container with a specific user
     """
 
     user = "1002:1003"
-    out = flowetl_run_command("bash -c 'id -g'", user=user)
+    out = flowetl_container.exec_run("bash -c 'id -g'", user=user).output.decode(
+        "utf-8"
+    )
     assert out.strip() == "1003"
