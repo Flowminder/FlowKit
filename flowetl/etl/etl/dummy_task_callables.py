5da9609df7cec093bbdde0160737d49f86e4a2e7
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-13 14:53:40 +0200

    add dummy callables

---
+++ flowetl/etl/etl/dummy_task_callables.py
@@ -0,0 +1,32 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# -*- coding: utf-8 -*-
+"""
+Contains the definition of dummy callables to be used when testing
+"""
+
+import os
+import logging
+
+from airflow.models import DagRun, TaskInstance
+
+# pylint: disable=unused-argument
+def dummy_callable(*, dag_run: DagRun, task_instance: TaskInstance, **kwargs):
+    """
+    Dummy python callable - raises an exception if the environment variable
+    TASK_TO_FAIL is set to the name of the current task, otherwise succeeds
+    silently.
+    """
+    logging.info(dag_run)
+    if os.environ["TASK_TO_FAIL"] == task_instance.task_id:
+        raise Exception
+
+
+def dummy_failing_callable(*, dag_run: DagRun, **kwargs):
+    """
+    Dummy python callable raising an exception
+    """
+    logging.info(dag_run)
+    raise Exception
