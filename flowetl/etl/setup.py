57e10c21f17a62864de6ad30112d79624ce364a8
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-13 14:51:27 +0200

    change name of module to etl

---
+++ flowetl/etl/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="etl",
+    url="https://github.com/Flowminder/FlowKit",
+    maintainer="Flowminder",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["apache-airflow==1.10.3"],
+)
