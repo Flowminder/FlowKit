40805785486685087d62ed3366c760d29cc1dc64
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-13 17:08:55 +0200

    remove use of Mock

--- flowetl/etl/tests/test_dummy_callables.py
+++ flowetl/etl/tests/test_dummy_callables.py
@@ -9,15 +9,19 @@ from unittest.mock import patch, MagicMock, PropertyMock
 from etl.dummy_task_callables import dummy_callable, dummy_failing_callable
 
 
+class DummyTaskInstance:
+    def __init__(self, task_id):
+        self.task_id = task_id
+
+
 def test_dummy_callable_succeeds_with_no_TASK_TO_FAIL_env_var_set():
     """
     Test that when no TASK_TO_FAIL env var set the dummy callable
     succeeds.
     """
     dag_run = {}
-    task_instance = MagicMock()
-    task_id_mock = PropertyMock(return_value="init")
-    type(task_instance).task_id = task_id_mock
+    task_id = "init"
+    task_instance = DummyTaskInstance(task_id=task_id)
 
     dummy_callable(dag_run=dag_run, task_instance=task_instance)
 
@@ -27,12 +31,10 @@ def test_dummy_callable_succeeds_when_TASK_TO_FAIL_env_var_is_not_same_as_task_i
     Test that when TASK_TO_FAIL env var is different from the task id the dummy
     callable succeeds.
     """
+    dag_run = {}
     TASK_TO_FAIL = "init"
     task_id = "extract"
-    dag_run = {}
-    task_instance = MagicMock()
-    task_id_mock = PropertyMock(return_value=task_id)
-    type(task_instance).task_id = task_id_mock
+    task_instance = DummyTaskInstance(task_id=task_id)
 
     with patch("os.environ", {"TASK_TO_FAIL": TASK_TO_FAIL}):
         dummy_callable(dag_run=dag_run, task_instance=task_instance)
@@ -43,11 +45,10 @@ def test_dummy_callable_fails_when_TASK_TO_FAIL_env_var_is_same_as_task_id():
     Test that if TASK_TO_FAIL is same as task id the dummy_callable raises
     an exception.
     """
-    TASK_TO_FAIL = "init"
     dag_run = {}
-    task_instance = MagicMock()
-    task_id_mock = PropertyMock(return_value=TASK_TO_FAIL)
-    type(task_instance).task_id = task_id_mock
+    TASK_TO_FAIL = "init"
+    task_id = TASK_TO_FAIL
+    task_instance = DummyTaskInstance(task_id=task_id)
 
     with patch("os.environ", {"TASK_TO_FAIL": TASK_TO_FAIL}):
         with pytest.raises(Exception):
