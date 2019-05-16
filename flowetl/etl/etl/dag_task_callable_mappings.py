44859ba4effc9f572f0f083d063516da8ac3ac40
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-16 12:38:55 +0200

    use of callables

--- flowetl/etl/etl/dag_task_callable_mappings.py
+++ flowetl/etl/etl/dag_task_callable_mappings.py
@@ -7,8 +7,17 @@
 Mapping task id to a python callable. Allows for the specification of a set of
 dummy callables to be used for testing.
 """
+from functools import partial
+from pathlib import Path
+
+from airflow.hooks.postgres_hook import PostgresHook
+
 from etl.dummy_task_callables import dummy__callable, dummy_failing__callable
-from etl.production_task_callables import success_branch__callable
+from etl.production_task_callables import (
+    move_file_and_record_ingestion_state__callable,
+    render_and_run_sql__callable,
+    success_branch__callable,
+)
 
 # callables to be used when testing the structure of the ETL DAG
 TEST_TASK_CALLABLES = {
@@ -23,15 +32,61 @@ TEST_TASK_CALLABLES = {
     "fail": dummy_failing__callable,
 }
 
+# All these are dummy values for now!
+mount_paths = {
+    "dump": "/dump",
+    "ingest": "/ingest",
+    "archive": "/archive",
+    "quarantine": "/quarantine",
+}
+
+db_hook = PostgresHook(postgres_conn_id="flowdb")
+config_path = Path("/config")
+
 # callables to be used in production
 PRODUCTION_TASK_CALLABLES = {
-    "init": dummy__callable,
-    "extract": dummy__callable,
-    "transform": dummy__callable,
-    "load": dummy__callable,
+    "init": partial(
+        move_file_and_record_ingestion_state__callable,
+        mount_paths=mount_paths,
+        from_dir="dump",
+        to_dir="ingest",
+    ),
+    "extract": partial(
+        render_and_run_sql__callable,
+        db_hook=db_hook,
+        config_path=config_path,
+        template_name="extract",
+    ),
+    "transform": partial(
+        render_and_run_sql__callable,
+        db_hook=db_hook,
+        config_path=config_path,
+        template_name="transform",
+    ),
+    "load": partial(
+        render_and_run_sql__callable,
+        db_hook=db_hook,
+        config_path=config_path,
+        template_name="load",
+    ),
     "success_branch": success_branch__callable,
-    "archive": dummy__callable,
-    "quarantine": dummy__callable,
-    "clean": dummy__callable,
+    "archive": partial(
+        move_file_and_record_ingestion_state__callable,
+        mount_paths=mount_paths,
+        from_dir="ingest",
+        to_dir="archive",
+    ),
+    "quarantine": partial(
+        move_file_and_record_ingestion_state__callable,
+        mount_paths=mount_paths,
+        from_dir="ingest",
+        to_dir="quarantine",
+    ),
+    "clean": partial(
+        render_and_run_sql__callable,
+        db_hook=db_hook,
+        config_path=config_path,
+        template_name="clean",
+    ),
     "fail": dummy_failing__callable,
 }
