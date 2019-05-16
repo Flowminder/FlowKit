849c80e943fe8b7768779bbad72d0d924e321b5d
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-16 17:23:48 +0200

    when not testing validate config and create a DAG for each CDR type

--- flowetl/dags/etl.py
+++ flowetl/dags/etl.py
@@ -9,6 +9,8 @@ Skeleton specification for ETL DAG
 import logging
 import os
 
+from pathlib import Path
+
 # need to import and not use so that airflow looks here for a DAG
 from airflow import DAG  # pylint: disable=unused-import
 
@@ -17,7 +19,8 @@ from etl.dag_task_callable_mappings import (
     TEST_TASK_CALLABLES,
     PRODUCTION_TASK_CALLABLES,
 )
-from etl.etl_utils import construct_etl_dag
+from etl.etl_utils import construct_etl_dag, CDRType
+from etl.config_parser import validate_config, get_config_from_file
 
 default_args = {"owner": "flowminder", "start_date": parse("1900-01-01")}
 
@@ -25,10 +28,23 @@ default_args = {"owner": "flowminder", "start_date": parse("1900-01-01")}
 if os.environ.get("TESTING", "") == "true":
     task_callable_mapping = TEST_TASK_CALLABLES
     logging.info("running in testing environment")
+    dag = construct_etl_dag(
+        task_callable_mapping=task_callable_mapping,
+        default_args=default_args,
+        cdr_type="testing",
+    )
 else:
     task_callable_mapping = PRODUCTION_TASK_CALLABLES
     logging.info("running in production environment")
 
-dag = construct_etl_dag(
-    task_callable_mapping=task_callable_mapping, default_args=default_args
-)
+    global_config_dict = get_config_from_file(
+        config_filepath=os.environ["MOUNT_HOME"] / Path("config/config.yml")
+    )
+    validate_config(global_config_dict=global_config_dict)
+
+    for cdr_type in list(CDRType._value2member_map_.keys()):
+        globals()[f"etl_{cdr_type}"] = construct_etl_dag(
+            task_callable_mapping=task_callable_mapping,
+            default_args=default_args,
+            cdr_type=cdr_type,
+        )
