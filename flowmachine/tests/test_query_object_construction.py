032f6972cec21c859b97c72b5dde6468e662806e
Maximilian Albert <maximilian.albert@flowminder.org>
2019-05-16 10:43:23 +0100

    Turn test into approvaltest, to avoid manual editing when query_ids change

--- flowmachine/tests/test_query_object_construction.py
+++ flowmachine/tests/test_query_object_construction.py
@@ -2,268 +2,254 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import json
 import pytest
+from approvaltests.approvals import verify
 from marshmallow import ValidationError
 
 from flowmachine.core.server.query_schemas import FlowmachineQuerySchema
 
 
-@pytest.mark.parametrize(
-    "expected_md5, query_spec",
-    [
-        (
-            "007f984a39f97f26684116efcf40b8f7",
-            {
-                "query_kind": "spatial_aggregate",
-                "locations": {
-                    "query_kind": "daily_location",
-                    "date": "2016-01-01",
-                    "aggregation_unit": "admin3",
-                    "method": "last",
-                    "subscriber_subset": None,
-                },
-            },
-        ),
-        (
-            "fe104962050bf4d6c70c545bb97aa9f7",
-            {
-                "query_kind": "location_event_counts",
-                "start_date": "2016-01-01",
-                "end_date": "2016-01-02",
-                "interval": "day",
+def test_construct_query(diff_reporter):
+    """
+    Test that query objects constructed by construct_query_object() have the expected query ids.
+    """
+    query_specs_to_test = [
+        {
+            "query_kind": "spatial_aggregate",
+            "locations": {
+                "query_kind": "daily_location",
+                "date": "2016-01-01",
                 "aggregation_unit": "admin3",
-                "direction": "both",
-                "event_types": None,
+                "method": "last",
                 "subscriber_subset": None,
             },
-        ),
-        (
-            "cde391fae9e861c0f1666e8f9cc6a5c5",
-            {
-                "query_kind": "spatial_aggregate",
-                "locations": {
-                    "query_kind": "modal_location",
-                    "aggregation_unit": "admin3",
-                    "locations": (
-                        {
-                            "query_kind": "daily_location",
-                            "date": "2016-01-01",
-                            "aggregation_unit": "admin3",
-                            "method": "last",
-                            "subscriber_subset": None,
-                        },
-                        {
-                            "query_kind": "daily_location",
-                            "date": "2016-01-02",
-                            "aggregation_unit": "admin3",
-                            "method": "last",
-                            "subscriber_subset": None,
-                        },
-                    ),
+        },
+        {
+            "query_kind": "location_event_counts",
+            "start_date": "2016-01-01",
+            "end_date": "2016-01-02",
+            "interval": "day",
+            "aggregation_unit": "admin3",
+            "direction": "both",
+            "event_types": None,
+            "subscriber_subset": None,
+        },
+        {
+            "query_kind": "spatial_aggregate",
+            "locations": {
+                "query_kind": "modal_location",
+                "aggregation_unit": "admin3",
+                "locations": (
+                    {
+                        "query_kind": "daily_location",
+                        "date": "2016-01-01",
+                        "aggregation_unit": "admin3",
+                        "method": "last",
+                        "subscriber_subset": None,
+                    },
+                    {
+                        "query_kind": "daily_location",
+                        "date": "2016-01-02",
+                        "aggregation_unit": "admin3",
+                        "method": "last",
+                        "subscriber_subset": None,
+                    },
+                ),
+            },
+        },
+        {"query_kind": "geography", "aggregation_unit": "admin3"},
+        {
+            "query_kind": "meaningful_locations_aggregate",
+            "aggregation_unit": "admin1",
+            "start_date": "2016-01-01",
+            "end_date": "2016-01-02",
+            "label": "unknown",
+            "labels": {
+                "evening": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [[1e-06, -0.5], [1e-06, -1.1], [1.1, -1.1], [1.1, -0.5]]
+                    ],
+                },
+                "day": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [[-1.1, -0.5], [-1.1, 0.5], [-1e-06, 0.5], [0, -0.5]]
+                    ],
                 },
             },
-        ),
-        (
-            "4e8eec45e2c4d396dec9f65dc1b780bd",
-            {"query_kind": "geography", "aggregation_unit": "admin3"},
-        ),
-        (
-            "f698fa2a4cf75254ffdac05cdd4c5377",
-            {
-                "query_kind": "meaningful_locations_aggregate",
-                "aggregation_unit": "admin1",
-                "start_date": "2016-01-01",
-                "end_date": "2016-01-02",
-                "label": "unknown",
-                "labels": {
-                    "evening": {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [[1e-06, -0.5], [1e-06, -1.1], [1.1, -1.1], [1.1, -0.5]]
-                        ],
-                    },
-                    "day": {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [[-1.1, -0.5], [-1.1, 0.5], [-1e-06, 0.5], [0, -0.5]]
-                        ],
-                    },
-                },
-                "tower_hour_of_day_scores": [
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    0,
-                    0,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    0,
-                    0,
-                    0,
-                    0,
-                    -1,
-                    -1,
-                    -1,
-                ],
-                "tower_day_of_week_scores": {
-                    "monday": 1,
-                    "tuesday": 1,
-                    "wednesday": 1,
-                    "thursday": 0,
-                    "friday": -1,
-                    "saturday": -1,
-                    "sunday": -1,
-                },
-                "tower_cluster_radius": 1.0,
-                "tower_cluster_call_threshold": 0,
-                "subscriber_subset": None,
+            "tower_hour_of_day_scores": [
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                0,
+                0,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                0,
+                0,
+                0,
+                0,
+                -1,
+                -1,
+                -1,
+            ],
+            "tower_day_of_week_scores": {
+                "monday": 1,
+                "tuesday": 1,
+                "wednesday": 1,
+                "thursday": 0,
+                "friday": -1,
+                "saturday": -1,
+                "sunday": -1,
             },
-        ),
-        (
-            "785186014a77aa54378cc4ae67203d53",
-            {
-                "query_kind": "meaningful_locations_between_label_od_matrix",
-                "aggregation_unit": "admin1",
-                "start_date": "2016-01-01",
-                "end_date": "2016-01-02",
-                "label_a": "day",
-                "label_b": "evening",
-                "labels": {
-                    "day": {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [[-1.1, -0.5], [-1.1, 0.5], [-1e-06, 0.5], [0, -0.5]]
-                        ],
-                    },
-                    "evening": {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [[1e-06, -0.5], [1e-06, -1.1], [1.1, -1.1], [1.1, -0.5]]
-                        ],
-                    },
+            "tower_cluster_radius": 1.0,
+            "tower_cluster_call_threshold": 0,
+            "subscriber_subset": None,
+        },
+        {
+            "query_kind": "meaningful_locations_between_label_od_matrix",
+            "aggregation_unit": "admin1",
+            "start_date": "2016-01-01",
+            "end_date": "2016-01-02",
+            "label_a": "day",
+            "label_b": "evening",
+            "labels": {
+                "day": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [[-1.1, -0.5], [-1.1, 0.5], [-1e-06, 0.5], [0, -0.5]]
+                    ],
                 },
-                "tower_hour_of_day_scores": [
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    0,
-                    0,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    0,
-                    0,
-                    0,
-                    0,
-                    -1,
-                    -1,
-                    -1,
-                ],
-                "tower_day_of_week_scores": {
-                    "monday": 1,
-                    "tuesday": 1,
-                    "wednesday": 1,
-                    "thursday": 0,
-                    "friday": -1,
-                    "saturday": -1,
-                    "sunday": -1,
+                "evening": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [[1e-06, -0.5], [1e-06, -1.1], [1.1, -1.1], [1.1, -0.5]]
+                    ],
                 },
-                "tower_cluster_radius": 1.0,
-                "tower_cluster_call_threshold": 0,
-                "subscriber_subset": None,
             },
-        ),
-        (
-            "f769334285671b759f18aefb9b58904d",
-            {
-                "query_kind": "meaningful_locations_between_dates_od_matrix",
-                "aggregation_unit": "admin1",
-                "start_date_a": "2016-01-01",
-                "end_date_a": "2016-01-02",
-                "start_date_b": "2016-01-01",
-                "end_date_b": "2016-01-05",
-                "label": "unknown",
-                "labels": {
-                    "day": {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [[-1.1, -0.5], [-1.1, 0.5], [-1e-06, 0.5], [0, -0.5]]
-                        ],
-                    },
-                    "evening": {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [[1e-06, -0.5], [1e-06, -1.1], [1.1, -1.1], [1.1, -0.5]]
-                        ],
-                    },
-                },
-                "tower_hour_of_day_scores": [
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    -1,
-                    0,
-                    0,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    0,
-                    0,
-                    0,
-                    0,
-                    -1,
-                    -1,
-                    -1,
-                ],
-                "tower_day_of_week_scores": {
-                    "monday": 1,
-                    "tuesday": 1,
-                    "wednesday": 1,
-                    "thursday": 0,
-                    "friday": -1,
-                    "saturday": -1,
-                    "sunday": -1,
-                },
-                "tower_cluster_radius": 1.0,
-                "tower_cluster_call_threshold": 2,
-                "subscriber_subset": None,
+            "tower_hour_of_day_scores": [
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                0,
+                0,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                0,
+                0,
+                0,
+                0,
+                -1,
+                -1,
+                -1,
+            ],
+            "tower_day_of_week_scores": {
+                "monday": 1,
+                "tuesday": 1,
+                "wednesday": 1,
+                "thursday": 0,
+                "friday": -1,
+                "saturday": -1,
+                "sunday": -1,
             },
-        ),
-    ],
-)
-def test_construct_query(expected_md5, query_spec):
-    """
-    Test that expected query objects are constructed by construct_query_object
-    """
-    obj = FlowmachineQuerySchema().load(query_spec)
-    assert expected_md5 == obj.query_id
+            "tower_cluster_radius": 1.0,
+            "tower_cluster_call_threshold": 0,
+            "subscriber_subset": None,
+        },
+        {
+            "query_kind": "meaningful_locations_between_dates_od_matrix",
+            "aggregation_unit": "admin1",
+            "start_date_a": "2016-01-01",
+            "end_date_a": "2016-01-02",
+            "start_date_b": "2016-01-01",
+            "end_date_b": "2016-01-05",
+            "label": "unknown",
+            "labels": {
+                "day": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [[-1.1, -0.5], [-1.1, 0.5], [-1e-06, 0.5], [0, -0.5]]
+                    ],
+                },
+                "evening": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [[1e-06, -0.5], [1e-06, -1.1], [1.1, -1.1], [1.1, -0.5]]
+                    ],
+                },
+            },
+            "tower_hour_of_day_scores": [
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                0,
+                0,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                0,
+                0,
+                0,
+                0,
+                -1,
+                -1,
+                -1,
+            ],
+            "tower_day_of_week_scores": {
+                "monday": 1,
+                "tuesday": 1,
+                "wednesday": 1,
+                "thursday": 0,
+                "friday": -1,
+                "saturday": -1,
+                "sunday": -1,
+            },
+            "tower_cluster_radius": 1.0,
+            "tower_cluster_call_threshold": 2,
+            "subscriber_subset": None,
+        },
+    ]
+
+    def get_query_id_for_query_spec(query_spec):
+        return FlowmachineQuerySchema().load(query_spec).query_id
+
+    query_ids_and_specs_as_json_string = json.dumps(
+        {get_query_id_for_query_spec(spec): spec for spec in query_specs_to_test},
+        indent=2,
+    )
+
+    verify(query_ids_and_specs_as_json_string, diff_reporter)
 
 
 # TODO: we should re-think how we want to test invalid values, now that these are validated using marshmallow
