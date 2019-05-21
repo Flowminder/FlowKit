c89b80da781fd48a54d982b73964c53ce325124a
Maximilian Albert <maximilian.albert@flowminder.org>
2019-05-21 18:59:05 +0100

    Fix tests

--- flowapi/tests/unit/test_secrets.py
+++ flowapi/tests/unit/test_secrets.py
@@ -2,26 +2,38 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import unittest
+import pytest
+import unittest.mock
 from pathlib import Path
 
-from flowapi.config import getsecret
+from flowapi.config import get_secret_or_env_var, UndefinedConfigOption
 
 
 def test_get_secrets(monkeypatch):
-    """Test getting a secret from the special /run/secrets directory."""
-    the_secret = "Shhhh"
+    """
+    Test getting a secret from the special /run/secrets directory.
+    """
     the_secret_name = "SECRET"
+    the_secret = "Shhhh"
     open_mock = unittest.mock.mock_open(read_data=the_secret)
     monkeypatch.setattr("builtins.open", open_mock)
-    secret = getsecret(the_secret_name, "Not the secret")
+    secret = get_secret_or_env_var(the_secret_name)
     assert the_secret == secret
     open_mock.assert_called_once_with(Path("/run/secrets") / the_secret_name, "r")
 
 
-def test_get_secrets_default(monkeypatch):
-    """Test getting a secret falls back to provided default with the file being there."""
-    the_secret = "Shhhh"
-    the_secret_name = "SECRET"
-    secret = getsecret(the_secret_name, the_secret)
-    assert the_secret == secret
+def test_get_secrets_with_env_var(monkeypatch):
+    """
+    Test getting a secret falls back to the environment variable if the secret is undefined.
+    """
+    monkeypatch.setenv("SECRET", "Hush")
+    secret = get_secret_or_env_var("SECRET")
+    assert "Hush" == secret
+
+
+def test_get_secrets_raises_error_if_value_is_undefined(monkeypatch):
+    """
+    Test getting a secret raises an error if neither the secret nor the env var is defined.
+    """
+    with pytest.raises(UndefinedConfigOption, match=""):
+        get_secret_or_env_var("SECRET")
