e417515a2a615f3338e720b9137f5749f0509521
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-28 13:16:45 +0200

    add test for dummy trigger callable

--- flowetl/etl/tests/test_dummy_callables.py
+++ flowetl/etl/tests/test_dummy_callables.py
@@ -6,14 +6,20 @@
 """
 Tests that the testing dummy callables behave as expected.
 """
-from unittest.mock import patch
-
 import pytest
+from unittest.mock import patch, Mock
+from uuid import uuid1
+from pendulum import utcnow
+from airflow.exceptions import DagNotFound
 
-from etl.dummy_task_callables import dummy__callable, dummy_failing__callable
+from etl.dummy_task_callables import (
+    dummy__callable,
+    dummy_failing__callable,
+    dummy_trigger__callable,
+)
 
 
-def test_dummy_callable_succeeds_with_no_TASK_TO_FAIL_env_var_set(
+def test_dummy__callable_succeeds_with_no_TASK_TO_FAIL_env_var_set(
     create_fake_task_instance
 ):
     """
@@ -27,7 +33,7 @@ def test_dummy_callable_succeeds_with_no_TASK_TO_FAIL_env_var_set(
     dummy__callable(dag_run=dag_run, task_instance=task_instance)
 
 
-def test_dummy_callable_succeeds_when_TASK_TO_FAIL_env_var_is_not_same_as_task_id(
+def test_dummy__callable_succeeds_when_TASK_TO_FAIL_env_var_is_not_same_as_task_id(
     create_fake_task_instance
 ):
     """
@@ -43,7 +49,7 @@ def test_dummy_callable_succeeds_when_TASK_TO_FAIL_env_var_is_not_same_as_task_i
         dummy__callable(dag_run=dag_run, task_instance=task_instance)
 
 
-def test_dummy_callable_fails_when_TASK_TO_FAIL_env_var_is_same_as_task_id(
+def test_dummy__callable_fails_when_TASK_TO_FAIL_env_var_is_same_as_task_id(
     create_fake_task_instance
 ):
     """
@@ -60,9 +66,34 @@ def test_dummy_callable_fails_when_TASK_TO_FAIL_env_var_is_same_as_task_id(
             dummy__callable(dag_run=dag_run, task_instance=task_instance)
 
 
-def test_dummy_failing_callable():
+def test_dummy_failing__callable():
     """
     Test that the dummy_failing_callable raises an Exception
     """
     with pytest.raises(Exception):
         dummy_failing__callable(dag_run={})
+
+
+def test_dummy_trigger__callable(monkeypatch):
+    """
+    Test that the dummy trigger callable works as expected.
+    """
+    mock_trigger_dag = Mock()
+    monkeypatch.setattr("etl.dummy_task_callables.trigger_dag", mock_trigger_dag)
+
+    uuid = uuid1()
+    monkeypatch.setattr("etl.dummy_task_callables.uuid1", lambda: uuid)
+
+    now = utcnow()
+    monkeypatch.setattr("etl.dummy_task_callables.utcnow", lambda: now)
+
+    try:
+        dummy_trigger__callable(dag_run={})
+    except DagNotFound:
+        # the testing etl dag doesn't exist!
+        pass
+
+    mock_trigger_dag.assert_called_once_with(
+        "etl_testing", run_id=str(uuid), execution_date=now
+    )
+
