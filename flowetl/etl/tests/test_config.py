eb04f2f64369c91303d75c0b0ddad5320af7fd8c
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-28 13:15:48 +0200

    add test for filter files function

--- flowetl/etl/tests/test_config.py
+++ flowetl/etl/tests/test_config.py
@@ -6,19 +6,25 @@
 """
 Tests for configuration parsing
 """
-from builtins import ValueError
+import pytest
+import yaml
+
 from copy import deepcopy
 from pathlib import Path
 from unittest.mock import Mock, patch
 from uuid import uuid1
-
-import pytest
-import yaml
 from pendulum import parse
 
+from etl.model import ETLRecord
 from etl.config_constant import config
-from etl.config_parser import get_cdr_type_config, validate_config
-from etl.etl_utils import CDRType, find_files, generate_table_names, parse_file_name
+from etl.config_parser import get_cdr_type_config, validate_config, get_config_from_file
+from etl.etl_utils import (
+    CDRType,
+    find_files,
+    generate_table_names,
+    parse_file_name,
+    filter_files,
+)
 
 
 def test_get_cdr_type_config_with_valid_type():
@@ -183,3 +189,68 @@ def test_parse_file_name_exception():
     file_name = "bob.csv"
     with pytest.raises(ValueError):
         parse_file_name(file_name=file_name, cdr_type_config=cdr_type_config)
+
+
+def test_filter_files(session, monkeypatch):
+    """
+    testing the filter files function
+
+    - set calls on 20160101 to quarantine so should not be filtered
+    - set calls on 20160102 to archive so should be filtered
+    - no record for sms 20160101 so should not be filtered
+    - bad_file.bad doesn't match any pattern so should  be filtered
+    """
+    cdr_type_config = config["etl"]
+
+    file1 = Path("./CALLS_20160101.csv.gz")
+    file2 = Path("./CALLS_20160102.csv.gz")
+    file3 = Path("./SMS_20160101.csv.gz")
+    file4 = Path("./bad_file.bad")
+
+    found_files = [file1, file2, file3, file4]
+
+    # add a some etl records
+    file1_data = {
+        "cdr_type": "calls",
+        "cdr_date": parse("2016-01-01").date(),
+        "state": "quarantine",
+    }
+
+    file2_data = {
+        "cdr_type": "calls",
+        "cdr_date": parse("2016-01-02").date(),
+        "state": "archive",
+    }
+
+    ETLRecord.set_state(
+        cdr_type=file1_data["cdr_type"],
+        cdr_date=file1_data["cdr_date"],
+        state=file1_data["state"],
+        session=session,
+    )
+
+    ETLRecord.set_state(
+        cdr_type=file2_data["cdr_type"],
+        cdr_date=file2_data["cdr_date"],
+        state=file2_data["state"],
+        session=session,
+    )
+
+    monkeypatch.setattr("etl.etl_utils.get_session", lambda: session)
+    filtered_files = filter_files(
+        found_files=found_files, cdr_type_config=cdr_type_config
+    )
+    assert filtered_files == [file1, file3]
+
+
+def test_get_config_from_file(tmpdir):
+    """
+    Test that we can load yaml to dict from file
+    """
+    sample_dict = {"A": 23, "B": [1, 2, 34], "C": {"A": "bob"}}
+    config_dir = tmpdir.mkdir("config")
+    config_file = config_dir.join("config.yml")
+    config_file.write(yaml.dump(sample_dict))
+
+    config = get_config_from_file(config_filepath=Path(config_file))
+    assert config == sample_dict
