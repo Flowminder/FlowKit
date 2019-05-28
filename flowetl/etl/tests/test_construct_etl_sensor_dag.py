ca86bf4700725ece9b13efdded2ee478e9eb92bd
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-28 14:19:59 +0200

    add licences

--- flowetl/etl/tests/test_construct_etl_sensor_dag.py
+++ flowetl/etl/tests/test_construct_etl_sensor_dag.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# -*- coding: utf-8 -*-
 from pendulum import parse
 from etl.etl_utils import construct_etl_sensor_dag
 from etl.dag_task_callable_mappings import (
