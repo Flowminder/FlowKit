ccd2a498afc88d8fcfd440194fd072d9a0f9176b
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-28 13:30:04 +0200

    use construct_etl_sensor_dag function

--- flowetl/dags/etl_sensor.py
+++ flowetl/dags/etl_sensor.py
@@ -1,61 +1,24 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-# -*- coding: utf-8 -*-
-"""
-Skeleton specification for ETL sensor DAG
-"""
-import os
 import logging
+import os
 
-from uuid import uuid1
 from airflow import DAG
-from airflow.models import DagRun
-from airflow.operators.python_operator import PythonOperator
-from airflow.api.common.experimental.trigger_dag import trigger_dag
+from pendulum import parse
 
-from pendulum import now, parse
-
-from etl.etl_utils import CDRType, generate_table_names
+from etl.etl_utils import construct_etl_sensor_dag
+from etl.dag_task_callable_mappings import (
+    TEST_ETL_SENSOR_TASK_CALLABLE,
+    PRODUCTION_ETL_SENSOR_TASK_CALLABLE,
+)
 
 default_args = {"owner": "flowminder", "start_date": parse("1900-01-01")}
 
-# pylint: disable=unused-argument
-def dummy_trigger_callable(*, dag_run: DagRun, **kwargs):
-    """
-    Dummy callable that triggers ETL dag
-    """
-    logging.info(dag_run)
-
-    if os.environ.get("TESTING", "") == "true":
-        # If testing then only one DAG and we kick this off
-        logging.info("Triggering ETL in testing environment")
-        trigger_dag("etl_testing", run_id=str(uuid.uuid1()), execution_date=now())
-    else:
-        # otherwise for now (will change!) kick all possible DAGs off
-        logging.info("Triggering ETL in production environment")
-        temp_base_conf = {
-            "file_name": "bob.csv",
-            "template_path": "some_path",
-            "cdr_type": "calls",
-            "cdr_date": "2016-01-01",
-        }
-
-        for cdr_type in CDRType:
-            uuid = uuid1()
-
-            temp_table_names = generate_table_names(uuid=uuid, cdr_type=cdr_type)
-            trigger_dag(
-                f"etl_{cdr_type}",
-                run_id=str(uuid),
-                execution_date=now(),
-                conf={**temp_base_conf, **temp_table_names},
-            )
-
-
-with DAG(dag_id="etl_sensor", schedule_interval=None, default_args=default_args) as dag:
-
-    sense = PythonOperator(
-        task_id="sense", python_callable=dummy_trigger_callable, provide_context=True
+if os.environ.get("TESTING", "") == "true":
+    logging.info("running in testing environment")
+    dag = construct_etl_sensor_dag(
+        callable=TEST_ETL_SENSOR_TASK_CALLABLE, default_args=default_args
+    )
+else:
+    logging.info("running in production environment")
+    dag = construct_etl_sensor_dag(
+        callable=PRODUCTION_ETL_SENSOR_TASK_CALLABLE, default_args=default_args
     )
