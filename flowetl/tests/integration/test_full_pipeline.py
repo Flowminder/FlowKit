950119b8d161925eb54fec3ec1b9089a42797651
Joshua Greenhalgh <joshua.greenhalgh@flowminder.org>
2019-05-30 14:05:35 +0200

    check files are where they should be

--- flowetl/tests/integration/test_full_pipeline.py
+++ flowetl/tests/integration/test_full_pipeline.py
@@ -1,3 +1,6 @@
+import os
+
+from pathlib import Path
 from pendulum import parse
 
 from etl.model import ETLRecord
@@ -21,6 +24,7 @@ def test_single_file_previously_quarantined(
             "TOPUPS_20160101.csv.gz",
         ]
     )
+
     # set CALLS_20160101 as archived and SMS_20160101 as quarantined
     CALLS_20160101_record = {
         "cdr_type": "calls",
@@ -50,10 +54,36 @@ def test_single_file_previously_quarantined(
 
     # 1 calls, 1 sms, 1 mds and 1 topups DAG should run and we wait for
     # their completion
-
     wait_for_completion("success", "etl_calls", session=flowetl_db_session)
     wait_for_completion("success", "etl_sms", session=flowetl_db_session)
     wait_for_completion("success", "etl_mds", session=flowetl_db_session)
     wait_for_completion("success", "etl_topups", session=flowetl_db_session)
 
-    assert False
+    # make sure files are where they should be
+
+    dump_files = ["CALLS_20160101.csv.gz", "bad_file.bad"]  # should have been ignored
+    archive_files = [
+        "CALLS_20160102.csv.gz",
+        "SMS_20160101.csv.gz",
+        "MDS_20160101.csv.gz",
+        "TOPUPS_20160101.csv.gz",
+    ]  # ingested so now in archive
+
+    dump = [file.name for file in Path(f"{os.getcwd()}/mounts/dump").glob("*")]
+    archive = [file.name for file in Path(f"{os.getcwd()}/mounts/archive").glob("*")]
+    quarantine = [
+        file.name for file in Path(f"{os.getcwd()}/mounts/quarantine").glob("*")
+    ]
+    ingest = [file.name for file in Path(f"{os.getcwd()}/mounts/ingest").glob("*")]
+
+    assert set(dump_files) == (set(dump) - set(["README.md"]))
+    assert set(archive_files) == (set(archive) - set(["README.md"]))
+
+    # quarantine and ingest should be empty
+    assert set() == (set(quarantine) - set(["README.md"]))
+    assert set() == (set(ingest) - set(["README.md"]))
+
+    # make sure tables expected exist in flowdb
+
+    # make sure etl_records table is what we expect
+
