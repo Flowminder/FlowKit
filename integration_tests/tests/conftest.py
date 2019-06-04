2432f4aa7fb5e01f04003edf062b6d00b37f8968
Maximilian Albert <maximilian.albert@flowminder.org>
2019-06-04 15:06:07 +0100

    Update conftest.py

--- integration_tests/tests/conftest.py
+++ integration_tests/tests/conftest.py
@@ -201,11 +201,11 @@ def fm_conn():
     -------
     flowmachine.core.connection.Connection
     """
-    POSTGRES_USER = os.getenv("POSTGRES_USER", "flowdb")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "flowflow")
+    FLOWDB_ADMIN_USER = os.getenv("FLOWDB_ADMIN_USER", "flowdb")
+    FLOWDB_ADMIN_PASSWORD = os.getenv("FLOWDB_ADMIN_PASSWORD", "flowflow")
     FLOWDB_HOST = os.getenv("FLOWDB_HOST", "localhost")
     FLOWDB_PORT = os.getenv("FLOWDB_PORT", "9000")
-    conn_str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{FLOWDB_HOST}:{FLOWDB_PORT}/flowdb"
+    conn_str = f"postgresql://{FLOWDB_ADMIN_USER}:{FLOWDB_ADMIN_PASSWORD}@{FLOWDB_HOST}:{FLOWDB_PORT}/flowdb"
 
     fm_conn = Connection(conn_str=conn_str)
     flowmachine.connect(conn=fm_conn)
