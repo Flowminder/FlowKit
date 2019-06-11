b19cea38cc3f22ce338708adaac2248ee24a9e2f
Maximilian Albert <maximilian.albert@flowminder.org>
2019-06-11 17:13:28 +0100

    Convert logging code to structlog

--- flowetl/etl/etl/__init__.py
+++ flowetl/etl/etl/__init__.py
@@ -3,3 +3,23 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # -*- coding: utf-8 -*-
+
+import structlog
+
+
+structlog.configure(
+    processors=[
+        structlog.stdlib.filter_by_level,
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.JSONRenderer(serializer=rapidjson.dumps),
+    ],
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    context_class=dict,
+    cache_logger_on_first_use=True,
+)
--- flowetl/etl/etl/dummy_task_callables.py
+++ flowetl/etl/etl/dummy_task_callables.py
@@ -8,12 +8,14 @@ Contains the definition of dummy callables to be used when testing
 """
 
 import os
-import logging
+import structlog
 
 from uuid import uuid1
 from pendulum import utcnow
 from airflow.models import DagRun, TaskInstance
 from airflow.api.common.experimental.trigger_dag import trigger_dag
+
+logger = structlog.get_logger("flowetl")
 
 # pylint: disable=unused-argument
 def dummy__callable(*, dag_run: DagRun, task_instance: TaskInstance, **kwargs):
@@ -22,7 +24,7 @@ def dummy__callable(*, dag_run: DagRun, task_instance: TaskInstance, **kwargs):
     TASK_TO_FAIL is set to the name of the current task, otherwise succeeds
     silently.
     """
-    logging.info(dag_run)
+    logger.info(dag_run)
     if os.environ.get("TASK_TO_FAIL", "") == task_instance.task_id:
         raise Exception
 
@@ -31,7 +33,7 @@ def dummy_failing__callable(*, dag_run: DagRun, **kwargs):
     """
     Dummy python callable raising an exception
     """
-    logging.info(dag_run)
+    logger.info(dag_run)
     raise Exception
 
 
@@ -40,5 +42,5 @@ def dummy_trigger__callable(*, dag_run: DagRun, **kwargs):
     In test env we just want to trigger the etl_testing DAG with
     no config.
     """
-    logging.info(dag_run)
+    logger.info(dag_run)
     trigger_dag("etl_testing", run_id=str(uuid1()), execution_date=utcnow())
