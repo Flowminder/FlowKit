7699f07fd07e3442246e4de7c74ecb43c008b2b2
Dan Williams <dan@ithium.net>
2019-07-01 12:11:46 +0100

    Removed gosu, added nss wrapper and global permissions on airflow (#716)
    The libnss-wrapper can fake /etc/passwd and /etc/group to say that the
    current userid and groupid belong to airflow. This, combined with the
    global permissions on the AIRFLOW_HOME allow an arbitrary uid/gid combo
    as passed through docker, to run the Airflow webserver and trigger DAGs.

--- flowetl/entrypoint.sh
+++ flowetl/entrypoint.sh
@@ -4,10 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-if [ "$(id -u)" = '0' ]; then
-    chown -R airflow: /mounts/
-
-    exec gosu airflow "$BASH_SOURCE" "$@"
-fi;
+# Used to fake a user and group when passed to docker and does not already exist
+# https://cwrap.org/nss_wrapper.html
+if ! getent passwd "$(id -u)" &> /dev/null && [ -e /usr/lib/libnss_wrapper.so ]; then
+		echo "airflow:x:$(id -u):$(id -g):Airflow:$HOME:/bin/false" > "$NSS_WRAPPER_PASSWD"
+		echo "airflow:x:$(id -g):" > "$NSS_WRAPPER_GROUP"
+fi
 
 exec /defaultentrypoint.sh "$@"
