ab4002c15ee66a25be0909f5f011513108b97dbd
Dan Williams <dan@ithium.net>
2019-07-02 13:30:55 +0100

    Added initial ETLPostQueryOutcome model (#989)

--- flowetl/etl/etl/model.py
+++ flowetl/etl/etl/model.py
@@ -98,3 +98,75 @@ class ETLRecord(Base):
             process = False
 
         return process
+
+
+# pylint: disable=too-few-public-methods
+class ETLPostQueryOutcome(Base):
+    """
+    DB Model for storing the post etl queries outcome.
+    """
+
+    __tablename__ = "post_etl_queries"
+    __table_args__ = {"schema": "etl"}
+
+    id = Column(Integer, primary_key=True)
+    cdr_type = Column(String)
+    cdr_date = Column(Date)
+    type_of_query_or_check = Column(String)
+    outcome = Column(String)
+    optional_comment_or_description = Column(String)
+
+    def __init__(
+        self,
+        *,
+        cdr_type: str,
+        cdr_date: pendulumDate,
+        type_of_query_or_check: str,
+        outcome: str,
+        optional_comment_or_description: str = "",
+    ):
+
+        self.cdr_type = etl_utils.CDRType(cdr_type)
+        self.cdr_date = cdr_date
+        self.type_of_query_or_check = type_of_query_or_check
+        self.outcome = outcome
+        self.optional_comment_or_description = optional_comment_or_description
+
+    @classmethod
+    def set_outcome(
+        cls,
+        *,
+        cdr_type: str,
+        cdr_date: pendulumDate,
+        type_of_query_or_check: str,
+        outcome: str,
+        optional_comment_or_description: str = "",
+        session: Session,
+    ) -> None:
+        """
+        Add new row to the post-etl book-keeping table.
+
+        Parameters
+        ----------
+        cdr_type : str
+            CDR type of file being processed ("calls", "sms", "mds" or "topups")
+        cdr_date : Date
+            The date with which the file's data is associated
+        type_of_query_or_check : str
+            The type of query or check that is being recorded
+        outcome : str
+            The outcome of the associated query
+        optional_comment_or_description : str
+            Any additional comments or description for the query
+        session : Session
+            A sqlalchemy session for a DB in which this model exists.
+        """
+        row = cls(
+            cdr_type=cdr_type,
+            cdr_date=cdr_date,
+            type_of_query_or_check=type_of_query_or_check,
+            outcome=outcome,
+            optional_comment_or_description=optional_comment_or_description,
+        )
+        session.add(row)
+        session.commit()
