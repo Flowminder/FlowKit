374180cfefbdc3a926f650bd3a5077d20cd11615
Dan Williams <dan@ithium.net>
2019-07-10 16:41:09 +0100

    Added logic to save the airflow logs when env var present (#1019)

--- flowetl/tests/integration/conftest.py
+++ flowetl/tests/integration/conftest.py
@@ -102,6 +102,7 @@ def container_env():
         "AIRFLOW__CORE__FERNET_KEY": "ssgBqImdmQamCrM9jNhxI_IXSzvyVIfqvyzES67qqVU=",
         "AIRFLOW__CORE__SQL_ALCHEMY_CONN": f"postgres://{flowetl_db['POSTGRES_USER']}:{flowetl_db['POSTGRES_PASSWORD']}@flowetl_db:5432/{flowetl_db['POSTGRES_DB']}",
         "AIRFLOW_CONN_FLOWDB": f"postgres://{flowdb['POSTGRES_USER']}:{flowdb['POSTGRES_PASSWORD']}@flowdb:5432/flowdb",
+        "AIRFLOW_HOME": os.environ["AIRFLOW_HOME"],
         "POSTGRES_USER": "flowetl",
         "POSTGRES_PASSWORD": "flowetl",
         "POSTGRES_DB": "flowetl",
@@ -329,6 +330,22 @@ def flowetl_container(
     )
     wait_for_container()
     yield container
+
+    save_airflow_logs = (
+        os.getenv("FLOWETL_INTEGRATION_TESTS_SAVE_AIRFLOW_LOGS", "FALSE").upper()
+        == "TRUE"
+    )
+    if save_airflow_logs:
+        logger.info(
+            "Saving airflow logs to /mounts/logs/ and outputting to stdout "
+            "(because FLOWETL_INTEGRATION_TESTS_SAVE_AIRFLOW_LOGS=TRUE)."
+        )
+        container.exec_run("bash -c 'cp -r $AIRFLOW_HOME/logs/* /mounts/logs/'")
+        airflow_logs = container.exec_run(
+            "bash -c 'find /mounts/logs -type f -exec cat {} \;'"
+        )
+        logger.info(airflow_logs)
+
     container.kill()
     container.remove()
 
