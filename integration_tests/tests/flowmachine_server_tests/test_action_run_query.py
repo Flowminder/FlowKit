89c8ef232647103a1f3b5f0d5b1a171b715e7fcb
Bhavin Panchal <bhavinpanchal86@live.com>
2019-07-10 17:00:17 +0100

    Updated test_action_run_query test

--- integration_tests/tests/flowmachine_server_tests/test_action_run_query.py
+++ integration_tests/tests/flowmachine_server_tests/test_action_run_query.py
@@ -141,7 +141,7 @@ async def test_run_query(zmq_port, zmq_host, fm_conn, redis):
                     "locations": {
                         "0": {
                             "aggregation_unit": [
-                                "Must be one of: admin0, admin1, admin2, admin3."
+                                "Must be one of: admin0, admin1, admin2, admin3, lon-lat."
                             ]
                         }
                     }
