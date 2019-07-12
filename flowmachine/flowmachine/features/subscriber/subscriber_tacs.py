0c6a849bfcff9f35e172a83b0a671ddebd07bded
Guilherme Zagatti <gzagatti@users.noreply.github.com>
2019-07-12 10:05:02 -0400

    Fix spacing.
    Co-Authored-By: maxalbert <maximilian.albert@flowminder.org>

--- flowmachine/flowmachine/features/subscriber/subscriber_tacs.py
+++ flowmachine/flowmachine/features/subscriber/subscriber_tacs.py
@@ -494,7 +494,7 @@ class SubscriberHandsetCharacteristic(SubscriberFeature):
     >>> subscriber_smart = SubscriberHandsetCharacteristic('2016-01-01', '2016-01-07',
         'hnd_type')
     >>> subscriber_smart.get_dataframe()
-             subscriber     value
+             subscriber        value
     0  038OVABN11Ak4W5P        Smart
     1  09NrjaNNvDanD8pk      Feature
     2  0ayZGYEQrqYlKw6g      Feature
