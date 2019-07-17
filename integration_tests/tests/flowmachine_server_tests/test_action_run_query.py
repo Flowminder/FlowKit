4cf3421e72267bc0df5ba5fe0f0b503493ef5087
Bhavin Panchal <bhavinpanchal86@live.com>
2019-07-17 12:39:48 +0100

    reverting previous change

--- integration_tests/tests/flowmachine_server_tests/test_action_run_query.py
+++ integration_tests/tests/flowmachine_server_tests/test_action_run_query.py
@@ -104,7 +104,7 @@ async def test_run_query(zmq_port, zmq_host, fm_conn, redis):
                     "subscriber_subset": None,
                 },
             },
-            {"0": {"locations": {"0": {"date": ["Not a valid date."]}}}},
+            {"locations": {"date": ["Not a valid date."]}},
         ),
         (
             {
@@ -117,13 +117,7 @@ async def test_run_query(zmq_port, zmq_host, fm_conn, redis):
                     "subscriber_subset": None,
                 },
             },
-            {
-                "0": {
-                    "locations": {
-                        "0": {"method": ["Must be one of: last, most-common."]}
-                    }
-                }
-            },
+            {"locations": {"method": ["Must be one of: last, most-common."]}},
         ),
         (
             {
@@ -159,11 +153,7 @@ async def test_run_query(zmq_port, zmq_host, fm_conn, redis):
                     "subscriber_subset": "virtually_all_subscribers",
                 },
             },
-            {
-                "0": {
-                    "locations": {"0": {"subscriber_subset": ["Must be one of: None."]}}
-                }
-            },
+            {"locations": {"subscriber_subset": ["Must be one of: None."]}},
         ),
     ],
 )
