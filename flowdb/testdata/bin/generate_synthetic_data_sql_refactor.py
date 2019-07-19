8be40a6319d327217104f81f2c69cd0056cbe8e0
Dan Williams <dan@ithium.net>
2019-07-19 15:28:07 +0100

    Added a first basic approach to refactoring generate_synthetic_data_sql
      - This adds only creating the infrastructure.sites data
      - Increments the id field so that it is reproducible

---
+++ flowdb/testdata/bin/generate_synthetic_data_sql_refactor.py
@@ -0,0 +1,94 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# !/usr/bin/env python
+
+"""
+Small script for generating arbitrary volumes of CDR call data inside the flowdb
+container.
+
+Produces sites, cells, tacs, call, sms and mds data.
+
+Optionally simulates a 'disaster' where all subscribers must leave a designated admin2 region
+for a period of time.
+"""
+
+import os
+import argparse
+import datetime
+from hashlib import md5
+from concurrent.futures.thread import ThreadPoolExecutor
+from contextlib import contextmanager
+from multiprocessing import cpu_count
+
+import sqlalchemy as sqlalchemy
+from sqlalchemy.exc import ResourceClosedError
+
+import structlog
+import json
+
+structlog.configure(
+    processors=[
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.JSONRenderer(serializer=json.dumps),
+    ]
+)
+
+logger = structlog.get_logger(__name__)
+parser = argparse.ArgumentParser(description="Flowminder Synthetic CDR Generator\n")
+
+parser.add_argument(
+    "--n-sites", type=int, default=1000, help="Number of sites to generate."
+)
+
+
+@contextmanager
+def log_duration(job: str, **kwargs):
+    """
+    Small context handler that logs the duration of the with block.
+
+    Parameters
+    ----------
+    job: str
+        Description of what is being run, will be shown under the "job" key in log
+    kwargs: dict
+        Any kwargs will be shown in the log as "key":"value"
+    """
+    start_time = datetime.datetime.now()
+    logger.info("Started", job=job, **kwargs)
+    yield
+    logger.info(
+        "Finished", job=job, runtime=str(datetime.datetime.now() - start_time), **kwargs
+    )
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    with log_duration("Generating synthetic data..", **vars(args)):
+        num_sites = args.n_sites
+
+        engine = sqlalchemy.create_engine(
+            f"postgresql://{os.getenv('POSTGRES_USER')}@/{os.getenv('POSTGRES_DB')}",
+            echo=False,
+            strategy="threadlocal",
+            pool_size=cpu_count(),
+            pool_timeout=None,
+        )
+
+        start_time = datetime.datetime.now()
+        start_id = 1000000
+
+        # Generate some randomly distributed sites and cells
+        with engine.begin() as trans:
+            with log_duration(job=f"Generating {num_sites} sites."):
+                # Truncate the table first so that we can possibly run multiple times
+                trans.execute("TRUNCATE infrastructure.sites;")
+                for x in range(start_id, num_sites + start_id):
+                    hash = md5(int(x).to_bytes(8, "big", signed=True)).hexdigest()
+                    trans.execute(
+                        f"INSERT INTO infrastructure.sites (id, version, date_of_first_service) SELECT '{hash}', 0, (date '2015-01-01' + random() * interval '1 year')::date;"
+                    )
