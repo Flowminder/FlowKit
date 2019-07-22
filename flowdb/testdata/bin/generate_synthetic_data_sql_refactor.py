7dbfff0281d1ca64efaf246b143415d0a96bff94
Dan Williams <dan@ithium.net>
2019-07-22 16:49:28 +0100

    Started to implement an approach to setting the distribution of data
    This starts by adding the generateNormalDistribution function, then
    applies this to the generation of TACs data - so that we have a
    distribution of handsets across the data set required.

--- flowdb/testdata/bin/generate_synthetic_data_sql_refactor.py
+++ flowdb/testdata/bin/generate_synthetic_data_sql_refactor.py
@@ -15,6 +15,7 @@ for a period of time.
 """
 
 import os
+import sys
 import argparse
 import datetime
 from hashlib import md5
@@ -22,6 +23,8 @@ from concurrent.futures.thread import ThreadPoolExecutor
 from contextlib import contextmanager
 from multiprocessing import cpu_count
 from itertools import cycle
+from math import floor
+import numpy as np
 
 import sqlalchemy as sqlalchemy
 from sqlalchemy.exc import ResourceClosedError
@@ -51,6 +54,12 @@ parser.add_argument(
 parser.add_argument(
     "--n-tacs", type=int, default=4000, help="Number of phone models to generate."
 )
+parser.add_argument(
+    "--n-subscribers", type=int, default=4000, help="Number of subscribers to generate."
+)
+parser.add_argument(
+    "--n-calls", type=int, default=200_000, help="Number of calls to generate per day."
+)
 
 # Logging context
 @contextmanager
@@ -73,11 +82,25 @@ def log_duration(job: str, **kwargs):
     )
 
 
+# Generate MD5 HashDigest
 def generate_hash(index):
     """
     Generates a md5 checksum from an integer index value
     """
     return md5(int(index).to_bytes(8, "big", signed=True)).hexdigest()
+
+
+# Generate normal distribution
+def generateNormalDistribution(size, mu=0, sigma=1):
+    """
+    Generates a normal distributed progression for use in seeding
+    data. If mu/sigma aren't passed in, they will default to 0/1
+    """
+
+    # Fix the seend to ensure data is the same
+    np.random.seed(42)
+
+    return np.random.normal(mu, sigma, size)
 
 
 if __name__ == "__main__":
@@ -87,6 +110,8 @@ if __name__ == "__main__":
         num_sites = min(10000, args.n_sites)
         num_cells = args.n_cells
         num_tacs = args.n_tacs
+        num_subscribers = args.n_subscribers
+        num_calls = args.n_calls
 
         engine = sqlalchemy.create_engine(
             f"postgresql://{os.getenv('POSTGRES_USER')}@/{os.getenv('POSTGRES_DB')}",
@@ -100,8 +125,11 @@ if __name__ == "__main__":
         start_id = 1000000
         dir = os.path.dirname(os.path.abspath(__file__))
 
-        # Generate some randomly distributed sites and cells
+        # Main generation process
         with engine.begin() as trans:
+
+            # The following generates the infrastructure schema data
+            # 1. Sites and cells
             with log_duration(
                 job=f"Generating {num_sites} sites and {num_cells} cells."
             ):
@@ -137,6 +165,7 @@ if __name__ == "__main__":
 
                     f.close()
 
+            # 2. TACS
             with log_duration(f"Generating {num_tacs} tacs."):
                 # First truncate the table
                 trans.execute("TRUNCATE infrastructure.tacs;")
@@ -153,14 +182,18 @@ if __name__ == "__main__":
                 ]
                 types = ["Smart", "Feature", "Basic"]
 
-                brand = cycle(brands)
+                # Get a random distribution for the brand types across the data set
+                distribution = generateNormalDistribution(num_tacs, 3)
+                y = 0
                 type = cycle(types)
-
                 for x in range(start_id, num_tacs + start_id):
                     id = x + 1000
+                    idx = floor(distribution[y])
                     hash = generate_hash(id)
+                    y += 1
+
                     trans.execute(
                         f"""
-                            INSERT INTO infrastructure.tacs (id, brand, model, hnd_type) VALUES ({id}, '{next(brand)}', '{hash}', '{next(type)}');
+                            INSERT INTO infrastructure.tacs (id, brand, model, hnd_type) VALUES ({id}, '{brands[idx]}', '{hash}', '{next(type)}');
                         """
                     )
