0a59a6931547ff6536e6e29729b64cf962593be6
Jono Gray <jonathan.gray@flowminder.org>
2019-08-22 17:48:52 +0100

    Fix bugs, change implementation so we can support specified bins with non-ints

--- flowmachine/flowmachine/features/utilities/histogram_aggregations.py
+++ flowmachine/flowmachine/features/utilities/histogram_aggregations.py
@@ -1,14 +1,67 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from functools import singledispatch
 
 from ...core.query import Query
+from itertools import count
 from typing import List, Tuple, Optional, Union
 
 
-class HistogramAggregation(Query):
-    """ 
+@singledispatch
+def _get_bins_clause(bins: int) -> Tuple[int, str]:
+    return (
+        bins,
+        f"""
+        SELECT v, 
+            (SELECT lower FROM bounds)+((v-1)*(SELECT (upper-lower)/({bins}) FROM bounds)) as lower, 
+            (SELECT lower FROM bounds)+((v)*(SELECT (upper-lower)/({bins}) FROM bounds)) as upper
+                        FROM generate_series(
+                            1, {bins}) as v
+        """,
+    )
 
+
+@_get_bins_clause.register
+def _(bins: list) -> Tuple[int, str]:
+    return (
+        len(bins),
+        f"""
+                SELECT * FROM (VALUES {",".join(f"({i}, {low}, {high})" for i, low, high in zip(count(), bins[:-1], bins[1:]))}) as t(v, lower, upper)
+                """,
+    )
+
+
+@singledispatch
+def _get_bounds_clause(bounds, value_column: str, metric: Query) -> str:
+    return f"""
+        SELECT max({value_column})::numeric as upper, 
+                min({value_column})::numeric as lower 
+            FROM ({metric.get_query()}) AS to_agg
+        """
+
+
+@_get_bounds_clause.register
+def _(bounds: tuple, value_column: str, metric: Query) -> str:
+    return f"""
+            SELECT {max(bounds)}::numeric as upper, 
+                   {min(bounds)}::numeric  as lower 
+             """
+
+
+class HistogramAggregation(Query):
+    """
+    Compute the histogram of another query.
+
+    Parameters
+    ----------
+    metric : Query
+        Query to build histogram over
+    bins : int, or list of float
+        Either an integer number of equally spaced bins, or a list of bin edges
+    range : tuple of float, default None
+        Optionally supply inclusive lower and upper bounds to build the histogram over. By default, the
+        histogram will cover the whole range of the data.
     """
 
     def __init__(
@@ -16,52 +69,49 @@ class HistogramAggregation(Query):
         *,
         metric: "Query",
         bins: Union[List[float], int],
-        ranges: Optional[Tuple[float, float]] = None,
+        range: Optional[Tuple[float, float]] = None,
+        value_column: str = "value",
     ) -> None:
+
         self.metric = metric
         self.bins = bins
-        self.ranges = ranges
+        self.range = range
+        self.value_column = value_column
+        if self.value_column not in self.metric.column_names:
+            raise ValueError(
+                f"'{self.value_column}' is not a column in this query. Must be one of '{self.metric.column_names}'"
+            )
         if not isinstance(self.bins, (int, list)):
             raise ValueError("Bins should be an integer or list of numeric values.")
-        if not isinstance(self.ranges, tuple) and None:
+        if not isinstance(self.range, tuple) and None:
             raise ValueError("Range should be tuple of two values")
 
         super().__init__()
 
     @property
     def column_names(self) -> List[str]:
-        return ["value", "bin_edges"]
+        return ["value", "lower_edge", "upper_edge"]
 
     def _make_query(self):
-        if isinstance(self.ranges, tuple) and not None:
-            max_range = max(self.ranges)
-            min_range = min(self.ranges)
-        elif self.ranges is None:
-            max_range = (
-                f"""SELECT MAX(value) FROM ({self.metric.get_query()}) AS to_agg """
+
+        num_bins, bins_sql = _get_bins_clause(self.bins)
+        bounds_sql = _get_bounds_clause(self.range, self.value_column, self.metric)
+
+        return f"""
+        WITH bounds AS ({bounds_sql}),
+            
+            breaks AS (
+            SELECT
+                numrange(lower, upper, CASE WHEN v={num_bins} THEN '[]' ELSE '[)' END) as bin
+            FROM ({bins_sql}) as b
             )
-            min_range = (
-                f""" SELECT MIN(value) FROM ({self.metric.get_query()}) AS to_agg """
-            )
-
-        filter_values = f""" SELECT value FROM ({self.metric.get_query()}) AS to_agg
-                        WHERE value BETWEEN ({min_range}) AND ({max_range})"""
-        if isinstance(self.bins, int):
-            sql = f""" 
-                SELECT
-                    count(value) AS value,
-                    width_bucket(foo.value::numeric,({max_range}),({min_range}),{self.bins}) AS bin_edges
-                FROM ({filter_values}) AS foo
-                GROUP BY bin_edges
-                """
-        elif isinstance(self.bins, list):
-
-            sql = f"""
-                SELECT 
-                    count(foo.value) AS value, 
-                    width_bucket(foo.value::numeric,Array{self.bins}::numeric[]) AS bin_edges 
-                FROM ({filter_values}) AS foo
-                GROUP BY bin_edges
-                """
-
-        return sql
+             
+        SELECT count(*) as value, 
+            lower(bin) as lower_edge,
+            upper(bin) as upper_edge 
+        FROM breaks
+         LEFT JOIN ({self.metric.get_query()}) as to_agg
+         ON bin @> to_agg.{self.value_column}::numeric
+        GROUP BY bin 
+        ORDER BY bin ASC
+        """
--- flowmachine/tests/test_histogram_aggregations.py
+++ flowmachine/tests/test_histogram_aggregations.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import numpy as np
+import pytest
 
 from flowmachine.features import RadiusOfGyration
 from flowmachine.features.utilities.histogram_aggregations import HistogramAggregation
@@ -12,38 +13,70 @@ def test_create_histogram_using_int_bins_value(get_dataframe):
     """
     Create histogram using one bins value.
     """
-    RoG = RadiusOfGyration("2016-01-01", "2016-01-02")
+    radius_of_gyration = RadiusOfGyration("2016-01-01", "2016-01-02")
 
-    agg = HistogramAggregation(metric=RoG, bins=5)
+    agg = HistogramAggregation(metric=radius_of_gyration, bins=5)
     df = get_dataframe(agg)
-    numpy_histogram = np.histogram(get_dataframe(RoG).value, bins=5)
-    # assert set(numpy_histogram[0].tolist()) == set(df.value.tolist())
+    numpy_histogram, numpy_bins = np.histogram(
+        get_dataframe(radius_of_gyration).value, bins=5
+    )
+    assert df.value.sum() == len(get_dataframe(radius_of_gyration))
+    assert numpy_histogram.tolist() == df.value.tolist()
+    assert numpy_bins.tolist()[:-1] == pytest.approx(df.lower_edge.tolist())
+    assert numpy_bins.tolist()[1:] == pytest.approx(df.upper_edge.tolist())
 
 
 def test_create_histogram_using_list_of_bins_values(get_dataframe):
     """
     Create histogram using list of bins values.
     """
-    RoG = RadiusOfGyration("2016-01-01", "2016-01-02")
+    radius_of_gyration = RadiusOfGyration("2016-01-01", "2016-01-02")
 
-    agg = HistogramAggregation(metric=RoG, bins=[10, 20, 30, 40, 50, 60])
+    agg = HistogramAggregation(metric=radius_of_gyration, bins=[10, 20, 30, 40, 50, 60])
     df = get_dataframe(agg)
-    numpy_histogram = np.histogram(
-        get_dataframe(RoG).value, bins=[10, 20, 30, 40, 50, 60]
+    numpy_histogram, numpy_bins = np.histogram(
+        get_dataframe(radius_of_gyration).value, bins=[10, 20, 30, 40, 50, 60]
     )
-    # assert set(numpy_histogram[0].tolist()) == set(df.value.tolist())
+    assert numpy_histogram.tolist() == df.value.tolist()
+    assert numpy_bins.tolist()[:-1] == pytest.approx(df.lower_edge.tolist())
+    assert numpy_bins.tolist()[1:] == pytest.approx(df.upper_edge.tolist())
 
 
 def test_create_histogram_using_bins_and_range_values(get_dataframe):
     """
     Create histogram using one bins and range values.
     """
-    RoG = RadiusOfGyration("2016-01-01", "2016-01-02")
+    radius_of_gyration = RadiusOfGyration("2016-01-01", "2016-01-02")
 
-    agg = HistogramAggregation(metric=RoG, bins=5, ranges=(130.00, 230.00))
+    agg = HistogramAggregation(
+        metric=radius_of_gyration, bins=5, range=(130.00, 230.00)
+    )
     df = get_dataframe(agg)
-    numpy_histogram = np.histogram(
-        get_dataframe(RoG).value, bins=5, range=(130.00, 230.00)
+    numpy_histogram, numpy_bins = np.histogram(
+        get_dataframe(radius_of_gyration).value, bins=5, range=(130.00, 230.00)
     )
 
-    assert set(numpy_histogram[0].tolist()) == set(df.value.tolist())
+    assert numpy_histogram.tolist() == df.value.tolist()
+    assert numpy_bins.tolist()[:-1] == pytest.approx(df.lower_edge.tolist())
+    assert numpy_bins.tolist()[1:] == pytest.approx(df.upper_edge.tolist())
+
+
+def test_create_histogram_using_bins_list_and_range_values(get_dataframe):
+    """
+    Create histogram using list of bins and range values (checking for consistency with numpy).
+    """
+    radius_of_gyration = RadiusOfGyration("2016-01-01", "2016-01-02")
+
+    agg = HistogramAggregation(
+        metric=radius_of_gyration, bins=[10, 20, 30, 40, 50, 60], range=(130.00, 230.00)
+    )
+    df = get_dataframe(agg)
+    numpy_histogram, numpy_bins = np.histogram(
+        get_dataframe(radius_of_gyration).value,
+        bins=[10, 20, 30, 40, 50, 60],
+        range=(130.00, 230.00),
+    )
+
+    assert numpy_histogram.tolist() == df.value.tolist()
+    assert numpy_bins.tolist()[:-1] == pytest.approx(df.lower_edge.tolist())
+    assert numpy_bins.tolist()[1:] == pytest.approx(df.upper_edge.tolist())
