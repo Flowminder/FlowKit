bfdc0dba408f28a0673e5073b05292872298945b
Maximilian Albert <maximilian.albert@flowminder.org>
2019-09-03 17:33:39 +0100

    Restart postgres and make it listen on localhost so that we can run the alembic migrations

--- flowdb/build_scripts/0060_run_database_migrations.sh
+++ flowdb/build_scripts/0060_run_database_migrations.sh
@@ -13,4 +13,16 @@ set -euo pipefail
 #  Run database migrations using alembic.
 #
 
+# Restart postgres and make it listen on localhost
+# so that Alembic can connect to it.
+pg_ctl -w \
+    -D "$PGDATA" \
+    -o "-c listen_addresses='127.0.0.1'" \
+	restart
+
+echo "---------------------------------"
+echo " * Applying database migrations. "
+echo "---------------------------------"
+
+cd /docker-entrypoint-initdb.d/
 alembic upgrade head
