022eb5f92d74735b62bcbf789926aee7c686cd6b
James Harrison <james.harrison@flowminder.org>
2019-09-05 17:59:00 +0100

    Remove kill-all-queries query for now

--- integration_tests/tests/conftest.py
+++ integration_tests/tests/conftest.py
@@ -222,11 +222,6 @@ def reset_cache_schema(fm_conn, redis_instance):
     and truncating the internal tables 'cache.cached' and 'cache.dependencies'
     so that they are empty.
     """
-    print("Killing any queries still running...")
-    with fm_conn.engine.begin():  # Kill any running queries
-        fm_conn.engine.execute(
-            f"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND application_name='{fm_conn.app_name}';"
-        )
     print("[DDD] Recreating cache schema... ", end="", flush=True)
     reset_cache(fm_conn, redis_instance, protect_table_objects=False)
     print("Done.")
