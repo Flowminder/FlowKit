9aec34d45a876fa12d480779ab1ed8d47d9f0af0
Jono Gray <jonathan.gray@flowminder.org>
2019-09-11 11:30:47 +0100

    Add MPL header

--- flowmachine/tests/test_update_available_dates.py
+++ flowmachine/tests/test_update_available_dates.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import asyncio
 
 import pytest
@@ -9,6 +13,10 @@ from flowmachine.core.server.server import update_available_dates
 
 @pytest.mark.asyncio
 async def test_available_dates_updates(flowmachine_connect):
+    """
+    Test the looping async task which updates the available dates on a loop
+    as part of the server process.
+    """
     reset_cache(flowmachine_connect, Query.redis, protect_table_objects=False)
     assert len(list(Table.get_stored())) == 0
     task = asyncio.create_task(
