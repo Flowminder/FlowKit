43304765f9c7b1bc670aa0b39013045e478ceca7
Jono Gray <jonathan.gray@flowminder.org>
2019-10-14 16:25:39 +0100

    Remove redoc specific tweak
    Discriminators now work, so we don't need a redoc specific version of the spec

--- integration_tests/tests/flowapi_tests/test_api_spec.py
+++ integration_tests/tests/flowapi_tests/test_api_spec.py
@@ -6,17 +6,6 @@ import requests
 from approvaltests import verify
 
 
-def test_generated_openapi_redoc_spec(flowapi_url, diff_reporter):
-    """
-    Verify the OpenAPI spec for FlowAPI (as formatted for redoc).
-    """
-    spec = requests.get(f"{flowapi_url}/api/0/spec/openapi-redoc.json").json()
-    spec_version = spec["info"].pop("version")
-    assert spec_version == flowapi.__version__
-    spec_as_json_string = json.dumps(sort_recursively(spec), indent=2, sort_keys=True)
-    verify(spec_as_json_string, diff_reporter)
-
-
 def test_generated_openapi_json_spec(flowapi_url, diff_reporter):
     """
     Verify the OpenAPI spec for FlowAPI.
