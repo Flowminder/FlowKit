aa165ec1a55f939b91b5613fec67ed2652b72348
Jono Gray <jonathan.gray@flowminder.org>
2019-10-22 15:00:50 +0100

    Infer scopes from schema

--- flowapi/flowapi/user_model.py
+++ flowapi/flowapi/user_model.py
@@ -3,18 +3,13 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-from quart_jwt_extended.exceptions import UserClaimsVerificationError
-from quart.exceptions import HTTPException
-from typing import Dict, Union, List, Tuple
+from typing import Tuple, Set, FrozenSet
 
 from quart_jwt_extended import get_jwt_claims, get_jwt_identity
 from quart import current_app, request
 
-from flowapi.flowapi_errors import (
-    MissingQueryKindError,
-    MissingAggregationUnitError,
-    BadQueryError,
-)
+from flowapi.permissions import query_to_scope_set, scopes_to_sets
+from flowapi.utils import get_query_parameters_from_flowmachine
 
 
 class UserObject:
@@ -32,117 +27,10 @@ class UserObject:
     """
 
     def __init__(
-        self,
-        username: str,
-        claims: Dict[str, Dict[str, Union[List[str], Dict[str, bool]]]],
+        self, username: str, scope_set: Set[FrozenSet[str, Tuple[str, str]]]
     ) -> None:
         self.username = username
-        self.claims = claims
-
-    def has_access(
-        self, *, action: str, query_kinds_and_aggregations: List[Tuple[str, str]]
-    ) -> bool:
-        """
-        Returns true if the user can do 'action' with this kind of query at this unit of aggregation.
-
-        Parameters
-        ----------
-        action: {'run', 'poll', 'get_results'}
-            Action to check
-        query_kinds_and_aggregations : list of tuples
-            List of tuples giving a query kind and aggregation unit
-
-        Returns
-        -------
-        bool
-            True if the user can do 'action' with this query
-
-        Raises
-        ------
-        UserClaimsVerificationError
-            If the user cannot do action with this kind of query at this level of aggregation
-        """
-        for query_kind, aggregation_unit in query_kinds_and_aggregations:
-            try:
-                action_rights = self.claims[query_kind]["permissions"][action]
-                aggregation_right = (
-                    aggregation_unit in self.claims[query_kind]["spatial_aggregation"]
-                )
-                if not action_rights:
-                    raise UserClaimsVerificationError(
-                        f"Token does not allow {action} for query kind '{query_kind}'"
-                    )
-                if not aggregation_right:
-                    raise UserClaimsVerificationError(
-                        f"Token does not allow query kind '{query_kind}' at spatial aggregation '{aggregation_unit}'"
-                    )
-            except KeyError:
-                raise UserClaimsVerificationError("Claims verification failed.")
-        return True
-
-    def _get_query_kinds_and_aggregation_units(
-        self, query_json: dict
-    ) -> List[Tuple[str, str]]:
-        """
-        Extract the query kind and aggregation unit from a query spec dict.
-
-        Parameters
-        ----------
-        query_json : dict
-            Dictionary containing a query spec
-
-        Returns
-        -------
-        list of tuples str, str
-            List of tuples giving query kind and aggregation unit
-
-        """
-
-        try:
-            query_kind = query_json["query_kind"]
-        except KeyError:
-            raise MissingQueryKindError
-        try:
-            if query_kind == "spatial_aggregate":
-                return self._get_query_kinds_and_aggregation_units(
-                    query_json=query_json["locations"]
-                )
-            elif query_kind == "joined_spatial_aggregate":
-                location_spec = self._get_query_kinds_and_aggregation_units(
-                    query_json=query_json["locations"]
-                )[0]
-                # If the metric spec doesn't have an aggregation unit, we stitch on the one
-                # from the locations.
-                # TODO: This is a bit of a hack, we should tidy this up soon!
-                metric_query_json = query_json["metric"].copy()
-                location_aggregation_unit = location_spec[1]
-                if "aggregation_unit" not in metric_query_json.keys():
-                    metric_query_json["aggregation_unit"] = location_aggregation_unit
-                else:
-                    if (
-                        metric_query_json["aggregation_unit"]
-                        != location_aggregation_unit
-                    ):
-                        # TODO: add support for different aggregation units
-                        raise ValueError(
-                            "Different aggregation units for metric and location are not currently supported."
-                        )
-                metric_spec = self._get_query_kinds_and_aggregation_units(
-                    query_json=metric_query_json
-                )[0]
-                return [location_spec, metric_spec]
-        except (KeyError, SyntaxError):
-            raise BadQueryError
-        try:
-            if "total_network_objects" in query_json:
-                aggregation_unit = query_json["total_network_objects"][
-                    "aggregation_unit"
-                ]
-            else:
-                aggregation_unit = query_json["aggregation_unit"]
-        except KeyError:
-            raise MissingAggregationUnitError
-        return [(query_kind, aggregation_unit)]
+        self.scope_set = scope_set
 
     def can_run(self, *, query_json: dict) -> bool:
         """
@@ -164,50 +52,8 @@ class UserObject:
             If the user cannot run this kind of query at this level of aggregation
 
         """
-        query_kinds_and_aggregations = self._get_query_kinds_and_aggregation_units(
-            query_json=query_json
-        )
 
-        return self.has_access(
-            action="run", query_kinds_and_aggregations=query_kinds_and_aggregations
-        )
-
-    @staticmethod
-    async def _get_params(*, query_id) -> dict:
-        """
-        Get the parameters of a query from flowmachine.
-
-        Parameters
-        ----------
-        query_id : str
-            ID of the query to get params for
-
-        Returns
-        -------
-        dict
-            Dictionary containing the query's original parameters
-
-        Raises
-        ------
-        HTTPException
-            404 if the query id is not known.
-
-        """
-        request.socket.send_json(
-            {
-                "request_id": request.request_id,
-                "action": "get_query_params",
-                "params": {"query_id": query_id},
-            }
-        )
-        reply = await request.socket.recv_json()
-        if reply["status"] == "error":
-            raise HTTPException(
-                description=f"Unknown query ID '{query_id}'",
-                name="Query ID not found",
-                status_code=404,
-            )
-        return reply["payload"]["query_params"]
+        return query_to_scope_set(dict(action="run", **query_json)) in self.scope_set
 
     async def can_poll_by_query_id(self, *, query_id) -> bool:
         """
@@ -229,19 +75,16 @@ class UserObject:
             If the user cannot get the status of this kind of query at this level of aggregation
         """
 
-        params = await self._get_params(query_id=query_id)
-        query_kinds_and_aggregations = self._get_query_kinds_and_aggregation_units(
-            query_json=params
-        )
-        return self.can_poll(query_kinds_and_aggregations=query_kinds_and_aggregations)
+        params = await get_query_parameters_from_flowmachine(query_id=query_id)
+        return self.can_poll(query_json=params)
 
-    def can_poll(self, *, query_kinds_and_aggregations: List[Tuple[str, str]]) -> bool:
+    def can_poll(self, *, query_json: dict) -> bool:
         """
         Returns true if the user can poll this kind of query at this unit of aggregation.
 
         Parameters
         ----------
-        query_kinds_and_aggregations : list of tuples
+        query_json : list of tuples
             List of tuples giving a query kind and aggregation unit
 
         Returns
@@ -255,8 +98,10 @@ class UserObject:
             If the user cannot get the status of this kind of query at this level of aggregation
         """
 
-        return self.has_access(
-            action="poll", query_kinds_and_aggregations=query_kinds_and_aggregations
+        return (
+            query_to_scope_set(dict(action="run", **query_json)) in self.scope_set
+            or query_to_scope_set(dict(action="get_result", **query_json))
+            in self.scope_set
         )
 
     async def can_get_results_by_query_id(self, *, query_id) -> bool:
@@ -278,17 +123,11 @@ class UserObject:
         UserClaimsVerificationError
             If the user cannot get the results of this kind of query at this level of aggregation
         """
-        params = await self._get_params(query_id=query_id)
-        query_kinds_and_aggregations = self._get_query_kinds_and_aggregation_units(
-            query_json=params
-        )
-        return self.can_get_results(
-            query_kinds_and_aggregations=query_kinds_and_aggregations
-        )
+        params = await get_query_parameters_from_flowmachine(query_id=query_id)
 
-    def can_get_results(
-        self, *, query_kinds_and_aggregations: List[Tuple[str, str]]
-    ) -> bool:
+        return self.can_get_results(query_json=params)
+
+    def can_get_results(self, *, query_json: dict) -> bool:
         """
         Returns true if the user can get the results of this kind of query at this unit of aggregation.
         Parameters
@@ -307,9 +146,9 @@ class UserObject:
             If the user cannot get the results of this kind of query at this level of aggregation
         """
 
-        return self.has_access(
-            action="get_result",
-            query_kinds_and_aggregations=query_kinds_and_aggregations,
+        return (
+            query_to_scope_set(dict(action="get_result", **query_json))
+            in self.scope_set
         )
 
     def can_get_geography(self, *, aggregation_unit: str) -> bool:
@@ -332,22 +171,22 @@ class UserObject:
             If the user get geography at this level
         """
 
-        return self.has_access(
-            action="get_result",
-            query_kinds_and_aggregations=[("geography", aggregation_unit)],
+        return (
+            query_to_scope_set(
+                dict(
+                    action="get_result",
+                    query_kind="geography",
+                    aggregation_unit=aggregation_unit,
+                )
+            )
+            in self.scope_set
         )
 
     def can_get_available_dates(self) -> bool:
-        allowed_to_access_available_dates = (
-            self.claims.get("available_dates", {})
-            .get("permissions", {})
-            .get("get_result", False)
+        return (
+            query_to_scope_set(dict(action="get_result", query_kind="available_dates"))
+            in self.scope_set
         )
-        if not allowed_to_access_available_dates:
-            raise UserClaimsVerificationError(
-                f"Token does not allow access to available dates."
-            )
-        return True
 
 
 def user_loader_callback(identity):
@@ -384,4 +223,4 @@ def user_loader_callback(identity):
     )
     current_app.access_logger.info("Loaded user", **log_dict)
 
-    return UserObject(username=identity, claims=claims)
+    return UserObject(username=identity, scope_set=scopes_to_sets(claims))
