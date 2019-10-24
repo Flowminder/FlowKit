d33bdb93f0621f08318d45bad50e9cf6b18060c8
James Harrison <james.harrison@flowminder.org>
2019-10-24 17:21:28 +0100

    Add setup.py

---
+++ automation/setup.py
@@ -0,0 +1,44 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import io
+
+from setuptools import find_packages, setup
+
+import versioneer
+
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="automation",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url="http://github.com/Flowminder/FlowKit",
+    license="MPLv2",
+    maintainer="Flowminder",
+    maintainer_email="flowkit@flowminder.org",
+    description="Automates event-driven Jupyter-notebook-based workflows that interact with FlowAPI.",
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "flowclient",
+        "pyyaml",
+        "papermill >= 1.2.1",
+        "prefect",
+        "nteract-scrapbook",
+        "sh",
+        "pendulum",
+        "get-secret-or-env-var",
+        "sqlalchemy",
+    ],
+    extras_require={
+        "test": ["pytest", "coverage", "flowkit-jwt-generator"],
+        "postgres": ["psycopg2-binary"],
+        "examples": ["pandas", "geopandas", "descartes", "matplotlib"],
+    },
+)
