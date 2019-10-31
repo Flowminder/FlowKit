e02a607a3d0cc9eca8e8333172d25b5a210bd0b3
Jono Gray <jonathan.gray@flowminder.org>
2019-10-31 16:36:29 +0000

    Squashing docstrings

--- flowauth/backend/flowauth/token_management.py
+++ flowauth/backend/flowauth/token_management.py
@@ -191,6 +191,28 @@ def add_token(server):
 # Duplicated in FlowAuth (cannot use this implementation there because
 # this module is outside the docker build context for FlowAuth).
 def squash(xs, ix=0):
+    """
+    Squashes a list of scope strings by combining them where possible.
+
+    Given two strings of the form <action>:<query_kind>:<arg_name>:<arg_val> that differ only
+    in <action>, will yield <action_a>,<action_b>:<query_kind>:<arg_name>:<arg_val>.
+
+    Repeatedly squashes, then if ix < the greatest number of : separated elements in the result, squashes again
+    using the next : separated element.
+
+    Parameters
+    ----------
+    xs : list of str
+        List of scope strings of the form <action>:<query_kind>:<arg_name>:<arg_val>
+    ix : int, default 0
+
+
+    Yields
+    ------
+    str
+        Merged scope string
+
+    """
     sq = {}
     can_squash = False
     for x in xs:
@@ -223,13 +245,20 @@ def squash(xs, ix=0):
 
 def squashed_scopes(scopes: List[str]) -> Iterable[str]:
     """
+    Squashes a list of scope strings by combining them where possible.
+
+    Given two strings of the form <action>:<query_kind>:<arg_name>:<arg_val> that differ only
+    in <action>, will yield <action_a>,<action_b>:<query_kind>:<arg_name>:<arg_val>.
 
     Parameters
     ----------
-    scopes
+    scopes : list of str
+        List of scope strings of the form <action>:<query_kind>:<arg_name>:<arg_val>
 
-    Returns
-    -------
+    Yields
+    ------
+    str
+        Merged scope string
 
     """
     yield from squash(scopes)
