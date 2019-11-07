c670d58aa694f3db1c9bbf468e1ef71148f905d6
Jono Gray <jonathan.gray@flowminder.org>
2019-11-07 16:22:35 +0000

    Don't attempt to handle multiple sms receipients, make event detail tables separate tables that ref the supertable

--- flowdb/bin/build/0030_schema_interactions.sql
+++ flowdb/bin/build/0030_schema_interactions.sql
@@ -14,7 +14,6 @@ subscribers.
                                 data.
   - locations:                  contains a new row for each time a cell
                                 moves
-  - receiving_parties:
   - event_supertable:           contains a row per call/mds/sms event. The
                                 event_type field may be any of 1. calls, 2. sms, 3. mds and
                                 fianlly 4. topup
@@ -55,88 +54,61 @@ CREATE SCHEMA IF NOT EXISTS interactions;
         ON interactions.locations
         USING GIST (position);
 
-    CREATE TABLE IF NOT EXISTS interactions.receiving_parties(
-
-        parties_key             BIGSERIAL PRIMARY KEY,
-        cell_id                 BIGINT REFERENCES interactions.locations(id),
-        party_msisdn            TEXT
-
-        );
-
-
-    CREATE SEQUENCE interactions.event_id START 1;
-    
     CREATE TABLE IF NOT EXISTS interactions.event_supertable (
-        event_id                BIGINT NOT NULL PRIMARY KEY,
+        event_id                BIGSERIAL NOT NULL,
         subscriber_id           BIGINT REFERENCES interactions.subscriber(id),
         cell_id                 BIGINT REFERENCES interactions.locations(id),
         time_sk                 BIGINT REFERENCES public.d_time(time_dim_id),
         date_sk                 BIGINT REFERENCES public.d_date(date_dim_id),
-        event_type              VARCHAR(5) NOT NULL,
-        timestamp               TIMESTAMPTZ NOT NULL
-    );
-
-CREATE TABLE IF NOT EXISTS interactions.calls(
-        event_id                BIGINT NOT NULL DEFAULT nextval('interactions.event_id'),
-        subscriber_id           BIGINT REFERENCES interactions.subscriber(id),
-        cell_id                 BIGINT REFERENCES interactions.locations(id),
-        time_sk                 BIGINT REFERENCES public.d_time(time_dim_id),
-        date_sk                 BIGINT REFERENCES public.d_date(date_dim_id),
-        event_type              VARCHAR(5) NOT NULL DEFAULT 'calls',
         timestamp               TIMESTAMPTZ NOT NULL,
+        PRIMARY KEY (event_id, date_sk)
+    ) PARTITION BY RANGE (date_sk);
+
+    CREATE TABLE IF NOT EXISTS interactions.calls(
+        event_id                BIGINT NOT NULL,
+        date_sk                 BIGINT NOT NULL REFERENCES public.d_date(date_dim_id),
         called_subscriber_id    BIGINT REFERENCES interactions.subscriber(id),
         called_party_cell_id    BIGINT REFERENCES interactions.locations(id),
         calling_party_msisdn    TEXT,
         called_party_msisdn     TEXT,
-        call_duration           NUMERIC
-        CHECK ( event_type = 'calls' ), PRIMARY KEY (event_id, date_sk)
+        call_duration           NUMERIC,
+        PRIMARY KEY (event_id, date_sk),
+        FOREIGN KEY (event_id, date_sk) REFERENCES interactions.event_supertable (event_id, date_sk)
     ) PARTITION BY RANGE (date_sk);
 
     CREATE TABLE IF NOT EXISTS interactions.sms(
-        event_id                      BIGINT NOT NULL DEFAULT nextval('interactions.event_id'),
-        subscriber_id                 BIGINT REFERENCES interactions.subscriber(id),
-        cell_id                       BIGINT REFERENCES interactions.locations(id),
-        time_sk                       BIGINT REFERENCES public.d_time(time_dim_id),
-        date_sk                       BIGINT REFERENCES public.d_date(date_dim_id),
-        event_type                    VARCHAR(5) NOT NULL DEFAULT 'sms',
-        timestamp                     TIMESTAMPTZ NOT NULL,
-        calling_party_cell_id         BIGINT REFERENCES interactions.locations(id),
-        parties_key                   BIGINT REFERENCES interactions.receiving_parties(parties_key),
-        calling_party_msisdn          TEXT,
-        receiving_parties_msisdns     TEXT
-        CHECK ( event_type = 'sms' ), PRIMARY KEY (event_id, date_sk)
+        event_id                BIGINT NOT NULL,
+        date_sk                 BIGINT NOT NULL REFERENCES public.d_date(date_dim_id),
+        called_subscriber_id    BIGINT REFERENCES interactions.subscriber(id),
+        called_party_cell_id    BIGINT REFERENCES interactions.locations(id),
+        calling_party_msisdn    TEXT,
+        called_party_msisdn     TEXT,
+        PRIMARY KEY (event_id, date_sk),
+        FOREIGN KEY (event_id, date_sk) REFERENCES interactions.event_supertable (event_id, date_sk)
     ) PARTITION BY RANGE (date_sk);
 
     CREATE TABLE IF NOT EXISTS interactions.mds(
-        event_id                BIGINT NOT NULL DEFAULT nextval('interactions.event_id'),
-        subscriber_id           BIGINT REFERENCES interactions.subscriber(id),
-        cell_id                 BIGINT REFERENCES interactions.locations(id),
-        time_sk                 BIGINT REFERENCES public.d_time(time_dim_id),
-        date_sk                 BIGINT REFERENCES public.d_date(date_dim_id),
-        event_type              VARCHAR(5) NOT NULL DEFAULT 'mds',
-        timestamp               TIMESTAMPTZ NOT NULL,
+        event_id                BIGINT NOT NULL,
+        date_sk                 BIGINT NOT NULL REFERENCES public.d_date(date_dim_id),
         data_volume_total       NUMERIC,
         data_volume_up          NUMERIC,
         data_volume_down        NUMERIC,
-        duration                NUMERIC
-        CHECK ( event_type = 'mds' ), PRIMARY KEY (event_id, date_sk)
+        duration                NUMERIC,
+        PRIMARY KEY (event_id, date_sk),
+        FOREIGN KEY (event_id, date_sk) REFERENCES interactions.event_supertable (event_id, date_sk)
     ) PARTITION BY RANGE (date_sk);
 
     CREATE TABLE IF NOT EXISTS interactions.topup(
-        event_id                BIGINT NOT NULL DEFAULT nextval('interactions.event_id'),
-        subscriber_id           BIGINT REFERENCES interactions.subscriber(id),
-        cell_id                 BIGINT REFERENCES interactions.locations(id),
-        time_sk                 BIGINT REFERENCES public.d_time(time_dim_id),
-        date_sk                 BIGINT REFERENCES public.d_date(date_dim_id),
-        event_type              VARCHAR(5) NOT NULL DEFAULT 'topup',
-        timestamp               TIMESTAMPTZ NOT NULL,
+        event_id                BIGINT NOT NULL,
+        date_sk                 BIGINT NOT NULL REFERENCES public.d_date(date_dim_id),
         type                    INTEGER,
         recharge_amount         NUMERIC,
         airtime_fee             NUMERIC,
         tax_and_fee             NUMERIC,
         pre_event_balance       NUMERIC,
-        post_event_balance      NUMERIC
-        CHECK ( event_type = 'topup' ), PRIMARY KEY (event_id, date_sk)
+        post_event_balance      NUMERIC,
+        PRIMARY KEY (event_id, date_sk),
+        FOREIGN KEY (event_id, date_sk) REFERENCES interactions.event_supertable (event_id, date_sk)
     ) PARTITION BY RANGE (date_sk);
 
     CREATE TABLE IF NOT EXISTS interactions.subscriber_sightings(
@@ -146,9 +118,10 @@ CREATE TABLE IF NOT EXISTS interactions.calls(
         cell_id                 BIGINT REFERENCES interactions.locations(id),
         time_sk                 BIGINT REFERENCES public.d_time(time_dim_id),
         date_sk                 BIGINT REFERENCES public.d_date(date_dim_id),
-        event_id                BIGINT, /* REFERENCES interactions.event_supertable(event_id), - this can be added in PG12 */
+        event_id                BIGINT,
         timestamp               TIMESTAMPTZ NOT NULL,
-        PRIMARY KEY (sighting_id, date_sk)
+        PRIMARY KEY (sighting_id, date_sk),
+        FOREIGN KEY (event_id, date_sk) REFERENCES interactions.event_supertable (event_id, date_sk)
 
     ) PARTITION BY RANGE (date_sk);
     
