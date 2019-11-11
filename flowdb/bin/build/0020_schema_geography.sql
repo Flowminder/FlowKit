cccd9c73aa1f8ee93ae7fd55d4aab9862114511d
Jono Gray <jonathan.gray@flowminder.org>
2019-11-11 13:52:24 +0000

    Add some missing fields, and a missing geo table

--- flowdb/bin/build/0020_schema_geography.sql
+++ flowdb/bin/build/0020_schema_geography.sql
@@ -11,6 +11,8 @@ This schema is a placeholder for all tables created
 automatically by shapefile and raster file transformation
 scripts.
 
+- geoms:       this contains the geometries and their metadata as referenced by the geo bridge.
+
 - geo_bridge:  this is populated with a link between the 
                infrastructure.cells table, and each of the geography
                types.
@@ -18,13 +20,23 @@ scripts.
 */
 CREATE SCHEMA IF NOT EXISTS geography;
 
-    CREATE TABLE IF NOT EXISTS geography.geo_bridge (
+CREATE TABLE IF NOT EXISTS geography.geoms (
+    gid BIGSERIAL PRIMARY KEY,
+    added_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    short_name VARCHAR, /* e.g. admin3pcod */
+    long_name TEXT, /* e.g. admin3name */
+    kind VARCHAR DEFAULT 'admin_unit', /* The type of the geom - admin unit, coverage polygon etc. */
+    spatial_resolution INT /* Admin level, grid spatial resolution */
+);
+SELECT AddGeometryColumn('geography','geoms','geom','4326','MULTIPOLYGON',2);
+CREATE INDEX "geography_geom_gist" ON "geography"."geoms" USING GIST ("geom");
 
-        cell_id BIGINT,
-        geo_id NUMERIC,
-        valid_from DATE,
-        valid_to DATE,
-        linkage_method TEXT,
-        PRIMARY KEY (cell_id, geo_id, linkage_method)
-
-        );
+CREATE TABLE IF NOT EXISTS geography.geo_bridge (
+    cell_id BIGINT,
+    geo_id BIGINT REFERENCES  geography.geoms(gid),
+    valid_from DATE,
+    valid_to DATE,
+    weight DOUBLE PRECISION DEFAULT 1, /* Where a cell links to multiple geoms, this may be less than 1 */
+    linkage_method TEXT, /* Indicator for how the geometry is linked */
+    PRIMARY KEY (cell_id, geo_id, linkage_method)
+    );
