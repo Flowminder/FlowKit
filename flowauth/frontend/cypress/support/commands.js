95d0014671f40812d35b0380576a2bb5cd7f7a42
Jono Gray <jonathan.gray@flowminder.org>
2019-11-14 10:17:46 +0000

    Add spec upload and edit capabilities tests

--- flowauth/frontend/cypress/support/commands.js
+++ flowauth/frontend/cypress/support/commands.js
@@ -118,3 +118,24 @@ Cypress.Commands.add("goto", { prevSubject: false }, (url, options = {}) => {
     log: false
   });
 });
+
+/*
+Workaround for uploading files in Cypress
+https://github.com/cypress-io/cypress/issues/170#issuecomment-533519550
+ */
+
+Cypress.Commands.add(
+  "uploadFile",
+  { prevSubject: true },
+  (subject, fileName) => {
+    cy.fixture(fileName).then(content => {
+      const el = subject[0];
+      const testFile = new File([JSON.stringify(content)], fileName);
+      const dataTransfer = new DataTransfer();
+
+      dataTransfer.items.add(testFile);
+      el.files = dataTransfer.files;
+      cy.wrap(subject).trigger("change", { force: true });
+    });
+  }
+);
