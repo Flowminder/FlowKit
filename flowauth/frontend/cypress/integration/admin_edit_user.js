17c6e231e87ea42c182a508889814b19109e7721
James Harrison <james.harrison@flowminder.org>
2019-12-03 11:23:35 +0000

    Fix Cypress test

--- flowauth/frontend/cypress/integration/admin_edit_user.js
+++ flowauth/frontend/cypress/integration/admin_edit_user.js
@@ -12,17 +12,20 @@ describe("User management", function() {
     cy.get("#user_list").click();
   });
   it("Edit existing user password", function() {
+    const user_name = Math.random()
+      .toString(36)
+      .substring(2, 15);
     cy.get("#new").click();
     //Add new user with password
-    cy.get("#username").type("USER_TEST");
+    cy.get("#username").type(user_name);
     cy.get("#password").type("DUMMY_ORIGINAL_PASSWORD");
     cy.contains("Save").click();
-    cy.get("#edit_3").click();
+    cy.get("[data-action=edit][data-item-name=" + user_name + "]").click();
     cy.get("#password").type("DUMMY_UPDATED_PASSWORD");
     cy.contains("Save").click();
     cy.request("/signout");
     cy.request("POST", "/signin", {
-      username: "USER_TEST",
+      username: user_name,
       password: "DUMMY_UPDATED_PASSWORD"
     });
   });
