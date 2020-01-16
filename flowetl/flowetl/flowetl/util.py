36b62610f911e719e0a4d2a02e6e48297e6b7f55
Jono Gray <jonathan.gray@flowminder.org>
2020-01-16 14:05:37 +0000

    MPL headers

--- flowetl/flowetl/flowetl/util.py
+++ flowetl/flowetl/flowetl/util.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Callable, Dict, Iterable, List, Optional, Union
--- flowetl/tests/unit/test_wrapping_sql_mixin.py
+++ flowetl/tests/unit/test_wrapping_sql_mixin.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 def test_wrapped_sql():
     from airflow.operators.postgres_operator import PostgresOperator
     from flowetl.mixins.table_name_macros_mixin import TableNameMacrosMixin
