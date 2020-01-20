2b6e97eed7bbd35c43c94b075a77bf7290bacbd5
Jono Gray <jonathan.gray@flowminder.org>
2020-01-20 17:43:49 +0000

    Record counterparts for relevant cdr types

--- flowetl/flowetl/flowetl/operators/update_etl_table_operator.py
+++ flowetl/flowetl/flowetl/operators/update_etl_table_operator.py
@@ -8,8 +8,8 @@ UpdateETLTableOperator = fixed_sql_operator(
     class_name="UpdateETLTableOperator",
     sql="""
         INSERT INTO etl.etl_records (cdr_type, cdr_date, state, timestamp) VALUES ('{{ params.cdr_type }}', '{{ ds }}'::DATE, 'ingested', NOW());
-        INSERT INTO available_tables (table_name, has_locations, has_subscribers) VALUES ('{{ params.cdr_type }}', true, true)
+        INSERT INTO available_tables (table_name, has_locations, has_subscribers{% if params.cdr_type in ['calls', 'sms']  %}, has_counterparts {% endif %}) VALUES ('{{ params.cdr_type }}', true, true{% if params.cdr_type in ['calls', 'sms']  %}, true {% endif %})
             ON conflict (table_name)
-            DO UPDATE SET has_locations=EXCLUDED.has_locations, has_subscribers=EXCLUDED.has_subscribers;
+            DO UPDATE SET has_locations=EXCLUDED.has_locations, has_subscribers=EXCLUDED.has_subscribers{% if params.cdr_type in ['calls', 'sms']  %}, has_counterparts=EXCLUDED.has_counterparts {% endif %};
         """,
 )
