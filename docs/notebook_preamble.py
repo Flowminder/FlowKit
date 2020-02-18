c3e437a0e6d9dcef4b59235298ce117f7d2d89bc
Jono Gray <jonathan.gray@flowminder.org>
2020-02-18 14:13:17 +0000

    Bump docs

--- docs/notebook_preamble.py
+++ docs/notebook_preamble.py
@@ -5,7 +5,6 @@
 # This file is executed before running any Jupyter notebook during the docs build.
 # It is set as the 'preamble' option to the 'mknotebooks' plugin in mkdocs.yml
 
-import pandas as pd
 import pprint
 import warnings
 import logging
@@ -27,31 +26,10 @@ warnings.filterwarnings("ignore")
 logging.getLogger().setLevel(logging.ERROR)
 
 
-# Format pandas tables nicely
-
-
-def to_md(df):
-    """
-    Wrapper around `DataFrame.to_markdown`, to display MultiIndex headers on multiple lines.
-    """
-    if df.columns.nlevels == 1:
-        col_headers = map(str, df.columns.tolist())
-    else:
-        col_headers = ("<br>".join(map(str, col)) for col in df.columns.tolist())
-    headers = ["<br>".join((name or "" for name in df.columns.names)) + "<br><br>"] + [
-        h + "<br><br>" for h in col_headers
-    ]
-    if any(df.index.names):
-        headers[0] += ", ".join(map(str, df.index.names))
-        headers[1:] = [h + "&nbsp;" for h in headers[1:]]
-    return df.to_markdown(headers=headers)
-
-
 def format_dict(x):
     return f'><div class="codehilite"><pre>{pprint.pformat(x)}</pre></div>'
 
 
-get_ipython().display_formatter.formatters["text/html"].for_type(pd.DataFrame, to_md)
 get_ipython().display_formatter.formatters["text/markdown"].for_type(dict, format_dict)
 get_ipython().display_formatter.formatters["text/markdown"].for_type(
     str, lambda x: f">`{x}`"
