dfd5bb17014c4fe19e1fc5c2d02d1633d4563cec
James Harrison <james.harrison@flowminder.org>
2020-02-27 16:18:11 +0000

    Truncate long table entries, so we can display geopandas dataframes

--- docs/notebook_preamble.py
+++ docs/notebook_preamble.py
@@ -30,28 +30,45 @@ logging.getLogger().setLevel(logging.ERROR)
 # Format pandas tables nicely
 
 
-def to_md(df):
+def to_md(df, max_colwidth=50):
     """
-    Wrapper around `DataFrame.to_markdown`, to display MultiIndex headers on multiple lines.
+    Wrapper around `DataFrame.to_markdown`, to display MultiIndex headers on multiple lines,
+    and truncate entries longer than max_colwidth characters.
     """
     if df.columns.nlevels == 1:
         col_headers = map(str, df.columns.tolist())
     else:
+        # If multi-level column headers, split over multiple lines
         col_headers = ("<br>".join(map(str, col)) for col in df.columns.tolist())
-    headers = ["<br>".join((name or "" for name in df.columns.names)) + "<br><br>"] + [
-        h + "<br><br>" for h in col_headers
-    ]
+    # Add column level names in left-hand column
+    headers = ["<br>".join((name or "" for name in df.columns.names))] + list(
+        col_headers
+    )
     if any(df.index.names):
-        headers[0] += ", ".join(map(str, df.index.names))
-        headers[1:] = [h + "&nbsp;" for h in headers[1:]]
-    return df.to_markdown(headers=headers)
+        # Add index name (if there is one) on a new header line
+        headers[0] += "<br><br>" + ", ".join(map(str, df.index.names))
+        headers[1:] = [h + "<br><br>&nbsp;" for h in headers[1:]]
+
+    # Truncate entries wider than max_colwidth
+    return (
+        df.astype(str)
+        .apply(
+            lambda x: [
+                (a if len(a) <= max_colwidth else a[: max_colwidth - 3] + "...")
+                for a in x
+            ]
+        )
+        .to_markdown(headers=headers)
+    )
 
 
 def format_dict(x):
     return f'><div class="codehilite"><pre>{pprint.pformat(x)}</pre></div>'
 
 
-get_ipython().display_formatter.formatters["text/html"].for_type(pd.DataFrame, to_md)
+get_ipython().display_formatter.formatters["text/markdown"].for_type(
+    pd.DataFrame, to_md
+)
 get_ipython().display_formatter.formatters["text/markdown"].for_type(dict, format_dict)
 get_ipython().display_formatter.formatters["text/markdown"].for_type(
     str, lambda x: f">`{x}`"
