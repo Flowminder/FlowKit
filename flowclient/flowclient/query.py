494d6510ae6ed27d94b938dc7ab0fbab505a5057
James Harrison <james.harrison@flowminder.org>
2020-03-12 11:30:06 +0000

    Run query if it's not running

--- flowclient/flowclient/query.py
+++ flowclient/flowclient/query.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from flowclient.client import (
+    FlowclientConnectionError,
     Connection,
     run_query,
     get_status,
@@ -17,7 +18,7 @@ class Query:
         self.parameters = parameters  # TODO: make this a property? (For immutability; otherwise parameters could differ from query ID / result)
 
     def run(self) -> None:
-        self._query_id = run_query(
+        self.query_id = run_query(
             connection=self.connection, query_spec=self.parameters
         )
         # TODO: Return a future?
@@ -31,8 +32,9 @@ class Query:
         # Update connection (primarily intended for use when a token expires)
         if hasattr(self, "_connection") and new_connection.url != self._connection.url:
             # If new URL is for a different API, invalidate query ID and result
+            # TODO: Or should we disallow this?
             try:
-                delattr(self, "_query_id")
+                delattr(self, "query_id")
             except AttributeError:
                 pass
             try:
@@ -43,30 +45,34 @@ class Query:
 
     @property
     def status(self) -> str:
-        try:
-            query_id = self._query_id
-        except AttributeError:
+        if not hasattr(self, "query_id"):
             return "unknown"
-        status, _ = get_status(connection=self.connection, query_id=query_id)
+        status, _ = get_status(connection=self.connection, query_id=self.query_id)
         return status
 
     @property
     def result(self) -> "pandas.DataFrame":
         if not hasattr(self, "_result"):
             # Don't have result yet
-            if not hasattr(self, "_query_id"):
-                # Query hasn't been set running
-                # TODO: Warn?
+            # TODO: Warn if not yet completed?
+            try:
+                self._result = get_result_by_query_id(
+                    connection=self.connection, query_id=self.query_id
+                )
+            except AttributeError, FlowclientConnectionError:
+                # TODO: Warn before running?
                 self.run()
-            self._result = get_result_by_query_id(
-                connection=self.connection, query_id=self._query_id
-            )
+                self._result = get_result_by_query_id(
+                    connection=self.connection, query_id=self.query_id
+                )
         # Return a copy, to avoid modifying the result
         return self._result.copy()
 
     def wait_until_ready(self, poll_interval: int = 1) -> None:
+        if not hasattr(self, "query_id"):
+            raise FlowclientConnectionError("Query is not running.")
         wait_for_query_to_be_ready(
             connection=self.connection,
-            query_id=self._query_id,
+            query_id=self.query_id,
             poll_interval=poll_interval,
         )
