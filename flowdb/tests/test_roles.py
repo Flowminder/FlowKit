1b6c99aa18c433cbbe52bd130f496a7d70a86a84
Jono Gray <jonathan.gray@flowminder.org>
2020-03-13 12:37:07 +0000

    Fix some deprecated pytest usage

--- flowdb/tests/test_roles.py
+++ flowdb/tests/test_roles.py
@@ -50,121 +50,141 @@ def test_tables(env):
     return tables
 
 
-@pytest.mark.usefixtures("create_test_tables")
-@pytest.mark.parametrize("usr_env_prefix", ["flowmachine", "flowapi"], scope="class")
-class TestRole(object):
-    @pytest.fixture(scope="class", autouse=True)
-    def pwd(self, usr_env_prefix, env):
-        """
-        Returns the password for the given usr_env_prefix.
+@pytest.fixture(params=["flowmachine", "flowapi"])
+def user(request, create_test_tables):
+    return request.param
 
-        Returns
-        -------
-        str
-            The usr password.
-        """
-        return env["{}_FLOWDB_PASSWORD".format(usr_env_prefix.upper())]
 
-    @pytest.fixture(scope="class", autouse=True)
-    def usr(self, usr_env_prefix, env):
-        """
-        Returns the usr for the given usr_env_prefix.
+@pytest.fixture(autouse=True)
+def pwd(user, env):
+    """
+    Returns the password for the given usr_env_prefix.
 
-        Returns
-        -------
-        str
-            The usr
-        """
-        return env["{}_FLOWDB_USER".format(usr_env_prefix.upper())]
+    Returns
+    -------
+    str
+        The usr password.
+    """
+    return env["{}_FLOWDB_PASSWORD".format(user.upper())]
 
-    def test_cannot_drop_events(self, cursor):
-        """Role cannot DROP TABLE on events."""
-        with pytest.raises(pg.ProgrammingError):
-            cursor.execute("DROP TABLE events.calls")
 
-    @pytest.mark.skip_usrs(["flowapi"])
-    def test_select_events(self, cursor):
-        """Role can do SELECT on events.calls."""
+@pytest.fixture(autouse=True)
+def usr(user, env):
+    """
+    Returns the usr for the given usr_env_prefix.
+
+    Returns
+    -------
+    str
+        The usr
+    """
+    return env["{}_FLOWDB_USER".format(user.upper())]
+
+
+def test_cannot_drop_events(cursor):
+    """Role cannot DROP TABLE on events."""
+    with pytest.raises(pg.ProgrammingError):
+        cursor.execute("DROP TABLE events.calls")
+
+
+@pytest.mark.skip_usrs(["flowapi"])
+def test_select_events(cursor, user):
+    """Role can do SELECT on events.calls."""
+    cursor.execute("SELECT * FROM events.calls")
+
+    cursor.execute("SELECT * FROM routing.foo")
+
+
+@pytest.mark.skip_usrs(["flowapi"])
+def test_select_new_events(cursor, user):
+    """Role can read new tables created under the events schema."""
+    cursor.execute("SELECT * FROM events.calls_20160101;")
+
+
+@pytest.mark.skip_usrs(["flowmachine"])
+def test_cannot_select_events(cursor, user):
+    """Role cannot do SELECT on events.calls."""
+    with pytest.raises(pg.ProgrammingError):
         cursor.execute("SELECT * FROM events.calls")
 
-        cursor.execute("SELECT * FROM routing.foo")
 
-    @pytest.mark.skip_usrs(["flowapi"])
-    def test_select_new_events(self, cursor):
-        """Role can read new tables created under the events schema."""
-        cursor.execute("SELECT * FROM events.calls_20160101;")
-
-    @pytest.mark.skip_usrs(["flowmachine"])
-    def test_cannot_select_events(self, cursor):
-        """Role cannot do SELECT on events.calls."""
-        with pytest.raises(pg.ProgrammingError):
-            cursor.execute("SELECT * FROM events.calls")
-
-    def test_cannot_create_events(self, cursor):
-        """Role cannot create tables in the events.* schema."""
-        with pytest.raises(pg.ProgrammingError):
-            cursor.execute(
-                """
-                CREATE TABLE events.calls_20160102 () INHERITS (events.calls)
+def test_cannot_create_events(cursor):
+    """Role cannot create tables in the events.* schema."""
+    with pytest.raises(pg.ProgrammingError):
+        cursor.execute(
             """
-            )
+            CREATE TABLE events.calls_20160102 () INHERITS (events.calls)
+        """
+        )
 
-    def test_create_public(self, cursor):
-        """Role can CREATE TABLE in public."""
-        cursor.execute("CREATE TABLE foo(id TEXT)")
 
-    def test_drop_public(self, cursor):
-        """Role can DROP TABLE in public."""
-        cursor.execute("CREATE TABLE foo(id TEXT)")
-        cursor.execute("DROP TABLE foo")
+def test_create_public(cursor):
+    """Role can CREATE TABLE in public."""
+    cursor.execute("CREATE TABLE foo(id TEXT)")
 
-    @pytest.mark.skip_usrs(["flowmachine"])
-    def test_cannot_drop_geo(self, cursor):
-        """Flowapi cannot drop geography tables."""
-        with pytest.raises(pg.ProgrammingError):
-            cursor.execute("DROP TABLE geography.admin0")
 
-    @pytest.mark.skip_usrs(["flowmachine"])
-    def test_cannot_drop_cache_tables(self, cursor):
-        """Flowapi cannot drop cache tables"""
-        with pytest.raises(pg.ProgrammingError):
-            cursor.execute("DROP TABLE cache.blah")
+def test_drop_public(cursor):
+    """Role can DROP TABLE in public."""
+    cursor.execute("CREATE TABLE foo(id TEXT)")
+    cursor.execute("DROP TABLE foo")
 
-    def test_cannot_drop_cache_metadata_table(self, cursor):
-        """Role cannot drop cache.cached"""
-        with pytest.raises(pg.ProgrammingError):
-            cursor.execute("DROP TABLE cache.cached")
 
-    @pytest.mark.skip_usrs(["flowmachine"])
-    def test_cannot_select_cache_metadata_table(self, cursor):
-        """Flowapi cannot read cache metadata"""
-        with pytest.raises(pg.ProgrammingError):
-            cursor.execute("SELECT * FROM cache.cached")
+@pytest.mark.skip_usrs(["flowmachine"])
+def test_cannot_drop_geo(cursor, user):
+    """Flowapi cannot drop geography tables."""
+    with pytest.raises(pg.ProgrammingError):
+        cursor.execute("DROP TABLE geography.admin0")
 
-    def test_cannot_drop_cache_dependencies_table(self, cursor):
-        """Role cannot drop cache metadata"""
-        with pytest.raises(pg.ProgrammingError):
-            cursor.execute("DROP TABLE cache.dependencies")
 
-    @pytest.mark.skip_usrs(["flowapi"])
-    def test_can_drop_cache_tables(self, cursor):
-        """Flowmachine can drop cache tables"""
+@pytest.mark.skip_usrs(["flowmachine"], user)
+def test_cannot_drop_cache_tables(cursor):
+    """Flowapi cannot drop cache tables"""
+    with pytest.raises(pg.ProgrammingError):
         cursor.execute("DROP TABLE cache.blah")
 
-    def test_select_cache(self, cursor):
-        """Role can do SELECT on cache tables."""
-        cursor.execute("SELECT * FROM cache.blah")
 
-    def test_select_geo(self, cursor):
-        """Everybody can SELECT from the geography tables."""
-        cursor.execute("SELECT * FROM geography.admin0")
+def test_cannot_drop_cache_metadata_table(cursor):
+    """Role cannot drop cache.cached"""
+    with pytest.raises(pg.ProgrammingError):
+        cursor.execute("DROP TABLE cache.cached")
 
-    @pytest.mark.skip_usrs(["flowapi"])
-    def test_select_cache_metadata_table(self, cursor):
-        """Role can do SELECT on cache tables."""
+
+@pytest.mark.skip_usrs(["flowmachine"])
+def test_cannot_select_cache_metadata_table(cursor, user):
+    """Flowapi cannot read cache metadata"""
+    with pytest.raises(pg.ProgrammingError):
         cursor.execute("SELECT * FROM cache.cached")
 
-    @pytest.mark.skip_usrs(["flowapi"])
-    def test_select_cache_dependencies_table(self, cursor):
-        """Flowmachine can do SELECT on cache dependencies table."""
-        cursor.execute("SELECT * FROM cache.dependencies")
+
+def test_cannot_drop_cache_dependencies_table(cursor):
+    """Role cannot drop cache metadata"""
+    with pytest.raises(pg.ProgrammingError):
+        cursor.execute("DROP TABLE cache.dependencies")
+
+
+@pytest.mark.skip_usrs(["flowapi"])
+def test_can_drop_cache_tables(cursor, user):
+    """Flowmachine can drop cache tables"""
+    cursor.execute("DROP TABLE cache.blah")
+
+
+def test_select_cache(cursor):
+    """Role can do SELECT on cache tables."""
+    cursor.execute("SELECT * FROM cache.blah")
+
+
+def test_select_geo(cursor):
+    """Everybody can SELECT from the geography tables."""
+    cursor.execute("SELECT * FROM geography.admin0")
+
+
+@pytest.mark.skip_usrs(["flowapi"])
+def test_select_cache_metadata_table(cursor, user):
+    """Role can do SELECT on cache tables."""
+    cursor.execute("SELECT * FROM cache.cached")
+
+
+@pytest.mark.skip_usrs(["flowapi"])
+def test_select_cache_dependencies_table(cursor, user):
+    """Flowmachine can do SELECT on cache dependencies table."""
+    cursor.execute("SELECT * FROM cache.dependencies")
