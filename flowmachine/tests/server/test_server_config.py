fcd16d992bdba7b166ffb4ecc5684b0db7405a73
Jono Gray <jonathan.gray@flowminder.org>
2020-04-28 21:36:17 +0100

    Update test_server_config.py

--- flowmachine/tests/server/test_server_config.py
+++ flowmachine/tests/server/test_server_config.py
@@ -39,14 +39,16 @@ def test_get_server_config(monkeypatch):
     monkeypatch.setenv("FLOWMACHINE_CACHE_PRUNING_FREQUENCY", 1)
     monkeypatch.setenv("FLOWMACHINE_CACHE_PRUNING_TIMEOUT", 2)
     monkeypatch.setenv("FLOWMACHINE_SERVER_THREADPOOL_SIZE", 1)
+    monkeypatch.setenv("FLOWMACHINE_EVENT_LOOP", "asyncio")
     config = get_server_config()
-    assert len(config) == 6
+    assert len(config) == 7
     assert config.port == 5678
     assert config.debug_mode
     assert not config.store_dependencies
     assert config.cache_pruning_timeout == 2
     assert config.cache_pruning_frequency == 1
     assert config.server_thread_pool._max_workers == 1
+    assert config.event_loop == "asyncio"
 
 
 def test_get_server_config_defaults(monkeypatch):
@@ -59,11 +61,13 @@ def test_get_server_config_defaults(monkeypatch):
     monkeypatch.delenv("FLOWMACHINE_CACHE_PRUNING_FREQUENCY", raising=False)
     monkeypatch.delenv("FLOWMACHINE_CACHE_PRUNING_TIMEOUT", raising=False)
     monkeypatch.delenv("FLOWMACHINE_SERVER_THREADPOOL_SIZE", raising=False)
+    monkeypatch.delenv("FLOWMACHINE_EVENT_LOOP", raising=False)
     config = get_server_config()
-    assert len(config) == 6
+    assert len(config) == 7
     assert config.port == 5555
     assert not config.debug_mode
     assert config.store_dependencies
     assert config.cache_pruning_timeout == 600
     assert config.cache_pruning_frequency == 86400
     assert config.server_thread_pool._max_workers == min(32, os.cpu_count() + 4)
+    assert config.event_loop == "uvloop"
