c9e6e5116394d0773ebfdaab8d73b54c24e198aa
Jonathan Gray <jonathan.gray@flowminder.org>
2020-09-24 13:42:51 +0100

    Try out an alternative approach to redacting tle

--- flowmachine/flowmachine/features/location/redacted_total_events.py
+++ flowmachine/flowmachine/features/location/redacted_total_events.py
@@ -1,13 +1,22 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from functools import reduce
+
+import numpy as np
 
 from flowmachine.core.query import Query
+from flowmachine.features import UniqueSubscriberCounts
+from flowmachine.features.location.redacted_unique_subscriber_counts import (
+    RedactedUniqueSubscriberCounts,
+)
 from flowmachine.features.location.total_events import TotalLocationEvents
 from flowmachine.features.location.redacted_location_metric import (
     RedactedLocationMetric,
 )
-from flowmachine.utils import make_where
+from flowmachine.utils import standardise_date
+import datetime
+import pandas as pd
 
 
 class RedactedTotalEvents(RedactedLocationMetric, Query):
@@ -23,50 +32,59 @@ class RedactedTotalEvents(RedactedLocationMetric, Query):
         Total location events query to redact
     """
 
+    interval_to_pandas = dict(day="D", min="T", hour="H")
+    interval_to_delta_arg = dict(day="days", min="minutes", hour="hours")
+
     def __init__(self, total_events: TotalLocationEvents):
         self.redaction_target = total_events
+        # Construct redacted unique subscriber counts for each bucket
+        self.counts = []
+        self.additional_colums = []
+        for d in pd.date_range(
+            total_events.start,
+            total_events.stop,
+            freq=self.interval_to_pandas[total_events.interval],
+        ):
+            delta = d + datetime.timedelta(
+                **{self.interval_to_delta_arg[total_events.interval]: 1}
+            )
+            if total_events.hours != "all":
+                if not (total_events.hours[0] >= delta.hour >= total_events.hours[1]):
+                    continue
+            self.additional_colums.append(
+                (d.date, d.getattr(self.interval_to_delta_arg[total_events.interval]))
+            )
+            self.counts.append(
+                RedactedUniqueSubscriberCounts(
+                    unique_subscriber_counts=UniqueSubscriberCounts(
+                        d,
+                        d
+                        + datetime.timedelta(
+                            **{self.interval_to_delta_arg[total_events.interval]: 1}
+                        ),
+                        spatial_unit=total_events.spatial_unit,
+                        table=total_events.table,
+                        hours=total_events.hours,
+                    ),
+                )
+            )
         super().__init__()
 
     def _make_query(self):
-
-        # list of columns that we want to group by, these are all the time
-        # columns, plus the location columns
-        groups = [
-            x.split(" AS ")[0] for x in self.redaction_target.time_cols
-        ] + self.redaction_target.spatial_unit.location_id_columns
-
-        returning_columns = ", ".join(
-            [
-                x.split(" AS ")[-1]
-                for x in self.redaction_target.spatial_unit.location_id_columns
+        count_qs = [q.get_query() for q in self.counts]
+        if self.redaction_target.interval != "day":
+            count_qs = [
+                f"""SELECT {standardise_date(added[0])} as date, {added[1]} as {self.redaction_target.interval}, *
+                        FROM ({count_q}) as _
+                       """
+                for added, count_q in zip(self.additional_colums, count_qs)
             ]
-        )
 
-        returning_time_columns = ", ".join(
-            [x.split(" AS ")[-1] for x in self.redaction_target.time_cols]
-        )
-        # We now need to group this table by the relevant columns in order to
-        # get a count per region
-        sql = f"""
-            WITH tne AS (SELECT
-                {', '.join(self.redaction_target.spatial_unit.location_id_columns)},
-                {', '.join(self.redaction_target.time_cols)},
-                count(*) AS value,
-                count(distinct subscriber) > 15 AS safe_agg
-            FROM
-                ({self.redaction_target.unioned.get_query()}) unioned
-            {make_where(self.redaction_target.direction.get_filter_clause())}
-            GROUP BY
-                {', '.join(groups)})
-                
-            SELECT {returning_columns}, 
-                {returning_time_columns},
-                value
-            FROM tne NATURAL JOIN
-            (SELECT {returning_columns} FROM tne 
-            GROUP BY {returning_columns}
-            HAVING every(safe_agg)
-            ) _
+        redact_filter = reduce(lambda x, y: f"{x} UNION ALL {y}", count_qs)
+
+        return f"""
+        SELECT tne.* FROM
+        ({self.redaction_target.get_query()}) tne
+        NATURAL INNER JOIN
+        ({redact_filter}) redactor
         """
-
-        return sql
