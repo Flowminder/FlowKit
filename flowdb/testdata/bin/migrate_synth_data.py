9d851245732f216008b5c34d993f7d8348040daf
Jonathan Gray <jonathan.gray@flowminder.org>
2021-01-11 13:35:12 +0000

    Wrap partitioning in single transactions

--- flowdb/testdata/bin/migrate_synth_data.py
+++ flowdb/testdata/bin/migrate_synth_data.py
@@ -123,27 +123,26 @@ if __name__ == "__main__":
                 ).fetchall()
             ]
         init_futures = [locations_fut, subscribers_fut]
+        partitions_sql = []
         for dt in available_dates:
-            init_futures.append(
-                tp.submit(
-                    do_exec,
-                    (
-                        f"""CREATE TABLE interactions.events_supertable_{dt.strftime("%Y_%m_%d")} PARTITION OF interactions.event_supertable FOR VALUES FROM ({dt.strftime("%Y%m%d")}) TO ({(dt + datetime.timedelta(days=1)).strftime("%Y%m%d")});""",
-                        f"Adding events partition for {dt}.",
+            partitions_sql = [
+                *partitions_sql,
+                f"""CREATE TABLE interactions.events_supertable_{dt.strftime("%Y%m%d")} PARTITION OF interactions.event_supertable FOR VALUES FROM ({dt.strftime("%Y%m%d")}) TO ({(dt + datetime.timedelta(days=1)).strftime("%Y%m%d")});""",
+                f"""CREATE TABLE interactions.subscriber_sightings_{dt.strftime("%Y%m%d")} PARTITION OF interactions.subscriber_sightings FOR VALUES FROM ({dt.strftime("%Y%m%d")}) TO ({(dt + datetime.timedelta(days=1)).strftime("%Y%m%d")});""",
+            ]
+        init_futures = [
+            *init_futures,
+            tp.submit(
+                do_exec,
+                (
+                    "\n".join(
+                        partitions_sql,
+                        "Adding partitions for events and sighting.",
                         engine,
-                    ),
-                )
-            )
-            init_futures.append(
-                tp.submit(
-                    do_exec,
-                    (
-                        f"""CREATE TABLE interactions.subscriber_sightings_{dt.strftime("%Y_%m_%d")} PARTITION OF interactions.subscriber_sightings FOR VALUES FROM ({dt.strftime("%Y%m%d")}) TO ({(dt + datetime.timedelta(days=1)).strftime("%Y%m%d")});""",
-                        f"Adding sightings partition for {dt}.",
-                        engine,
-                    ),
-                )
-            )
+                    )
+                ),
+            ),
+        ]
 
         with engine.begin():
             available_dates = [
@@ -152,17 +151,23 @@ if __name__ == "__main__":
                     "SELECT DISTINCT cdr_date, cdr_type FROM etl.etl_records;"
                 ).fetchall()
             ]
-        for dt, typ in available_dates:
-            init_futures.append(
-                tp.submit(
-                    do_exec,
-                    (
-                        f"""CREATE TABLE interactions.{typ}_{dt.strftime("%Y_%m_%d")} PARTITION OF interactions.{typ} FOR VALUES FROM ({dt.strftime("%Y%m%d")}) TO ({(dt + datetime.timedelta(days=1)).strftime("%Y%m%d")});""",
-                        f"Adding event subtype partition for {dt}.",
+        partitions_sql = [
+            f"""CREATE TABLE interactions.{typ}_{dt.strftime("%Y%m%d")} PARTITION OF interactions.{typ} FOR VALUES FROM ({dt.strftime("%Y%m%d")}) TO ({(dt + datetime.timedelta(days=1)).strftime("%Y%m%d")});"""
+            for dt, typ in available_dates
+        ]
+        init_futures = [
+            *init_futures,
+            tp.submit(
+                do_exec,
+                (
+                    "\n".join(
+                        partitions_sql,
+                        "Adding partitions for event metadata.",
                         engine,
-                    ),
-                )
-            )
+                    )
+                ),
+            ),
+        ]
         [fut.result() for fut in wait(init_futures).done]
         calls_fut = tp.submit(
             do_exec,
