1c74c84b569fae3e84c9069c59cc26ea8cb04b94
john <john.roberts@flowminder.org>
2021-10-06 17:01:52 +0100

    Initial outline of query with dummy SQL in place

---
+++ flowmachine/flowmachine/core/server/query_schemas/mobility_estimation.py
@@ -0,0 +1,32 @@
+from marshmallow import fields
+from marshmallow.validate import OneOf, Range
+
+from flowmachine.features.subscriber.mobility_estimation import MobilityEstimation
+
+from .base_exposed_query import BaseExposedQuery
+
+from .base_schema import BaseSchema
+from .aggregation_unit import AggregationUnitKind
+from .field_mixins import StartAndEndField
+
+class MobilityEstimationExposed(BaseExposedQuery):
+    def __init__(
+        self,
+        *,
+        start_date,
+        end_date,
+        aggregation_unit
+    ):
+        self.start = start_date
+        self.stop = end_date
+        self.agg_unit = aggregation_unit
+
+    @property
+    def _flowmachine_query_obj(self):
+        return (
+            MobilityEstimation(self.start, self.stop, self.agg_unit)
+        )
+
+class MobilityEstimationSchema(StartAndEndField, BaseSchema):
+    query_kind = fields.String(valiate=OneOf(["mobility_estimation"]))
+    agg_unit = fields.Nested(AggregationUnitKind)
---
+++ flowmachine/flowmachine/features/subscriber/mobility_estimation.py
@@ -0,0 +1,38 @@
+from typing import List
+from flowmachine.core.query import Query
+
+class MobilityEstimation(Query):
+
+    def __init__(
+            self,
+            start,
+            stop,
+            agg_unit,
+    ):
+        pass
+
+    @property
+    def column_names(self) -> List[str]:
+        return [
+            "month",
+            "resident_count",
+            "resident_count_change",
+            "resident_count_change_percent",
+            "resident_count_change_median",
+            "resident_count_change_median_percent",
+            "high_mobility_resident_percent",
+            "displaced_residents_percent"
+        ]
+
+    def _make_query(self):
+        return f"""
+        SELECT 
+        1 as month,
+        2 AS resident_count,
+        3 AS resident_count_change,
+        4 AS resident_count_change_percent,
+        5 AS resident_count_change_median
+        6 AS resident_count_change_median_percent,
+        7 AS high_mobility_resident_percent,
+        8 AS displaced_residents_percent
+        """
