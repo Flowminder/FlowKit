f5acdd217472f65af268065bf7f52b8312d997b8
john <john.roberts@flowminder.org>
2021-10-12 11:11:57 +0100

    Fixing bug and blacking

--- flowmachine/tests/test_mobility_estimation.py
+++ flowmachine/tests/test_mobility_estimation.py
@@ -1,12 +1,15 @@
-from flowmachine.core.server.query_schemas.mobility_estimation import MobilityEstimationExposed
+from flowmachine.core.server.query_schemas.mobility_estimation import (
+    MobilityEstimationExposed,
+)
+
 
 def test_exposed_mobility_estimation(mocked_connections):
     query = MobilityEstimationExposed(
-        start_date="01-02-2021",
-        end_date="02-02-2021",
-        aggregation_unit="admin_1"
+        start_date="01-02-2021", end_date="02-02-2021", aggregation_unit="admin_1"
     )
-    assert query._flowmachine_query_obj._make_sql("test")[0] == f"""
+    assert (
+        query._flowmachine_query_obj._make_sql("test")[0]
+        == f"""
     'EXPLAIN (ANALYZE TRUE, TIMING FALSE, FORMAT JSON) CREATE TABLE test AS (
         SELECT 
         1 as month,
@@ -19,3 +22,4 @@ def test_exposed_mobility_estimation(mocked_connections):
         8 AS displaced_residents_percent
     )
         """
+    )
