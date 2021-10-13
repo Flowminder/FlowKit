b32df6493044c3985fb63b419167944a7155d3a1
john <john.roberts@flowminder.org>
2021-10-13 12:25:36 +0100

    Prototype query that works over two months in place
    Query returns:
    area
    Dummy for month
    resident_count (needs confirmation with analysis team)
    resident change from last month
    Percent resident change from last month
    Dummy for resident change against year median
    Nonsense for percent resident change against year median (calc'd from dummy)
    Dummy for high-mobility resident percent
    Dummy for displacement percent
    
    Used new library dateutils to do date funcs, so please update your pipenvs.

--- flowmachine/tests/test_mobility_estimation.py
+++ flowmachine/tests/test_mobility_estimation.py
@@ -1,25 +1,49 @@
+from decimal import Decimal
+
 from flowmachine.core.server.query_schemas.mobility_estimation import (
     MobilityEstimationExposed,
 )
+from flowmachine.features.subscriber.mobility_estimation import MobilityEstimation
 
 
 def test_exposed_mobility_estimation(mocked_connections):
     query = MobilityEstimationExposed(
-        start_date="01-02-2021", end_date="02-02-2021", aggregation_unit="admin_1"
+        start_date="2021-01-01", end_date="2021-02-01", aggregation_unit="admin3"
     )
     assert (
         query._flowmachine_query_obj._make_sql("test")[0]
         == f"""
-    'EXPLAIN (ANALYZE TRUE, TIMING FALSE, FORMAT JSON) CREATE TABLE test AS (
+('EXPLAIN (ANALYZE TRUE, TIMING FALSE, FORMAT JSON) CREATE TABLE test AS 
+
+        (SELECT month, resident_count, resident_count_change,
+resident_count_change_percent, resident_count_change_median,
+resident_count_change_median_percent, high_mobility_resident_percent,
+displaced_residents_percent FROM (
         SELECT 
         1 as month,
         2 AS resident_count,
         3 AS resident_count_change,
         4 AS resident_count_change_percent,
-        5 AS resident_count_change_median
+        5 AS resident_count_change_median,
         6 AS resident_count_change_median_percent,
         7 AS high_mobility_resident_percent,
         8 AS displaced_residents_percent
-    )
+        ) _)'
         """
+    )
+
+
+def test_mobility_estimation():
+    query = MobilityEstimation(start="2016-01-01", stop="2016-02-01", agg_unit="admin3")
+    out = next(iter(query))
+    assert out == (
+        "NPL.1.1.1_1",
+        9999,
+        947,
+        136,
+        Decimal("16.76942046855733662100"),
+        -9052,
+        Decimal("-1.50243040212107821500"),
+        9999,
+        9999,
     )
