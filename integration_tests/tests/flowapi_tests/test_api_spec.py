484aa785a0433b95a7818c9119460eb156034b1b
Jonathan Gray <jonathan.gray@flowminder.org>
2021-10-19 23:29:20 +0100

    Need to specify loader for yaml.load now

--- integration_tests/tests/flowapi_tests/test_api_spec.py
+++ integration_tests/tests/flowapi_tests/test_api_spec.py
@@ -20,7 +20,10 @@ def test_generated_openapi_yaml_spec(flowapi_url, diff_reporter):
     """
     Verify the OpenAPI spec for FlowAPI in yaml form.
     """
-    spec = yaml.load(requests.get(f"{flowapi_url}/api/0/spec/openapi.yaml").content)
+    spec = yaml.load(
+        requests.get(f"{flowapi_url}/api/0/spec/openapi.yaml").content,
+        Loader=yaml.CLoader,
+    )
     spec_version = spec["info"].pop("version")
     assert spec_version == flowapi.__version__
     spec_as_json_string = json.dumps(sort_recursively(spec), indent=2, sort_keys=True)
