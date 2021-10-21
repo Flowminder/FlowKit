61ed2fd226494b0111046d2262415aba69808b62
Jonathan Gray <jonathan.gray@flowminder.org>
2021-10-21 12:56:58 +0100

    Fix another rebase change

--- flowmachine/flowmachine/features/utilities/event_table_subset.py
+++ flowmachine/flowmachine/features/utilities/event_table_subset.py
@@ -133,11 +133,6 @@ class EventTableSubset(Query):
                 )
         self.columns = sorted(self.columns)
 
-        self.sqlalchemy_table = get_sqlalchemy_table_definition(
-            self.table_ORIG.fully_qualified_table_name,
-            engine=get_db().engine,
-        )
-
         if self.start == self.stop:
             raise ValueError("Start and stop are the same.")
 
