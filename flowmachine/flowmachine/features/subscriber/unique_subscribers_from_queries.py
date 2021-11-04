0fd9ae14b3ccf3886aacaf0281eb9e7259dccace
john <john.roberts@flowminder.org>
2021-11-04 16:45:09 +0000

    Adding docstrings + license

--- flowmachine/flowmachine/features/subscriber/unique_subscribers_from_queries.py
+++ flowmachine/flowmachine/features/subscriber/unique_subscribers_from_queries.py
@@ -1,3 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# -*- coding: utf-8 -*-
+"""
+Given a list of queries with a 'subscriber' column, returns a 1-column table of unique subscribers.
+"""
+
 from typing import List
 
 from flowmachine.core.query import Query
@@ -5,8 +14,16 @@ from flowmachine.core.query import Query
 
 class UniqueSubscribersFromQueries(Query):
     """
-    Given a list of queries with a 'subscriber' column, returns a table of unique subscribers.
+    Class representing unique subscribers across a set of queries
+
+    Parameters
+    ----------
+    query_list: List[Query]
+        A list of Query objects, each of which must include a `subscriber` column
+
     """
+
+    # Review question: Does this need to be 'subscriber' or can we make the column to select a parameter?
 
     def __init__(self, query_list: List[Query]):
         self.query_list = query_list
