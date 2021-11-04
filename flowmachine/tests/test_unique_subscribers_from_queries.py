23c50cfa8bbd1c9732b1705737e0f146ba05f6a3
john <john.roberts@flowminder.org>
2021-11-04 14:26:55 +0000

    Initial test

---
+++ flowmachine/tests/test_unique_subscribers_from_queries.py
@@ -0,0 +1,13 @@
+import pytest
+
+from flowmachine.features.subscriber.unique_subscribers_from_queries import (
+    UniqueSubscribersFromQueries,
+)
+from flowmachine.features.utilities.events_tables_union import EventTableSubset
+
+
+def test_unique_subscribers_from_queries(get_dataframe):
+    unique_subscriber_query = UniqueSubscribersFromQueries(
+        [EventTableSubset(start="2016-01-01", stop="2016-01-03")]
+    )
+    assert get_dataframe(unique_subscriber_query)
