fec840e57b99fbb0dd7c107c79c8dc7fb3325f51
john <john.roberts@flowminder.org>
2021-11-04 16:34:03 +0000

    Updating test

--- flowmachine/tests/test_unique_subscribers_from_queries.py
+++ flowmachine/tests/test_unique_subscribers_from_queries.py
@@ -10,5 +10,9 @@ def test_unique_subscribers_from_queries(get_dataframe):
     unique_subscriber_query = UniqueSubscribersFromQueries(
         [EventTableSubset(start="2016-01-01", stop="2016-01-03")]
     )
-    # Need a better test
-    assert len(get_dataframe(unique_subscriber_query)) > 0
+    # Using flowdb_synthetic_data (testdata seems to be empty?)
+    print(get_dataframe(unique_subscriber_query))
+    assert (
+        get_dataframe(unique_subscriber_query).iloc[4]
+        == "7e3229b9b15ca4cf00f7ed0b494b2378"
+    )
