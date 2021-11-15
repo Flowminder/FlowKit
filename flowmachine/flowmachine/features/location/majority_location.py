f8a4d1bfd43079b7eb970c695fc0e534b8f83c5a
john <john.roberts@flowminder.org>
2021-11-15 16:14:59 +0000

    Small rename and test data

--- flowmachine/flowmachine/features/location/majority_location.py
+++ flowmachine/flowmachine/features/location/majority_location.py
@@ -6,23 +6,28 @@ from flowmachine.features.utilities.subscriber_locations import BaseLocation
 
 
 class MajorityLocation(BaseLocation, Query):
-    def __init__(self, subscriber_subset: Query, weight_column):
-        if weight_column not in subscriber_subset.column_names:
+    def __init__(self, subscriber_location_weights: Query, weight_column):
+        if weight_column not in subscriber_location_weights.column_names:
             raise ValueError("weight_column must exist in subscriber_subset")
-        self.subscriber_subset = subscriber_subset
+        self.subscriber_location_weights = subscriber_location_weights
         self.weight_column = weight_column
+        self.spatial_unit = subscriber_location_weights.spatial_unit
         super().__init__()
 
     @property
     def column_names(self) -> List[str]:
         # This should be a reduction, so no new columns
-        return ["subscriber"] + self.subscriber_subset.spatial_unit.location_id_columns
+        return [
+            "subscriber"
+        ] + self.subscriber_location_weights.spatial_unit.location_id_columns
 
     def _make_query(self):
-        loc_id = ",".join(self.subscriber_subset.spatial_unit.location_id_columns)
+        loc_id = ",".join(
+            self.subscriber_location_weights.spatial_unit.location_id_columns
+        )
         sql = f"""
 WITH subscriber_subset AS (
-    {self.subscriber_subset.get_query()}
+    {self.subscriber_location_weights.get_query()}
 ), summed_weights AS (
     SELECT subscriber, sum({self.weight_column}) AS total_weight
     FROM subscriber_subset
--- flowmachine/tests/test_majority_location.py
+++ flowmachine/tests/test_majority_location.py
@@ -19,18 +19,19 @@ def test_majority_location(get_dataframe):
         )
     )
     ml = MajorityLocation(lv, "dl_count")
-    sql = ml.get_query()
     out = get_dataframe(ml)
     assert len(out) == 15
     assert out.subscriber.is_unique
     target = df.from_records(
         [
-            ["6kj8RD7YQ4kwWlQy", "AUQZGMW3"],
-            ["7qKmzkeMbmk5nOa0", "w4H81eLM"],
-            ["7XebRKr35JMJnq8A", "9eosMq9j"],
-            ["8rxEQOL3ePZdAe1z", "xaFQVHqu"],
-            ["aK9GDX8nozb2RB67", "ns6vzdkC"],
+            ["1QBlwRo4Kd5v3Ogz", "524 3 08 44"],
+            ["37J9rKydzJ0mvo0z", "524 4 12 62"],
+            ["3XKdxqvyNxO2vLD1", "524 4 12 62"],
+            ["81B6q0K8k325XWmn", "524 1 03 13"],
+            ["8dXPM6JXj05qwjW0", "524 4 12 62"],
+            ["bKZLwjrMQG7z468y", "524 4 12 62"],
+            ["g9D5KEK9BQzOa8z0", "524 4 11 57"],
         ],
         columns=["subscriber", "pcod"],
     )
-    assert_frame_equal(out.head(5), target)
+    assert_frame_equal(out.head(7), target)
