326f2e5a7a62428f4032e5102a2e7ad12375abb1
john <john.roberts@flowminder.org>
2021-11-22 15:44:15 +0000

    Exposing flows(join_tpye) param

--- flowmachine/flowmachine/core/server/query_schemas/flows.py
+++ flowmachine/flowmachine/core/server/query_schemas/flows.py
@@ -32,11 +32,12 @@ class InputToFlowsSchema(OneOfSchema):
 
 
 class FlowsExposed(BaseExposedQuery):
-    def __init__(self, *, from_location, to_location):
+    def __init__(self, *, from_location, to_location, join_type=None):
         # Note: all input parameters need to be defined as attributes on `self`
         # so that marshmallow can serialise the object correctly.
         self.from_location = from_location
         self.to_location = to_location
+        self.join_type = join_type
 
     @property
     def _flowmachine_query_obj(self):
@@ -49,7 +50,7 @@ class FlowsExposed(BaseExposedQuery):
         """
         loc1 = self.from_location._flowmachine_query_obj
         loc2 = self.to_location._flowmachine_query_obj
-        return RedactedFlows(flows=Flows(loc1, loc2))
+        return RedactedFlows(flows=Flows(loc1, loc2, join_type=self.join_type))
 
 
 class FlowsSchema(BaseSchema):
@@ -57,5 +58,6 @@ class FlowsSchema(BaseSchema):
     query_kind = fields.String(validate=OneOf(["flows"]))
     from_location = fields.Nested(InputToFlowsSchema, required=True)
     to_location = fields.Nested(InputToFlowsSchema, required=True)
+    join_type = fields.String(validate=OneOf(Flows.valid_joins))
 
     __model__ = FlowsExposed
--- flowmachine/flowmachine/features/location/flows.py
+++ flowmachine/flowmachine/features/location/flows.py
@@ -123,6 +123,8 @@ class Flows(FlowLike, Query):
         Join type of the join between loc_1 and loc_2
     """
 
+    valid_joins = ("inner", "full outer", "left", "right", "left outer", "right outer")
+
     def __init__(self, loc1, loc2, join_type="inner"):
         if loc1.spatial_unit != loc2.spatial_unit:
             raise InvalidSpatialUnitError(
