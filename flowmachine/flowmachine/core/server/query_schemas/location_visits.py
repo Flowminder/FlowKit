ca3bf7bbd821cde185077c292854fe02d6fa43c8
john <john.roberts@flowminder.org>
2021-11-26 10:41:10 +0000

    Implementing first bit of LocationVisits -pre merge into MajorityLocation

--- flowmachine/flowmachine/core/server/query_schemas/location_visits.py
+++ flowmachine/flowmachine/core/server/query_schemas/location_visits.py
@@ -1,9 +1,12 @@
 from marshmallow import fields
 from marshmallow.validate import OneOf
+from marshmallow_oneofschema import OneOfSchema
 
 from flowmachine.core.server.query_schemas import BaseExposedQuery
 from flowmachine.core.server.query_schemas.base_schema import BaseSchema
 from flowmachine.core.server.query_schemas.daily_location import DailyLocationSchema
+from flowmachine.core.server.query_schemas.modal_location import ModalLocationSchema
+from flowmachine.features import DayTrajectories
 from flowmachine.features.subscriber.location_visits import LocationVisits
 
 
@@ -14,10 +17,13 @@ class LocationVisitsExposed(BaseExposedQuery):
 
     def _flowmachine_query_obj(self):
         return LocationVisits(
-            day_trajectories=self.day_trajectories,
+            day_trajectories=DayTrajectories(self.day_trajectories),
         )
 
 
 class LocationVisitsSchema(BaseSchema):
     query_kind = fields.String(validate=OneOf(["location_visits"]))
-    day_trajectories = fields.List(fields.Nested(DailyLocationSchema))
+    day_trajectories = fields.List(
+        fields.Nested(DailyLocationSchema),
+        validate=OneOf([DailyLocationSchema, ModalLocationSchema]),
+    )
