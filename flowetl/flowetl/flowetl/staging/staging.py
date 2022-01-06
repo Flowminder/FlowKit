fb7c8bfe92ab377749788d822d69dcc68559273b
john <john.roberts@flowminder.org>
2022-01-06 10:13:58 +0000

    Renaming 'archive' to 'staging'

--- flowetl/flowetl/flowetl/staging/staging.py
+++ flowetl/flowetl/flowetl/staging/staging.py
@@ -6,7 +6,9 @@ import psycopg2
 import os
 
 import logging
+
 logger = logging.getLogger("flowdb")
+
 
 def _parse_date(date):
     if type(date) == dt.date:
@@ -40,6 +42,7 @@ def get_cursor(db_con):
 
 # TODO: Full rebuild query (reduced table, cell table, geography table)
 
+
 class ArchiveStep:
     def __init__(self, query_path, query_args=None):
         if query_args is None:
@@ -47,7 +50,7 @@ class ArchiveStep:
         self.query_path = query_path
         self.query_args = query_args
         self.query_name = Path(query_path).name
-        with open(query_path, 'r') as query_file:
+        with open(query_path, "r") as query_file:
             logger.info(f"Loading {self.query_name} with args {self.query_args}")
             try:
                 self.query = query_file.read().format(**self.query_args)
@@ -60,20 +63,22 @@ class ArchiveStep:
         return cursor.execute(self.query)
 
 
-
 class ArchiveManager:
-
-    def __init__(self, archive_dir, opt_out_list_path = None, tower_clustering_method = None):
+    def __init__(
+        self, archive_dir, opt_out_list_path=None, tower_clustering_method=None
+    ):
         # TODO: path validation, input validation
         self.archive_dir = Path(archive_dir).absolute().__str__()
         self.opt_out_path = Path(opt_out_list_path).absolute().__str__()
         self.tower_clustering_method = tower_clustering_method
         self.db_con = psycopg2.connect(
-            host = os.getenv("FLOWDB_HOST"),
-            port = os.getenv("FLOWDB_PORT"),
-            user = os.getenv("POSTGRES_USER"), # Replace with lesss expansive permissions later
-            password = os.getenv("POSTGRES_PASSWORD"),
-            dbname = "flowdb"
+            host=os.getenv("FLOWDB_HOST"),
+            port=os.getenv("FLOWDB_PORT"),
+            user=os.getenv(
+                "POSTGRES_USER"
+            ),  # Replace with lesss expansive permissions later
+            password=os.getenv("POSTGRES_PASSWORD"),
+            dbname="flowdb",
         )
         self.query_args = {
             "csv_dir": self.archive_dir,
@@ -85,9 +90,13 @@ class ArchiveManager:
         self.query_args["date"] = date
 
         with cd(Path(__file__).parent):
-            staging = ArchiveStep("sql/create_and_fill_staging_table.sql", self.query_args)
+            staging = ArchiveStep(
+                "sql/create_and_fill_staging_table.sql", self.query_args
+            )
             opt_out = ArchiveStep("sql/opt_out.sql", self.query_args)
-            reduce = ArchiveStep("sql/create_and_fill_reduced_table.sql", self.query_args)
+            reduce = ArchiveStep(
+                "sql/create_and_fill_reduced_table.sql", self.query_args
+            )
         with get_cursor(self.db_con) as cur:
             staging.execute(cur)
             if self.opt_out_path:
@@ -96,4 +105,3 @@ class ArchiveManager:
                 # This needs to be implemented. Leaving as cellid for now.
                 raise NotImplementedError
             reduce.execute(cur)
-
--- flowetl/tests/unit/test_csv_ingestion.py
+++ flowetl/tests/unit/test_csv_ingestion.py
@@ -1,19 +1,20 @@
-from flowetl.archive.archive import ArchiveManager
+from flowetl.staging.staging import ArchiveManager
 import pytest
+
 
 @pytest.fixture
 def flowmachine_env(monkeypatch):
-    with open("../../../development_environment" ,'r') as env_file:
+    with open("../../../development_environment", "r") as env_file:
         for line in env_file.readlines():
             if "=" in line:
                 key, sep, value = line.partition("=")
                 if sep == "=":
                     monkeypatch.setenv(key, value.strip())
 
+
 def test_csv_ingestion(flowmachine_env):
     # Need to mount this in the actual docker image somehow
     csv_dir = "/test_data/static_csvs"
     opt_out_path = "/test_data/static_csvs/opt_out_list.csv"
-    archive = ArchiveManager(csv_dir, opt_out_list_path = opt_out_path)
+    archive = ArchiveManager(csv_dir, opt_out_list_path=opt_out_path)
     archive.retrieve_csv_on_date("2021_09_29")
-
