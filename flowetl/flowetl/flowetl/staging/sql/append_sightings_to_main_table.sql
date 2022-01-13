4f3806423da548ab0cbbe49501c1929a95b190b9
john <john.roberts@flowminder.org>
2022-01-13 10:52:55 +0000

    Partitioning now in place

---
+++ flowetl/flowetl/flowetl/staging/sql/append_sightings_to_main_table.sql
@@ -0,0 +1,8 @@
+BEGIN;
+
+ALTER TABLE reduced.sightings_{date} ADD CONSTRAINT {date}
+    CHECK (sighting_date == DATE '{date}')
+
+-- Partitions are inclusive on the lower bound, exclusive on the upper
+ALTER TABLE reduced.sightings ATTACH PARTITION reduced.sightings_{date}
+    FOR VALUES FROM (DATE '{date}') TO (DATE '{date}') + 1
