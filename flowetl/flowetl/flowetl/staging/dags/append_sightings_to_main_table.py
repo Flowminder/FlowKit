472f7d198ee678f6630de8658db036e0abf4f846
john <john.roberts@flowminder.org>
2022-02-02 10:48:44 +0000

    DAG structure in place

--- flowetl/flowetl/flowetl/staging/dags/append_sightings_to_main_table.py
+++ flowetl/flowetl/flowetl/staging/dags/append_sightings_to_main_table.py
@@ -1,9 +1,38 @@
 from airflow import DAG
-from airflow.operators.postgres_operator import PostgresOperator
 
 # TODO Tomorrow: Design the ingestion DAG
-with DAG() as dag:
+from ..operators.create_and_fill_day_sightings_table import (
+    CreateAndFillDaySightingsTable,
+)
+from ..operators.create_sightings_table import CreateSightingsTable
+from ..operators.default_location_mapping import DefaultLocationMapping
 
-    sighting_task = PostgresOperator(sql=Path("../sql/create_sightings_table.sql"))
-    other_task = PythonOperator(SightingTask)
-    sighting_task >> other_task  # Hey, you can define DAGs using operators!
+
+def csv_ingestion_dag(csvs, date):
+    with DAG(
+        "append_sightings_to_main_table",
+        params={"csv_dir": csvs, "date": date},
+    ) as dag:
+        from ..operators.append_sightings_to_main_table import (
+            AppendSightingsToMainTable,
+        )
+        from ..operators.apply_mapping_to_staged_events import (
+            ApplyMappingToStagedEvents,
+        )
+        from ..operators.create_and_fill_staging_table import CreateAndFillStagingTable
+
+        create_and_fill_staging_table = CreateAndFillStagingTable()
+        location_mapping = DefaultLocationMapping()
+        apply_mapping_to_staged_events = ApplyMappingToStagedEvents()
+        create_sightings_table = CreateSightingsTable()
+        create_day_sightings_table = CreateAndFillDaySightingsTable()
+        append_sightings = AppendSightingsToMainTable()
+
+        append_sightings << [create_day_sightings_table, create_sightings_table]
+        (
+            create_day_sightings_table
+            << apply_mapping_to_staged_events
+            << location_mapping
+            << create_and_fill_staging_table
+        )
+        return dag
