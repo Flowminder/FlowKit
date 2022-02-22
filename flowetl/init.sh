c471e9c0c02dc5be1163f6e9ecb82b2ec15ef729
Jonathan Gray <jonathan.gray@flowminder.org>
2022-02-22 15:32:26 +0000

    Fix pools command

--- flowetl/init.sh
+++ flowetl/init.sh
@@ -8,4 +8,4 @@ set -euo pipefail
 
 airflow db init
 airflow users create -r Admin -u "${FLOWETL_AIRFLOW_ADMIN_USERNAME:?Need to set FLOWETL_AIRFLOW_ADMIN_USERNAME non-empty}" -e ${FLOWETL_AIRFLOW_ADMIN_EMAIL:-admin@example.com} -f ${FLOWETL_AIRFLOW_ADMIN_FIRSTNAME:-admin} -l ${FLOWETL_AIRFLOW_ADMIN_LASTNAME:-user} -p "${FLOWETL_AIRFLOW_ADMIN_PASSWORD:?Need to set FLOWETL_AIRFLOW_ADMIN_PASSWORD non-empty}"
-airflow pools -s postgres_etl ${FLOWETL_AIRFLOW_PG_POOL_SLOT_COUNT:-4} "Allows an upper bound on number of concurrent high load postgres dags."
+airflow pools set postgres_etl ${FLOWETL_AIRFLOW_PG_POOL_SLOT_COUNT:-4} "Allows an upper bound on number of concurrent high load postgres dags."
