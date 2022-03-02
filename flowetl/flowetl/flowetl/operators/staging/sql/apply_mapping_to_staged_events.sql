fe68de4836c35ff958a7975b4e3adaf0a030a140
Thingus <john.roberts@flowminder.org>
2022-03-02 12:12:09 +0000

    Removing more unused functions and queries

--- flowetl/flowetl/flowetl/operators/staging/sql/apply_mapping_to_staged_events.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-UPDATE staging_table_{{ds_nodash}}
-    SET cell_id =
-        (SELECT location_id
-        FROM reduced.cell_location_mapping
-        WHERE reduced.cell_location_mapping.cell_id = staging_table_{{ds_nodash}}.cell_id);
-
-COMMIT;
