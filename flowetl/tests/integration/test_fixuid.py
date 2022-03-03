ebec008255aa647a45188885d5c2b54dcd0ad97b
Jonathan Gray <jonathan.gray@flowminder.org>
2022-03-03 15:22:39 +0000

    Need a db to start the container

--- flowetl/tests/integration/test_fixuid.py
+++ flowetl/tests/integration/test_fixuid.py
@@ -8,39 +8,41 @@ Tests that fixuid works as expected
 """
 
 
-def test_uid(docker_client, container_tag):
+def test_uid(
+    flowetl_db_container, docker_client, container_tag, container_network, container_env
+):
     """
     test that we can run the flowetl container with a specific user.
     Check UID is correct.
     """
-
+    flowetl_db_container()
     user = "1002:0"
     out = docker_client.containers.run(
         f"flowminder/flowetl:{container_tag}",
         "bash -c 'id -u'",
         user=user,
-        environment={
-            "AIRFLOW__CORE__SQL_ALCHEMY_CONN": f"postgres://TEST_USER:TEST_PASSWORD@DUMMY_DB:5432/DUMMY_DB"
-        },
+        environment=container_env["flowetl"],
         auto_remove=True,
+        network=container_network.name,
     )
     print(out)
     assert out.decode("utf-8").strip() == "1002"
 
 
-def test_uid_is_airflow(docker_client, container_tag):
+def test_uid_is_airflow(
+    flowetl_db_container, docker_client, container_tag, container_network, container_env
+):
     """
     Test that the user we run the container with is the airflow user.
     """
-
+    flowetl_db_container()
     user = "1002:0"
     out = docker_client.containers.run(
         f"flowminder/flowetl:{container_tag}",
         "bash -c 'id -u | id -nu'",
         user=user,
-        environment={
-            "AIRFLOW__CORE__SQL_ALCHEMY_CONN": f"postgres://TEST_USER:TEST_PASSWORD@DUMMY_DB:5432/DUMMY_DB"
-        },
         auto_remove=True,
+        environment=container_env["flowetl"],
+        network=container_network.name,
     )
     assert out.decode("utf-8").strip() == "airflow"
