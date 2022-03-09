fa7d16d6bf49fb6bc79e27e424467ed0fab4f86b
Thingus <john.roberts@flowminder.org>
2022-03-09 09:23:48 +0000

    Fixing a DAG import error?

--- flowetl/tests/unit/test_wrapping_sql_mixin.py
+++ flowetl/tests/unit/test_wrapping_sql_mixin.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from airflow import DAG
+
 
 def test_wrapped_sql(mock_basic_dag):
     from airflow.providers.postgres.operators.postgres import PostgresOperator
