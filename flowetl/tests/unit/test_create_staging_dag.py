6b50a527726c6b49b7e9bd91a7bfedc86734531d
Thingus <john.roberts@flowminder.org>
2022-04-11 09:40:21 +0100

    Test for new DAG generator

--- flowetl/tests/unit/test_create_staging_dag.py
+++ flowetl/tests/unit/test_create_staging_dag.py
@@ -1,13 +1,26 @@
 import datetime
 
-from flowetl.util import create_staging_dag
+import pytest
+
+from flowetl.util import create_staging_dag, create_sighting_dag
 
 
-def test_create_staging_dag(monkeypatch):
-    """Tests that create_staging_dag runs through. Does NOT test the DAG works!"""
+@pytest.fixture()
+def dummy_dag_env(monkeypatch):
     monkeypatch.setenv("SOURCE_TREE", "foo")
     monkeypatch.setenv("FLOWDB_CSV_DIR", "bar")
+
+
+def test_create_staging_dag(dummy_dag_env):
+    """Tests that create_staging_dag runs through. Does NOT test the DAG works!"""
     dag = create_staging_dag(
+        start_date=datetime.datetime(2021, 9, 29, tzinfo=datetime.timezone.utc)
+    )
+    assert dag
+
+
+def test_create_sighting_dag(dummy_dag_env):
+    dag = create_sighting_dag(
         start_date=datetime.datetime(2021, 9, 29, tzinfo=datetime.timezone.utc)
     )
     assert dag
