19a2f762f22904fc44f5c072120008e5caebdeb4
Jonathan Gray <jonathan.gray@flowminder.org>
2022-06-09 15:17:01 +0100

    Fix import

--- flowmachine/flowmachine/features/location/redacted_in_out_flows.py
+++ flowmachine/flowmachine/features/location/redacted_in_out_flows.py
@@ -1,11 +1,11 @@
 from flowmachine.core import Query
-from flowmachine.features.location.flows import BaseInOutFlow
+from flowmachine.features.location.flows import InOutFlow
 from flowmachine.features.location.redacted_location_metric import (
     RedactedLocationMetric,
 )
 
 
-class RedactedInOutFlow(RedactedLocationMetric, BaseInOutFlow, Query):
+class RedactedInOutFlow(RedactedLocationMetric, InOutFlow, Query):
     """
     An object representing the redacted summation of all movement into or out of
     a set of locations
@@ -16,7 +16,7 @@ class RedactedInOutFlow(RedactedLocationMetric, BaseInOutFlow, Query):
         The underlying Inflow or Outflow to redact; normally created from `flows.inflow` or `flows.outflow`
     """
 
-    def __init__(self, *, in_out_flows: BaseInOutFlow):
+    def __init__(self, *, in_out_flows: InOutFlow):
         self.redaction_target = in_out_flows
         # self.spatial_unit is used in self._geo_augmented_query
         self.spatial_unit = in_out_flows.spatial_unit
