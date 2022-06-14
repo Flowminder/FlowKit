99948116332a0dc9a70836c1f4528feae7723039
Jonathan Gray <jonathan.gray@flowminder.org>
2022-06-14 14:23:19 +0100

    More explicit trans, skip autoflow deps when not required

--- flowmachine/flowmachine/core/connection.py
+++ flowmachine/flowmachine/core/connection.py
@@ -170,8 +170,8 @@ class Connection:
         if schema is not None:
             exists_query = "{} AND table_schema='{}'".format(exists_query, schema)
         exists_query = "SELECT EXISTS({})".format(exists_query)
-        with self.engine.begin():
-            return self.engine.execute(exists_query).fetchall()[0][0]
+        with self.engine.begin() as trans:
+            return trans.execute(exists_query).fetchall()[0][0]
 
     @property
     def available_dates(self) -> Dict[str, List[datetime.date]]:
