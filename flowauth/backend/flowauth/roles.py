9efd936203fc69b9f541a7448eac603744975eb4
Thingus <john.roberts@flowminder.org>
2022-06-23 13:07:10 +0100

    GET role endpoint now running

--- flowauth/backend/flowauth/roles.py
+++ flowauth/backend/flowauth/roles.py
@@ -17,7 +17,7 @@ def role_to_dict(role):
     return {
         "id": role.id,
         "name": role.name,
-        "scopes": sorted([scope.scope for scope in role.scopes]),
+        "scopes": sorted([scope.id for scope in role.scopes]),
         "latest_token_expiry": role.latest_token_expiry.strftime(
             "%Y-%m-%dT%H:%M:%S.%fZ"
         ),
@@ -33,12 +33,14 @@ def role_to_dict(role):
 def list_roles():
     return jsonify([role_to_dict(role) for role in Role.query.all()])
 
+
 @blueprint.route("/<role_id>", methods=["GET"])
 @login_required
 @admin_permission.require(http_exception=401)
 def get_role(role_id):
     role = Role.query.filter(Role.id == role_id).first()  # First or error?
     return jsonify(role_to_dict(role))
+
 
 @blueprint.route("/server/<server_id>", methods=["GET"])
 @login_required
