3960186f439ebf301af20d130e8aefaacbd6568e
James Harrison <james.harrison@flowminder.org>
2022-07-05 18:59:23 +0100

    Add aggregation_unit property to unmoving_at_reference_location_counts

--- flowmachine/flowmachine/core/server/query_schemas/unmoving_at_reference_location_counts.py
+++ flowmachine/flowmachine/core/server/query_schemas/unmoving_at_reference_location_counts.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marshmallow import fields
+from marshmallow import fields, validates_schema, ValidationError
 from marshmallow.validate import OneOf
 
 from flowmachine.features.location.redacted_unmoving_at_reference_location_counts import (
@@ -15,6 +15,7 @@ from flowmachine.features.subscriber.unmoving_at_reference_location import (
     UnmovingAtReferenceLocation,
 )
 from . import BaseExposedQuery
+from .aggregation_unit import AggregationUnitKind
 from .base_schema import BaseSchema
 from .reference_location import ReferenceLocationSchema
 from .unique_locations import UniqueLocationsSchema
@@ -34,6 +35,10 @@ class UnmovingAtReferenceLocationCountsExposed(BaseExposedQuery):
         # so that marshmallow can serialise the object correctly.
         self.locations = locations
         self.reference_locations = reference_locations
+
+    @property
+    def aggregation_unit(self):
+        return self.locations.aggregation_unit
 
     @property
     def _flowmachine_query_obj(self):
@@ -59,5 +64,23 @@ class UnmovingAtReferenceLocationCountsSchema(BaseSchema):
 
     # query_kind parameter is required here for claims validation
     query_kind = fields.String(validate=OneOf([__model__.query_kind]), required=True)
+    aggregation_unit = AggregationUnitKind(dump_only=True)
     locations = fields.Nested(UniqueLocationsSchema, required=True)
     reference_locations = fields.Nested(ReferenceLocationSchema, required=True)
+
+    @validates_schema
+    def validate_aggregation_units(self, data, **kwargs):
+        """
+        Validate that locations and reference_locations have the same aggregation unit
+        """
+        try:
+            if (
+                data["locations"].aggregation_unit
+                != data["reference_locations"].aggregation_unit
+            ):
+                raise ValidationError(
+                    "'locations' and 'reference_locations' parameters must have the same aggregation unit"
+                )
+        except AttributeError:
+            # Nested schema was invalid - appropriate ValidationError will be raised from elsewhere
+            pass
