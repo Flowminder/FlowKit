f348afbf56ca98e0aeb02318eb6d57185385704c
James Harrison <james.harrison@flowminder.org>
2022-07-05 18:58:42 +0100

    Add aggregation_unit property to unique_visitor_counts

--- flowmachine/flowmachine/core/server/query_schemas/unique_visitor_counts.py
+++ flowmachine/flowmachine/core/server/query_schemas/unique_visitor_counts.py
@@ -2,11 +2,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marshmallow import fields
+from marshmallow import fields, validates_schema, ValidationError
 from marshmallow.validate import OneOf
 
 from flowmachine.features.location.unique_visitor_counts import UniqueVisitorCounts
 from .active_at_reference_location_counts import ActiveAtReferenceLocationCountsSchema
+from .aggregation_unit import AggregationUnitKind
 from .base_schema import BaseSchema
 
 from .unique_subscriber_counts import UniqueSubscriberCountsSchema
@@ -34,6 +35,10 @@ class UniqueVisitorCountsExposed(BaseExposedQuery):
         self.unique_subscriber_counts = unique_subscriber_counts
 
     @property
+    def aggregation_unit(self):
+        return self.unique_subscriber_counts.aggregation_unit
+
+    @property
     def _flowmachine_query_obj(self):
         """
         Return the underlying flowmachine object.
@@ -54,9 +59,27 @@ class UniqueVisitorCountsSchema(BaseSchema):
 
     # query_kind parameter is required here for claims validation
     query_kind = fields.String(validate=OneOf([__model__.query_kind]), required=True)
+    aggregation_unit = AggregationUnitKind(dump_only=True)
     active_at_reference_location_counts = fields.Nested(
         ActiveAtReferenceLocationCountsSchema(), required=True
     )
     unique_subscriber_counts = fields.Nested(
         UniqueSubscriberCountsSchema(), required=True
     )
+
+    @validates_schema
+    def validate_aggregation_units(self, data, **kwargs):
+        """
+        Validate that active_at_reference_location_counts and unique_subscriber_counts have the same aggregation unit
+        """
+        try:
+            if (
+                data["active_at_reference_location_counts"].aggregation_unit
+                != data["unique_subscriber_counts"].aggregation_unit
+            ):
+                raise ValidationError(
+                    "'active_at_reference_location_counts' and 'unique_subscriber_counts' parameters must have the same aggregation unit"
+                )
+        except AttributeError:
+            # Nested schema was invalid - appropriate ValidationError will be raised from elsewhere
+            pass
