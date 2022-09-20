d9df22451e770f6d0ecfa74594bb1b01260f18f9
Thingus <john.roberts@flowminder.org>
2022-09-20 16:22:41 +0100

    New style of RoleList needs buttons wiring up

--- flowauth/frontend/src/RoleList.jsx
+++ flowauth/frontend/src/RoleList.jsx
@@ -5,10 +5,10 @@
 import { withStyles } from "@material-ui/core/styles";
 import RoleDetails from "./RoleDetails";
 import Lister from "./Lister";
-import {deleteRole, getServer, getServers, getRole } from "./util/api";
+import {deleteRole, getServer, getServers, getRole, getRolesOnServer } from "./util/api";
 import React, { useEffect, useState } from "react";
-import { Typography } from "@material-ui/core";
-import { Button, Stack } from "rsuite";
+import { Typography, Grid, Paper, Button } from "@material-ui/core";
+import { EditLocation } from "@material-ui/icons";
 
 const styles = (theme) => ({
   root: {
@@ -17,51 +17,34 @@ const styles = (theme) => ({
     paddingBottom: theme.spacing.unit * 2,
   },
 });
-// const RoleList = Lister(
-//   RoleDetails,
-//   "Roles",
-//   getRoles,
-//   deleteRole
-// // );
 
 
 function RoleItem(props) {
   // Properties:
   // role_id
 
-  const {role_id} = props
-
-  const [role, setRole] = useState({})
-
-  useEffect(
-    () => {
-      const fetch_role = (async(role_id) => {
-        const role = await getRole(role_id)
-        setRole(role)
-      })
-
-      fetch_role(role_id).catch(console.error)
-    }, []
-  )
+  const {role} = props
+  const edit_action = () => console.debug(role.id)
+  const delete_action = () => deleteRole(role.id)
 
   return(
-    <Grid>
-      <Item>{role.name}</Item>
-      <Item><Button>Edit</Button></Item>
-      <Item><Button>Delete</Button></Item>
+    <Grid container spacing = {3}>
+      <Grid item xs={8}>{role.name}</Grid>
+      <Grid item xs={1}><Button onClick={edit_action}>Edit</Button></Grid>
+      <Grid item xs={1}r><Button onClick={delete_action}>Delete</Button></Grid>
     </Grid>
   )
 }
 
 function ServerRoleList(props) {
-  const {role_ids} = props
+  const {roles} = props
 
   return(
-    <Stack>
-      {role_ids.map(
-        (role_id) => <Item><RoleItem role_id = {role_id}/></Item>)
+    <Grid direction='column'>
+      {roles.map(
+        (role) => <Paper><RoleItem role = {role}/></Paper>)
       }
-    </Stack>
+    </Grid>
   )
 }
 
@@ -69,67 +52,79 @@ function ServerHeader(props) {
   const {server} = props
 
   return(
-    <Grid>
-      <Item>{server.name}</Item>
-      <Item><Button>Edit</Button></Item>
+    <Grid direction='column'>
+      <Paper>{server.name}</Paper>
+      <Paper><Button>Add New</Button></Paper>
     </Grid>
   )
 }
 
 function ServerRoleView(props) {
-  const {server_id} = props
+  const {server_id, edit_action, delete_action} = props
 
   const [server, setServer] = useState({})
+  const [roles, setRoleList] = useState([])
 
   useEffect(
     () => {
       const fetch_server=(async(server_id) => {
         const server = await getServer(server_id)
+        const role_list = await getRolesOnServer(server_id)
         console.debug("Got server", server)
+        console.debug("Got roles", roles)
         setServer(server)
+        setRoleList(role_list)
       })
 
-      fetch_server(server_id).catch(console.error)
+      fetch_server(server_id).catch((err) => console.error(err))
     }, []
   )
-  
+
   return(
-    <Stack>
-      <Item><ServerHeader server = {server} /></Item>
-      <Item><ServerRoleList role_ids = {server.roles}/></Item>
-    </Stack>
+    <Grid direction='column'>
+      <Paper><ServerHeader server = {server} /></Paper>
+      <Paper><ServerRoleList roles = {roles}/></Paper>
+    </Grid>
   )
 }
 
 
 function RoleList(props) {
 
+  const {editAction, deleteAction} = props
+
   const [server_list, setServerList] = useState([])
 
   // On load, get list of servers.
   useEffect(
     () => {
-      const fetch_servers = async () => {
+      const fetch_servers = (async () => {
         const servers = await getServers()
         setServerList(servers)
         console.debug(server_list)
-      }
+      })
 
-      fetch_servers().catch(console.error)
+      fetch_servers().catch((err) => console.error(err))
     }
   , []);
 
 
   return (
     <React.Fragment>
-      <Stack>
+      <Grid direction='column'>
       {server_list.map((server) => (
-        <Item><ServerRoleView server_id = {server.id}/></Item>
+        <Paper><ServerRoleView
+          server_id = {server.id}
+          edit_action = {editAction}
+          delete_action = {deleteAction}
+        /></Paper>
       ))}
-      </Stack>
+      </Grid>
     </React.Fragment>
   )
 }
+
+
 
 
 export default withStyles(styles)(RoleList);
