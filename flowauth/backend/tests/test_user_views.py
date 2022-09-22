5a8fcca07fb3493519aed59b353ff8b08efcd126
Thingus <john.roberts@flowminder.org>
2022-09-22 17:52:28 +0100

    Removing unused /roles/server/<server_id> endpoint

--- flowauth/backend/tests/test_user_views.py
+++ flowauth/backend/tests/test_user_views.py
@@ -5,25 +5,11 @@
 import pytest
 
 
-@pytest.mark.skip(reason="Users do not have direct access to servers anymore")
-@pytest.mark.usefixtures("test_data")
-def test_server_access(client, auth, test_user):
-    uid, uname, upass = test_user
+# @pytest.mark.skip(reason="Users do not have direct access to servers anymore")
+def test_server_access(client, auth, test_user_with_roles):
+    uid, uname, upass = test_user_with_roles
     # Log in first
     response, csrf_cookie = auth.login(uname, upass)
     response = client.get("/tokens/servers", headers={"X-CSRF-Token": csrf_cookie})
     assert 200 == response.status_code  # Should get an OK
     assert [{"id": 1, "server_name": "DUMMY_SERVER_A"}] == response.get_json()
-
-
-# Replaece with test_no_roles
-# @pytest.mark.usefixtures("test_data")
-# def test_no_tokens(client, auth, test_user):
-#     uid, uname, upass = test_user
-#     Log in first
-# response, csrf_cookie = auth.login(uname, upass)
-# response = client.get("/tokens/tokens", headers={"X-CSRF-Token": csrf_cookie})
-# assert 200 == response.status_code  # Should get an OK
-# result = response.get_json()
-#
-# assert [] == result  # Can't do anything
