38e222f5ad6b98480e3a1120c950b73aac0d7a2d
Thingus <john.roberts@flowminder.org>
2022-10-21 09:56:56 +0100

    Rebase clobbered new cypress filepaths, redoing...

---
+++ flowauth/frontend/cypress.config.js
@@ -0,0 +1,33 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "67obxt",
+
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+
+  defaultCommandTimeout: 5000,
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+  },
+
+  component: {
+    setupNodeEvents(on, config) {},
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
