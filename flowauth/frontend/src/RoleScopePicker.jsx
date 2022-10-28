7ddae72aa686b2d0bc73e3e1a11c8cac00c57133
Thingus <john.roberts@flowminder.org>
2022-10-28 14:31:56 +0100

    RoleScopePicker in progress

--- flowauth/frontend/src/RoleScopePicker.jsx
+++ flowauth/frontend/src/RoleScopePicker.jsx
@@ -7,80 +7,114 @@ import React from "react";
 import {useEffect, useState} from "react"
 import { Grid } from "rsuite";
 import { getServerScopes, getRoleScopes } from "./util/api";
-import { List, ListItem, Checkbox, ListItemIcon, ListItemText} from "@material-ui/core"
-import cs from "date-fns/esm/locale/cs/index.js";
+import { List, ListItem, Checkbox, ListItemIcon, ListItemText, Collapse} from "@material-ui/core"
+import { ExpandLess, ExpandMore} from "@material-ui/icons";
 //import {scopesGraph, jsonify, highest_common_roots} from "./util/util"
 
 function ScopeItem(props) {
-  const {scope_name, is_checked} = props
-  return <ListItem key = {scope_name} role={undefined} value={is_checked} onClick={undefined}>
+  var {scope_name, init_check, onChangeCallback} = props
+
+  const [isChecked, setIsChecked] = useState(init_check)
+
+  const flip_check = () => {
+    setIsChecked(!isChecked)
+  }
+
+  useEffect(() => {
+    onChangeCallback(scope_name)
+  }, [isChecked])
+
+  return <ListItem key={scope_name} value={isChecked} onClick={flip_check}>
     <ListItemIcon>
-      <Checkbox checked ={is_checked} />
+      <Checkbox checked ={isChecked} />
     </ListItemIcon>
     <ListItemText primary={scope_name} />
   </ListItem>
 }
 
-function NestedScopeList(props) {
-  const {nested_scopes} = props
 
-  return <ListItem key = {"foo"} />
+function NestedScopeList(props) {
+  const {outer_scope, inner_scopes, onChangeCallback} = props
+  const [open, setOpen] = useState(false)
+  const handleClick = () =>{
+    setOpen(!open)
+  }
+
+  const newCallback = (inner_scope) => {
+    onChangeCallback(outer_scope + ":" + inner_scope)
+  }
+
+  return <ListItem button onClick={handleClick}>
+    <ListItemText primary = {outer_scope} />
+    {open ? <ExpandLess /> : <ExpandMore />}
+    <Collapse in={open}>
+      <ScopeList scopes = {inner_scopes} onChangeCallback = {newCallback}/>
+    </Collapse>
+  </ListItem>
 }
 
+
 function ScopeList (props) {
-  const {scopes} = props
+  const {scopes, onChangeCallback} = props
   const [flatScopes, setFlatScopes] = useState([])
   const [nestedScopes, setNestedScopes] = useState([])
   // const flat_scopes 
   // const nested_scopes
   useEffect(() => {
-    console.debug("Scopes", scopes)
     setFlatScopes(scopes.filter(s => !s.name.includes(":")))
+
+    // This mess takes every scope that is 
+    // {name:scope1:scope2, enabled:true}, {name:scope1:scope3, enabled:false}
+    // and turns it into 
+    // {outer_scope : scope1, inner_scopes[{name:scope2, enabled:true}, {name:scope3, enabled:false}]}
     const complex_scopes = scopes.filter(s => s.name.includes(":"))
     // If Array.prototype.group() existed it would be ideal here...
     const tl_scopes = [...new Set(complex_scopes.map(s => s.name.split(":")[0]))]
-    console.debug(tl_scopes)
     const nested_scopes = tl_scopes.map(
-      ts => new Object({[ts]:
-        complex_scopes
+      ts => new Object({"outer_scope":ts,
+        "inner_scopes":complex_scopes
           .filter(cs => cs.name.startsWith(ts))
           .map(cs => new Object({
-            "name": cs.name.replace(ts, cs.name),
+            "name": cs.name.replace(ts, "").replace(":", ""),
             "enabled": cs.enabled
           }))
-      })
-    )
-    console.debug(nested_scopes)
+      }))
+    setNestedScopes(nested_scopes)
   }, [scopes])
 
-  return <List>
+  return <List disablePadding>
     {flatScopes.map(
-      scope => <ScopeItem scope_name = {scope.name} key={scope.name} is_checked={scope.enabled}/>
+      scope => <ScopeItem 
+        scope_name = {scope.name}
+        key={scope.name}
+        init_checked={scope.enabled}
+        onChangeCallback={onChangeCallback}
+      />
     )}
-    {/* map<NestedScopeList scopes={nested_scopes} /> */}
+    {nestedScopes.map( scope =>
+      <NestedScopeList outer_scope = {scope.outer_scope} inner_scopes={scope.inner_scopes} key={scope.name} onChangeCallback={onChangeCallback}/>
+    )}
   </List>
 }
+
 
 // Component for picking scopes for a role
 function RoleScopePicker (props) {
   const {role_id, server_id, updateScopes} = props
-  const [roleScopes, setRoleScopes] = useState([])
-  const [serverScopes, setServerScopes] = useState([])
   const [checkedScopes, setCheckedScopes] = useState([])
-  const [rightsChoices, setRightsChoices] = useState({})
-  const [selectedRights, setSelectedRights] = useState([])
   const [hasError, setHasError] = useState(false)
   const [error, setError] = useState({})
 
+  //Initial setup
   useEffect(
     () => {
       const fetch_scopes = async () => {
         const role_scopes = await getRoleScopes(role_id)
         const server_scopes = await getServerScopes(server_id)
         const checked_scopes = server_scopes.map(
-          srv_scp => new Object({
-            "name":srv_scp.name,
-            "enabled":role_scopes.map(y => y.name).includes(srv_scp.name)})
+          srv_scope => new Object({
+            "name":srv_scope.name,
+            "enabled":role_scopes.map(y => y.name).includes(srv_scope.name)})
         )
         setCheckedScopes(checked_scopes)
       }
@@ -89,9 +123,16 @@ function RoleScopePicker (props) {
       // This needs to be cancellable
     }, []
   )
-  
-  return <ScopeList scopes = {checkedScopes}></ScopeList>
 
+  const onChangeCallback = (changed_scopes) => {
+    const new_scopes = checkedScopes.map(s => changed_scopes.includes(s.name) ? {"name":s.name, "enabled":!s.enabled} : s)
+    console.debug(new_scopes)
+    setCheckedScopes(new_scopes)
+  }
+
+  useEffect(() => updateScopes(checkedScopes), [checkedScopes])
+
+  return <ScopeList scopes = {checkedScopes} onChangeCallback = {onChangeCallback} />
 }
 
 
