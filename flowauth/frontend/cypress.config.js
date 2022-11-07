f87d58103eea0efa9500e461ead88c8a322f5524
Thingus <john.roberts@flowminder.org>
2022-11-07 13:29:30 +0000

    Prettier for cypress.config.js

--- flowauth/frontend/cypress.config.js
+++ flowauth/frontend/cypress.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '67obxt',
+  projectId: "67obxt",
   retries: {
     runMode: 2,
     openMode: 0,
@@ -11,9 +11,9 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
-})
+});
