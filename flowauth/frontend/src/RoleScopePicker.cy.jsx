ac53f8a0b2e4f07cb7d211824ded0587c1291251
Thingus <john.roberts@flowminder.org>
2022-11-11 14:32:08 +0000

    Rebuild rolescopepicker (#5580)
    * Complete rebuild of RoleScopePicker
    
    * Some progress on our own RoleScopePicker
    
    * RoleScopePicker in progress
    
    * RoleScopePicker now expanding right
    
    * Nested simple scopes now talking back to the root
    
    * Having issues communicating down the tree
    
    * New data flow for RoleScopePicker
    
    * RoleScopePicker buggy...
    
    * RoleScopePicker not handling indeterminants well
    
    * Component seems to be working!
    
    Thanks to Stef for asking the right question
    
    * Cypress test for RoleScopePicker
    
    * Licence headers and changelog
    
    * Removing debug statements + fixing some alignment issues
    
    * Cleaning up branch
    
    * RPS optimisation in progress
    
    * Plumbing RSP into RoleDetails + performance
    
    * Updating Cypress for node
    
    * Updating Cypress in package-lock
    
    * RSP now works for new roles

--- flowauth/frontend/src/RoleScopePicker.cy.jsx
+++ flowauth/frontend/src/RoleScopePicker.cy.jsx
@@ -1,102 +1,140 @@
-import RoleScopePicker from './RoleScopePicker'
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
+/* eslint-disable react/react-in-jsx-scope */
+import RoleScopePicker from "./RoleScopePicker"
 
 //Need a dummy set of server scopes
 //and a dummy set of existing role scopes
 //
 
 
-describe('<RoleScopePicker>', () => {
-	it('mounts', () => {
-		cy.intercept({
-				method: 'GET',
-				url: '/admin/servers/1/scopes', 
-			},
-			[
-				{
-					"enabled": true,
-					"id": 1,
-					"name": "admin0:dummy_query:dummy_nested_query"
-				},
-				{
-					"enabled": true,
-					"id": 3,
-					"name": "admin0:dummy_query:inner_dummy_query"
-				},
-				{
-					"enabled": true,
-					"id": 5,
-					"name": "get_results"
-				}
-			]
-		).as('getServerScopes');
+describe("<RoleScopePicker>", () => {
+  beforeEach(() => {
+    cy.intercept({
+      method: "GET",
+      url: "/admin/servers/1/scopes", 
+    },
+    [
+      {
+        "enabled": true,
+        "id": 1,
+        "name": "admin0:dummy_query:dummy_nested_query:silly_query"
+      },
+      {
+        "enabled": true,
+        "id": 3,
+        "name": "admin0:dummy_query:inner_dummy_query"
+      },
+      {
+        "enabled": true,
+        "id": 6,
+        "name": "admin0:other_query:inner_dummy_query"
+      },
+      {
+        "enabled": true,
+        "id": 4,
+        "name": "admin1:dummy_query:inner_dummy_query"
+      },
+      {
+        "enabled": true,
+        "id": 7,
+        "name": "get_results"
+      }
+    ]
+    ).as("getServerScopes");
 
-		cy.intercept({
-			method: 'GET',
-			url:'/roles/1/scopes'
-		},
-		[{
-			"id": 3,
-			"name": "admin0:dummy_query:inner_dummy_query",
-		},
-		{
-			"id":1,
-			"name":"admin0:dummy_query:dummy_nested_query"
-		},
-		{
-			"id":7,
-			"name":"get_results"
-		}
-	]
-		).as('getRoleScopes');
-				
-		cy.mount(<RoleScopePicker
-				updateScopes={()=>{}}
-				server_id = {1}
-				role_id = {1}
-			/>);
-		cy.wait("@getServerScopes")
-		cy.wait("@getRoleScopes")
-		cy.get('.rs-picker-toggle-value > span').should("include.text", "admin0 (All), get_results")
-	}),
+    cy.intercept({
+      method: "GET",
+      url:"/roles/1/scopes"
+    },
+    [{
+      "id": 3,
+      "name": "admin0:dummy_query:inner_dummy_query",
+    },
+    {
+      "id":1,
+      "name":"admin0:dummy_query:dummy_nested_query"
+    },
+    {
+      "id":7,
+      "name":"get_results"
+    }
+    ]
+    ).as("getRoleScopes");
+  })
+	
+  it("mounts", () => {
+    cy.mount(<RoleScopePicker
+      updateScopes={()=>{}}
+      server_id = {1}
+      role_id = {1}
+    />);
+    cy.wait("@getServerScopes")
+    cy.wait("@getRoleScopes")
+  })
 
-	it('respects deeply nested loading differences', () => {
-		cy.intercept({
-				method: 'GET',
-				url: '/admin/servers/1/scopes', 
-			},
-			[
-				{
-					"enabled": true,
-					"id": 1,
-					"name": "admin0:dummy_query:other_query"
-				},
-				{
-					"enabled": true,
-					"id": 3,
-					"name": "admin0:dummy_query:dummy_query"
-				}
-			]
-		).as('getServerScopes');
+  it("expands", () => {
+    cy.mount(<RoleScopePicker
+      updateScopes={()=>{}}
+      server_id = {1}
+      role_id = {1}
+    />);
+    cy.get("[data-cy=nested-admin0]").within(() => {
+      cy.get("[data-cy=chevron]")
+        .click()
+    }).contains("dummy_query")
+  })
 
-		cy.intercept({
-			method: 'GET',
-			url:'/roles/1/scopes'
-		},
-		[{
-			"id": 3,
-			"name": "admin0:dummy_query:dummy_query",
-		},
-	]
-		).as('getRoleScopes');
-				
-		cy.mount(<RoleScopePicker
-				updateScopes={()=>{}}
-				server_id = {1}
-				role_id = {1}
-			/>);
-		cy.wait("@getServerScopes")
-		cy.wait("@getRoleScopes")
-		cy.get('.rs-picker-toggle-value > span').should("include.text", "admin0:dummy_query:dummy_query")
-	})
+  it("cascades correctly", () => {
+    cy.mount(<RoleScopePicker
+      updateScopes={()=>{}}
+      server_id = {1}
+      role_id = {1}
+    />);
+    // Check admin0 starts indeterminant
+    cy.get("[data-cy=nested-admin0")
+      .find("[data-cy=checkbox]").first()
+      .invoke("prop", "indeterminate", true)
+
+    // Expand child scopes and check click on checkbox works
+    cy.get("[data-cy=nested-admin0]").within(() => {
+      cy.get("[data-cy=chevron]")
+        .click()
+      cy.get("[data-cy=nested-dummy_query]")
+        .find("[data-cy=checkbox]").first()
+        .invoke("prop", "checked", true)
+        .click()
+        .invoke("prop", "checked", false)
+    })
+
+    // Check both child scopes of admin0 change on click
+    cy.get("[data-cy=nested-other_query]")
+      .find("[data-cy=checkbox]").first()
+      .click()
+
+    //Check both children being true cause admin0 to become true
+    cy.get("[data-cy=nested-admin0")
+      .find("[data-cy=checkbox]").first()
+      .invoke("prop", "checked", true)
+
+    //Click on admin0's checkbox
+    cy.get("[data-cy=nested-admin0")
+      .find("[data-cy=checkbox]").first()
+      .click()
+
+    // Check both children are now false
+    cy.get("[data-cy=nested-admin0]")
+      .find("[data-cy=nested-dummy_query]")
+      .find("[data-cy=checkbox]").first()
+      .invoke("prop", "checked", false)
+    cy.get("[data-cy=nested-admin0]")
+      .find("[data-cy=nested-other_query]")
+      .find("[data-cy=checkbox]").first()
+      .invoke("prop", "checked", false)
+  })
+
 })
-
--- flowauth/frontend/src/RoleScopePicker.jsx
+++ flowauth/frontend/src/RoleScopePicker.jsx
@@ -2,109 +2,227 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* eslint-disable react/prop-types */
+
 import React from "react";
 import {useEffect, useState} from "react"
 import { getServerScopes, getRoleScopes } from "./util/api";
-import RightsCascade from "./RightsCascade";
-import {scopesGraph, jsonify, highest_common_roots} from "./util/util"
+import { List, ListItem, Checkbox, ListItemIcon, ListItemText, Collapse, Button} from "@material-ui/core"
+import { ExpandLess, ExpandMore} from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles((theme) => ({
+  ".MuiListItem-root": {
+    "flex-direction": "column",
+    "align-items": "self-start"
+  },
+  ".CollapsibleScope":{
+    "display":"flex"
+  },
+  ".MuiSvgIcon-root": {
+    "margin":"2px 0"
+  },
+  ".MuiTextItem-root": {
+    "align-self":"center"
+  }
+}));
+
+
+function ScopeItem(props) {
+  const classes = useStyles()
+  var {scope, init_check, flipScopeCallback} = props
+
+  const [isChecked, setIsChecked] = useState(init_check)
+
+  // When an inner scope item is checked, use flipScopeCallback to
+  // flip the scope with scope.key in the root of the component
+  const onClick = () => {
+    flipScopeCallback([scope.key], !scope.enabled)
+  }
+
+  useEffect(() => {
+    setIsChecked(scope.enabled)
+  }, [scope])
+
+  return <ListItem 
+    key={scope.key}
+    value={scope.enabled} 
+    onClick={onClick}
+    data-cy={`scope-item-${scope.key}`}
+  >
+    <ListItemIcon>
+      <Checkbox checked ={isChecked} className={classes[".MuiSvgIcon-root"]}/>
+    </ListItemIcon>
+    <ListItemText primary={scope.name} />
+  </ListItem>
+}
+
+
+function NestedScopeList(props) {
+  const classes = useStyles()
+  const {outer_scope, inner_scopes, flipScopeCallback} = props
+  const [open, setOpen] = useState(false) //remember to put this back to false
+  const [isChecked, setIsChecked] = useState(false)
+  const [isIndeterminant, setIsIndeterminant] = useState(false)
+
+  useEffect(() => {
+    if (inner_scopes.every(s => s.enabled === true)){
+      setIsChecked(true)
+      setIsIndeterminant(false)
+    }
+    else if (inner_scopes.every(s => s.enabled === false)){
+      setIsChecked(false)
+      setIsIndeterminant(false)
+    }
+    else {
+      setIsChecked(true)
+      setIsIndeterminant(true)
+    }
+  }, [inner_scopes])
+  
+  const handleChevronClick = () =>{
+    setOpen(!open)
+  }
+
+  const handleCheckboxClick = () => {
+    var is_checked
+    if(isIndeterminant){
+      setIsIndeterminant(false)
+      is_checked = true
+    } else {
+      is_checked = !isChecked
+    }
+    flipScopeCallback(inner_scopes.map(s => s.key), is_checked) 
+  }
+
+  return <ListItem 
+    className={classes[".MuiListItem-root"]}
+    data-cy={`nested-${outer_scope}`}
+  >
+    <div className={classes[".CollapsibleScope"]}>
+      <Checkbox 
+        checked={isChecked}
+        indeterminate={isIndeterminant}
+        onClick={handleCheckboxClick} 
+        data-cy={"checkbox"}
+      />
+      <ListItemText primary = {outer_scope} className={classes[".MuiTextItem-root"]} />
+      <Button onClick={handleChevronClick} data-cy={"chevron"}>
+        {open ? <ExpandLess className={classes[".MuiSvgIcon-root"]} /> : <ExpandMore className={classes[".MuiSvgIcon-root"]} />}
+      </Button>
+    </div>
+    <Collapse in={open} unmountOnExit>
+      <ScopeList scopes = {inner_scopes} flipScopeCallback={flipScopeCallback}/>
+    </Collapse> 
+  </ListItem>
+}
+
+
+function ScopeList (props) {
+  const classes = useStyles();
+  const {scopes, flipScopeCallback} = props
+  const [flatScopes, setFlatScopes] = useState([])
+  const [nestedScopes, setNestedScopes] = useState([])
+
+  useEffect(() => {
+    const flat_scopes = scopes.filter(s => !s.name.includes(":"))
+    setFlatScopes(flat_scopes)
+    
+    // This mess takes every scope that is 
+    // {name: scope1:scope2, key: scope1:scope2 enabled:true}, {name: scope1:scope3, key: scope1:scope3, enabled:false}
+    // and turns it into 
+    // {outer_scope : scope1, inner_scopes[{name:scope2, key: scope1:scope2 enabled:true}, {name:scope3, key: scope1:scope3, enabled:false}]}
+    const complex_scopes = scopes.filter(s => s.name.includes(":"))
+    // If Array.prototype.group() existed it would be ideal here...
+    const tl_scopes = [...new Set(complex_scopes.map(s => s.name.split(":")[0]))]
+    const nested_scopes = tl_scopes.map(
+      ts => new Object({"outer_scope":ts,
+        "inner_scopes":complex_scopes
+          .filter(cs => cs.name.startsWith(ts))
+          .map(cs => new Object({
+            "name": cs.name.replace(ts, "").replace(":", ""),
+            "key": cs.key,
+            "enabled": cs.enabled,
+            "id": cs.id
+          }))
+      }))
+    setNestedScopes(nested_scopes)
+  }, [scopes])
+
+  return <List 
+    disablePadding
+    className = {classes[".MuiListItem-root"]}
+    data-cy = {`scope-list-${flatScopes[0] ? flatScopes[0].key : ""}`}
+  >
+    {flatScopes.map(scope => 
+      <ScopeItem 
+        scope = {scope}
+        key = {scope.key}
+        flipScopeCallback={flipScopeCallback}
+        init_check = {scope.enabled}
+      />
+    )}
+    {nestedScopes.map( scope =>
+      <NestedScopeList 
+        outer_scope = {scope.outer_scope}
+        inner_scopes = {scope.inner_scopes}
+        key = {scope.outer_scope}
+        flipScopeCallback = {flipScopeCallback}
+      />
+    )}
+  </List>
+}
+
 
 // Component for picking scopes for a role
 function RoleScopePicker (props) {
+
   const {role_id, server_id, updateScopes} = props
-  const [roleScopes, setRoleScopes] = useState([])
-  const [serverScopes, setServerScopes] = useState([])
-  const [rightsChoices, setRightsChoices] = useState({})
-  const [selectedRights, setSelectedRights] = useState([])
+  const [checkedScopes, setCheckedScopes] = useState([])
   const [hasError, setHasError] = useState(false)
   const [error, setError] = useState({})
 
-  useEffect(() => {
-    const fetch_role_scopes = async (server_scopes) => {
-      console.log("Fetching role scopes...")
-      const role_scopes = await getRoleScopes(role_id);
-      console.log("Role scopes fetched: ", role_scopes)
-      console.log("Server scopes are:", server_scopes)
-      setRoleScopes(role_scopes);
-      
-      // selectedRights contains the list of names of selected scopes for this role.
-      // The trick is that, in the case of nested scopes, selectedRights only contains
-      // the shortest needed name.
-      // For example, there are two scopes available, admin1:foo and admin1:bar.
-      // If roleScopes contains both admin1:foo and admin1:bar, selectedRights should
-      // only contain admin1 - presence of only that implies that all sub-scopes are selected as well.
-      // On the other hand, if roleScopes only contains admin1:foo, selectedRights should also
-      // only contain admin1:foo
-
-      const sel_rights = highest_common_roots(
-        scopesGraph(role_scopes.map(x => x.name)),
-        scopesGraph(server_scopes.map(x => x.name)))
-      console.debug("Selected rights:", sel_rights.join(":"))
-      setSelectedRights(sel_rights)
-    }
-
-    const fetch_server_scopes = async() => {
-      console.log("Fetching server scopes....")
-      const server_scopes = await getServerScopes(server_id);
-      console.log("Server scopes fetched: ", server_scopes)
-      setServerScopes(server_scopes)
-      return server_scopes
-    }
-
-    const set_initial_state = async () => {
-      if (server_id >= 0){
-        const server_scopes = await fetch_server_scopes()
-        if (role_id >= 0){
-          await fetch_role_scopes(server_scopes)
-        }
+  //Initial setup
+  useEffect(
+    () => {
+      const fetch_scopes = async () => {
+        const role_scopes = await getRoleScopes(role_id).catch(err => {
+          if (err.code === 404){
+            return []
+          }
+        })
+        const server_scopes = await getServerScopes(server_id)
+        const checked_scopes = server_scopes.map(
+          srv_scope => new Object({
+            "name":srv_scope.name,
+            "key":srv_scope.name,
+            "enabled":role_scopes.map(y => y.name).includes(srv_scope.name),
+            "id":srv_scope.id
+          }))
+        setCheckedScopes(checked_scopes)
       }
-    }
+      
+      fetch_scopes().catch((err) => console.error(err))
 
-    set_initial_state()//.catch(err => console.log(err))
-    
-  }, [role_id, server_id])
+      return() => {}
+      // This needs to be cancellable
+    }, [role_id, server_id]
+  )
 
-  // On scopes change, jsonify the scopes into the appropriate format for
-  // MultiCascader
-  useEffect(() =>{
-    if (serverScopes.length > 0){
-      const unneeded = []
-      const baz = scopesGraph(serverScopes.map(x => x.name))
-      const foo = jsonify(baz, unneeded);
-      setRightsChoices(foo)
-    }
+  // Callback that flips all checkboxes
+  const flipScopes = (changed_scope_names, enabled) => {
+    console.debug(`Flipping ${changed_scope_names} to ${enabled} from RoleScopeCallback`)
+    const new_scopes = checkedScopes.map(s => changed_scope_names.includes(s.name) ? {"name":s.name, "key":s.key, "enabled":enabled, "id":s.id} : s)
+    setCheckedScopes(new_scopes)
+  }
 
-  }, [serverScopes])
+  useEffect(
+    () => {
+      updateScopes(checkedScopes)
+    }, [checkedScopes]
+  )
 
-  useEffect(() => {
-    updateScopes(roleScopes)
-  }, [roleScopes])
-  
-  const handleChange = (checked_roles) => {
-    //'checked_roles' in this case is a list of _labels_ from RightsCascade's onChange
-    // So we need to filter RoleScopes to all the scopes with that 'name' string
-    // This is further complicated by RightsCascade...apparently only returning the root
-    // label of a tree? This could be a case of it returning the lowest common root of a
-    // selected node; so if there is _only_ admin3:dummy_query:dummy_query, and you select
-    // admin3 -> dummy_query -> dummy_query, you have selected all _admin3_, therefore 
-    // new_labels contains only admin3.
-    
-    setSelectedRights(checked_roles)
-    console.debug("checked_roles:",checked_roles)
-    setRoleScopes(serverScopes.filter(
-      s_scope => checked_roles.some(
-        c_role => s_scope.name.startsWith(c_role)
-      )
-    ))
-  };
-
-  return (
-    <RightsCascade
-      options={rightsChoices}
-      value={selectedRights}
-      onChange={handleChange}
-      // label={"Scopes"}
-    />
-  );
+  return <ScopeList scopes = {checkedScopes} flipScopeCallback = {flipScopes} />
 }
 
 export default RoleScopePicker
