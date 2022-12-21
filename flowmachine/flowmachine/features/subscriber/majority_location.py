3921f9cccd6cfda4a7581c3db7b4c14337009a82
James Harrison <james.harrison@flowminder.org>
2022-12-21 18:35:20 +0000

    Add 'minimum_total_weight' argument to MajorityLocation

--- flowmachine/flowmachine/features/subscriber/majority_location.py
+++ flowmachine/flowmachine/features/subscriber/majority_location.py
@@ -24,14 +24,23 @@ class MajorityLocation(BaseLocation, Query):
         The query object containing subscribers, locations, and weights.
     weight_column: str
         The column, when summed, that will produce the count used to threshold the majority
-    include_unlocatable: bool default False
+    minimum_total_weight: int, default 0
+        If the summed weight for a subscriber is less than `minimum_total_weight`,
+        that subscriber will only be assigned a location with weight greater than `minimum_total_weight/2`.
+        This is useful if, for example, `subscriber_location_weights` is a count of the number of days
+        a location was a subscriber's daily location over one week - if a subscriber was not active every day,
+        their total weight would be less than 7, which would lower the threshold for a majority.
+        Setting `minimum_total_weight=7` in this case ensures that a subscriber must have the same
+        daily location on a majority of _all_ days during the week, not just a majority of their _active_ days.
+    include_unlocatable: bool, default False
         If `True`, returns every unique subscriber in the `subscriber_location_weights` query, with
         the location column as `NULL` if no majority is reached.
         If `False`, returns only subscribers that have achieved a majority location
 
     Notes
     -----
-    Any rows where weight < 0 will be dropped
+    Any rows where weight < 0 in the `subscriber_location_weights` query will be dropped.
+    This is necessary to ensure the query can return at most one location per subscriber.
     """
 
     def __init__(
@@ -39,6 +48,7 @@ class MajorityLocation(BaseLocation, Query):
         *,
         subscriber_location_weights: Query,
         weight_column: str,
+        minimum_total_weight: int = 0,
         include_unlocatable: bool = False,
     ):
         if "subscriber" not in subscriber_location_weights.column_names:
@@ -49,9 +59,12 @@ class MajorityLocation(BaseLocation, Query):
             raise InvalidSpatialUnitError(
                 "subscriber_location_weights query needs a spatial_unit attribute"
             )
+        if minimum_total_weight < 0:
+            raise ValueError("minimum_total_weight cannot be negative")
 
         self.subscriber_location_weights = subscriber_location_weights
         self.weight_column = weight_column
+        self.minimum_total_weight = minimum_total_weight
         self.include_unlocatable = include_unlocatable
         self.spatial_unit = subscriber_location_weights.spatial_unit
         super().__init__()
@@ -66,7 +79,7 @@ class MajorityLocation(BaseLocation, Query):
 WITH subscriber_subset AS (
     {self.subscriber_location_weights.get_query()}
 ), summed_weights AS (
-    SELECT subscriber, sum({self.weight_column}) AS total_weight
+    SELECT subscriber, greatest(sum({self.weight_column}), {self.minimum_total_weight}) AS total_weight
     FROM subscriber_subset
     WHERE {self.weight_column} >= 0
     GROUP BY subscriber
