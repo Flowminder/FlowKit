bc2a725a998df5716c3dedd2fe09dd09c1041ed3
Thingus <john.roberts@flowminder.org>
2023-01-06 10:36:40 +0000

    Wait for init and output logs

--- flowetl/tests/integration/conftest.py
+++ flowetl/tests/integration/conftest.py
@@ -394,7 +394,6 @@ def flowetl_container(
         )
         init_container.wait()
         logger.info("Init container log", logs=init_container.logs())
-
         logger.info("Starting FlowETL container")
         container = docker_client.containers.run(
             f"flowminder/flowetl:{container_tag}",
@@ -617,11 +616,15 @@ def run_dag(flowetl_container):
         trigger_cmd = ["airflow", "dags", "trigger", "-e", exec_date]
         if run_id is not None:
             trigger_cmd += ["-r", run_id]
-
-        flowetl_container.exec_run(
+        logger.info("Unpausing dag")
+        unpause_result = flowetl_container.exec_run(
             ["airflow", "dags", "unpause", dag_id], user="airflow"
         )
-        return flowetl_container.exec_run([*trigger_cmd, dag_id], user="airflow")
+        logger.info("Unpaused", result=unpause_result)
+        logger.info("Triggering", trigger_cmd=trigger_cmd)
+        run_result = flowetl_container.exec_run([*trigger_cmd, dag_id], user="airflow")
+        logger.info("Triggered", result=run_result)
+        return run_result
 
     yield trigger_dag
 
@@ -637,8 +640,12 @@ def dag_status(flowetl_container):
     """
 
     def dag_status(*, dag_id, exec_date):
+
         status_cmd = ["airflow", "dags", "state", dag_id, exec_date]
-        return flowetl_container.exec_run(status_cmd, user="airflow")
+        logger.info("Getting status", status_cmd=status_cmd)
+        status = flowetl_container.exec_run(status_cmd, user="airflow")
+        logger.info("Got status", status=status)
+        return status
 
     yield dag_status
 
