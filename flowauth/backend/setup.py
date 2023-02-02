7e5c6b75113f6780b0b9365936fa498ba5a2f7f4
Thingus <john.roberts@flowminder.org>
2023-02-02 14:41:09 +0000

    Fixing typo

--- flowauth/backend/setup.py
+++ flowauth/backend/setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "flask",
         "flask-sqlalchemy",
-        "flask-migration",
+        "flask-migrate",
         "alembic",
         "sqlalchemy <1.4.28",
         "flask-login",
