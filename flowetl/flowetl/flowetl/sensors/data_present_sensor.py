7962add71c187d6c2eebf5c5b6ddab047a486e1e
Thingus <john.roberts@flowminder.org>
2023-02-20 09:22:19 +0000

    Using SELECT EXISTS instead of COUNT

--- flowetl/flowetl/flowetl/sensors/data_present_sensor.py
+++ flowetl/flowetl/flowetl/sensors/data_present_sensor.py
@@ -6,6 +6,6 @@ from flowetl.mixins.fixed_sql_mixin import fixed_sql_operator
 
 DataPresentSensor = fixed_sql_operator(
     class_name="DataPresentSensor",
-    sql="SELECT COUNT (*) FROM {{ staging_table }} LIMIT 100;",
+    sql="SELECT EXISTS(SELECT * FROM {{ staging_table }} LIMIT 1);",
     is_sensor=True,
 )
