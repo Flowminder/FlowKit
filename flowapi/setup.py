5e5258ae6eba256746190d864e261139fd231174
James Harrison <james.harrison@flowminder.org>
2023-03-22 13:37:29 +0000

    Remove pins due to closed issue

--- flowapi/setup.py
+++ flowapi/setup.py
@@ -37,8 +37,8 @@ setup(
         "pyyaml >= 5.1",
         "apispec[yaml]",
         "get-secret-or-env-var",
-        "prance[osv] < 0.22.11.5.0",  # Pending https://github.com/RonnyPfannschmidt/prance/issues/145
-        "openapi_spec_validator ==0.4.0",  # Pending https://github.com/RonnyPfannschmidt/prance/issues/145
+        "prance[osv]",
+        "openapi_spec_validator",
         "werkzeug",
     ],
     extras_require={"test": ["pytest", "coverage"]},
