537296c87eeab40fd149e034a19050b12455cdd3
Thingus <john.roberts@flowminder.org>
2024-02-29 10:47:12 +0000

    Tidying up qa_check script

--- flowdb/testdata/test_data/py/run_etl_tests.py
+++ flowdb/testdata/test_data/py/run_etl_tests.py
@@ -3,29 +3,18 @@ from datetime import date, datetime
 from itertools import product
 from pathlib import Path
 from typing import Generator
-import flowetl
 from jinja2 import Environment, PackageLoader, Template
 from sqlalchemy import create_engine
 import os
 
 env = Environment(loader=PackageLoader("flowetl", "qa_checks/qa_checks"))
 
-count_duplicate_template = env.get_template("count_duplicates.sql")
-
-count_duplicates = count_duplicate_template.render(
-    final_table="events.calls", cdr_type="events"
-)
-
 db_user = os.getenv("POSTGRES_USER", "flowdb")
 db_name = os.getenv("POSTGRES_DB", "flowdb")
 db_port = os.getenv("POSTGRES_PORT", "9000")
 db_password = os.getenv("POSTGRES_PASSWORD", "flowflow")  # In here for dev, don't think
 conn_str = f"postgresql://{db_user}:{db_password}@localhost:{db_port}/{db_name}"
-print(conn_str)
-print(count_duplicates)
 engine = create_engine(conn_str)
-out = engine.execute(count_duplicates)
-print(out.fetchall())
 
 
 # @James; if something like this already exists, I'll grab
@@ -44,14 +33,13 @@ VALUES(
 
 """
 
+update_template = env.from_string(update_template_string)
+
 
 @dataclass
 class QaTemplate:
     display_name: str
     template: Template
-
-
-update_template = env.from_string(update_template_string)
 
 
 @dataclass
@@ -70,9 +58,8 @@ class MockQaScenario:
     tables: list[str]
 
 
-qa_scn = MockQaScenario([date(2021, 1, 1), date(2021, 1, 2)], ["calls"])
-
-
+# NOTE: given this gets run _after_ all the ingestion, 'dates' may be an irrelevence
+# unless final_table is normally one of the dated partitions
 def render_qa_check(template: Template, date: date, cdr_type: str) -> str:
     return template.render(
         final_table=f"events.{cdr_type}",
@@ -81,9 +68,12 @@ def render_qa_check(template: Template, date: date, cdr_type: str) -> str:
     )
 
 
+qa_scn = MockQaScenario(dates=[date(2016, 1, 1), date(2016, 1, 2)], tables=["calls"])
+
 templates = (
     QaTemplate(Path(t).name, env.get_template(t)) for t in env.list_templates(".sql")
 )
+
 
 qa_rows = (
     QaRow(
