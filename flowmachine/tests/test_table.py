1a2646feba870d1437963f671c5c2e3eb78e36aa
Jonathan Gray <jonathan.gray@flowminder.org>
2024-10-18 12:34:11 +0100

    Split version when checking the id

--- flowmachine/tests/test_table.py
+++ flowmachine/tests/test_table.py
@@ -82,7 +82,7 @@ def test_dependencies():
     t2 = Table("events.calls", columns=["id"])
     assert len(t2.dependencies) == 1
     t2_parent = t2.dependencies.pop()
-    assert "057addedac04dbeb1dcbbb6b524b43f0" == t2_parent.query_id
+    assert "057addedac04dbeb1dcbbb6b524b43f0" == t2_parent.query_id.split("_")[0]
 
 
 def test_subset():
