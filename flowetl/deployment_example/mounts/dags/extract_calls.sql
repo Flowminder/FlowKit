6a8ee449609ac80df3aa96b445b0f0049b0e11ce
James Harrison <james.harrison@flowminder.org>
2025-01-29 14:38:49 +0000

    Set collation in example ETL scripts

--- flowetl/deployment_example/mounts/dags/extract_calls.sql
+++ flowetl/deployment_example/mounts/dags/extract_calls.sql
@@ -1,5 +1,5 @@
 SELECT uuid_generate_v4()::TEXT as id, TRUE as outgoing, event_time::TIMESTAMPTZ as datetime, NULL::NUMERIC as duration,
-    NULL::TEXT as network, msisdn::TEXT as msisdn, NULL::TEXT as msisdn_counterpart, cell_id::TEXT as location_id, NULL::TEXT as imsi,
-    NULL::TEXT as imei, NULL::NUMERIC(8) as tac, NULL::NUMERIC as operator_code, NULL::NUMERIC as country_code
+    NULL::TEXT as network, msisdn::TEXT COLLATE "C" as msisdn, NULL::TEXT COLLATE "C" as msisdn_counterpart, cell_id::TEXT as location_id, NULL::TEXT COLLATE "C" as imsi,
+    NULL::TEXT COLLATE "C" as imei, NULL::NUMERIC(8) as tac, NULL::NUMERIC as operator_code, NULL::NUMERIC as country_code
 
     FROM {{ staging_table }}
